9460546b1f076bf2d494398e98c1f8c1e2c9523e
nikuco <nikolce.mihajlovski@gmail.com>
2012-04-25 02:25:12 +0200

    Added simple test calls for the basic table operations.

--- src/main/java/com/tightdb/TableTest.java
+++ src/main/java/com/tightdb/TableTest.java
@@ -4,6 +4,23 @@ public class TableTest {
 
 	public static void main(String[] args) {
 		TableBase base = new TableBase();
+
+		base.registerColumn(ColumnType.ColumnTypeString, "name");
+		base.registerColumn(ColumnType.ColumnTypeInt, "salary");
+
+		// base.addRow() - not in the JNI TableBase?
+		base.insertString(0, 1, "John");
+		base.insertLong(0, 1, 24000);
+		base.insertDone();
+
+		System.out.println(base.getColumnName(0));
+		System.out.println(base.getColumnName(1));
+
+		System.out.println(base.getCount());
+		System.out.println(base.getString(0, 0));
+		System.out.println(base.getString(1, 0));
+		
+		base.removeRow(0);
 	}
 
 }
