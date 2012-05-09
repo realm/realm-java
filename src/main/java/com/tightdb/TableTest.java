742907169090643c37ca1fb85d0c49eeeffb2a2b
nikuco <nikolce.mihajlovski@gmail.com>
2012-05-09 17:27:30 +0200

    Reverted the simple test back to normal.

--- src/main/java/com/tightdb/TableTest.java
+++ src/main/java/com/tightdb/TableTest.java
@@ -4,34 +4,34 @@ public class TableTest {
 
 	public static void main(String[] args) {
 		System.out.println("starting...");
-		
+
 		System.loadLibrary("tightdb");
-		
+
 		TableBase base = new TableBase();
 
 		System.out.println("created table");
-		
+
 		TableSpec tableSpec = new TableSpec();
-		//tableSpec.addColumn(ColumnType.ColumnTypeString, "name");
+		tableSpec.addColumn(ColumnType.ColumnTypeString, "name");
 		tableSpec.addColumn(ColumnType.ColumnTypeInt, "salary");
 		base.updateFromSpec(tableSpec);
 
 		System.out.println("specified structure");
-		
-		base.insertLong(0, 0, 24000); // FAILS HERE!
-		//base.insertString(0, 0, "John");
+
+		base.insertString(0, 0, "John");
+		base.insertLong(1, 0, 24000);
 		base.insertDone();
-		
+
 		System.out.println("inserted data");
 
-//		System.out.println(base.getColumnName(0));
-//		System.out.println(base.getColumnName(1));
-//
-//		System.out.println(base.getCount());
-//		System.out.println(base.getString(0, 0));
-//		System.out.println(base.getLong(1, 0));
-//
-//		base.removeRow(0);
+		System.out.println(base.getColumnName(0));
+		System.out.println(base.getColumnName(1));
+
+		System.out.println(base.getCount());
+		System.out.println(base.getString(0, 0));
+		System.out.println(base.getLong(1, 0));
+
+		base.removeRow(0);
 	}
 
 }
