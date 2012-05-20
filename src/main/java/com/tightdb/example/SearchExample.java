21a73f5f86bd7eaf0ad555fec458b4a297cee1fe
nikuco <nikolce.mihajlovski@gmail.com>
2012-05-20 09:21:47 +0200

    Removed obsolete code.

--- src/main/java/com/tightdb/example/SearchExample.java
+++ src/main/java/com/tightdb/example/SearchExample.java
@@ -3,28 +3,28 @@ package com.tightdb.example;
 import java.util.Arrays;
 import java.util.Date;
 
-import com.tightdb.example.generated.Person;
-import com.tightdb.example.generated.PersonQuery;
-import com.tightdb.example.generated.PersonTable;
-import com.tightdb.example.generated.PersonView;
+import com.tightdb.generated.Employee;
+import com.tightdb.generated.EmployeeQuery;
+import com.tightdb.generated.EmployeeTable;
+import com.tightdb.generated.EmployeeView;
 import com.tightdb.lib.TightDB;
 
 public class SearchExample {
 
 	public static void main(String[] args) {
-		PersonTable persons = new PersonTable();
+		EmployeeTable Employees = new EmployeeTable();
 
-		Person john = persons.add("John", "Doe", 10000, true, new byte[] { 1, 2, 3 }, new Date(), "extra");
-		Person johny = persons.add("Johny", "Goe", 20000, true, new byte[] { 1, 2, 3 }, new Date(), true);
-		Person nikolche = persons.insert(1, "Nikolche", "Mihajlovski", 30000, false, new byte[] { 4, 5 }, new Date(), 1234.56);
+		Employee john = Employees.add("John", "Doe", 10000, true, new byte[] { 1, 2, 3 }, new Date(), "extra");
+		Employee johny = Employees.add("Johny", "Goe", 20000, true, new byte[] { 1, 2, 3 }, new Date(), true);
+		Employee nikolche = Employees.insert(1, "Nikolche", "Mihajlovski", 30000, false, new byte[] { 4, 5 }, new Date(), 1234.56);
 
-		TightDB.print(persons);
+		TightDB.print(Employees);
 
 		// .salary.is(11) doesn't work
-		PersonQuery q1 = persons.firstName.startsWith("J").lastName.endWith("e");
+		EmployeeQuery q1 = Employees.firstName.startsWith("J").lastName.endWith("e");
 		System.out.println(q1);
 		
-		PersonView results = q1.findAll();
+		EmployeeView results = q1.findAll();
 		System.out.println(results);
 		
 		TightDB.print(results);
@@ -38,12 +38,12 @@ public class SearchExample {
 		
 		results.clear();
 		
-		TightDB.print(persons);
+		TightDB.print(Employees);
 		
-		long count = persons.firstName.contains("iko").clear();
+		long count = Employees.firstName.contains("iko").clear();
 		System.out.println("Removed " + count + " rows!");
 		
-		TightDB.print(persons);
+		TightDB.print(Employees);
 	}
 
 }
