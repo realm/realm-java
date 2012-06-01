1e3ea1e5fe4e69a0d91f7fbb57beeba7b3a26a74
nikuco <nikolce.mihajlovski@gmail.com>
2012-06-01 09:50:30 +0200

    Completed Row methods documentation.

--- src/main/java/com/tightdb/doc/RowDesc.java
+++ src/main/java/com/tightdb/doc/RowDesc.java
@@ -9,13 +9,13 @@ public class RowDesc extends AbstractDesc {
 	}
 
 	public void describe() {
-		method("Row", "after", "");
-		method("Row", "before", "");
-		method("AbstractColumn[]", "columns", "");
-		method("Row", "next", "");
-		method("Row", "previous", "");
-		method("FooType", "getFoo", "");
-		method("void", "setFoo", "", "FooType", "value");
+		method("Row", "after", "Get the row after the next delta rows", "long", "delta");
+		method("Row", "before", "Get the row before the previous delta rows", "long", "delta");
+		method("AbstractColumn[]", "columns", "Get the columns with values from the row");
+		method("Row", "next", "Get the next row as an object");
+		method("Row", "previous", "Get the previous row as an object");
+		method("FooType", "getFoo", "(Generic) Get the value of the 'foo' column. For each column there will be such method");
+		method("void", "setFoo", "(Generic) Set the value of the 'foo' column. For each column there will be such method", "FooType", "value");
 	}
 
 }
--- src/main/resources/RowExamples.java
+++ src/main/resources/RowExamples.java
@@ -1,63 +1,45 @@
 package com.tightdb.doc;
 
 import com.tightdb.generated.Employee;
+import com.tightdb.lib.AbstractColumn;
 
 public class RowExamples {
 
 	public static void main(String[] args) {
 
-		/* EXAMPLE: add */
+		/* EXAMPLE: after */
 
-		people.add("Mary", 21, false);
-		people.add("Lars", 24, true);
+		Person third = people.at(0).after(2);
 		
-		/* EXAMPLE: at */
+		/* EXAMPLE: before */
 
-		People p = people.at(42);
+		Person second = people.at(4).before(3);
 		
-		/* EXAMPLE: clear */
+		/* EXAMPLE: columns */
 
-		people.clear();
+		for (AbstractColumn<?, ?, ?, ?> column : john.columns()) {
+			System.out.println(column.getName() + "=" + column.getReadableValue());
+		}
 		
-		/* EXAMPLE: first */
+		/* EXAMPLE: next */
 
-		Person firstPerson = people.first();
+		Person fifth = people.at(3).next();
 		
-		/* EXAMPLE: getName */
+		/* EXAMPLE: previous */
 
-		String tableName = people.getName();
+		Person secondLast = people.last().previous();
 		
-		/* EXAMPLE: insert */
+		/* EXAMPLE: getFoo */
 
-		people.insert(0, "Mary", 21, false);
-		people.insert(0, "Lars", 21, true);
+		String name = people.first().getName();
+		int age = people.last().getAge();
 		
-		/* EXAMPLE: isEmpty */
+		/* EXAMPLE: setFoo */
 
-		boolean empty = people.isEmpty();
-		
-		/* EXAMPLE: iterator */
-
-		for (Person p : people) System.out.println(p);
-		
-		/* EXAMPLE: last */
-
-		Person lastPerson = people.last();
-		
-		/* EXAMPLE: remove */
-
-		people.remove(0);
-		
-		/* EXAMPLE: size */
-
-		long size = people.size();
-		
-		/* EXAMPLE: where */
-
-		people.where().age.is(22).findAll();
+		people.last().setName("John");
+		people.first().getAge(30);
 		
 		/* EXAMPLE: END! */
 		
 	}
-	
 }
