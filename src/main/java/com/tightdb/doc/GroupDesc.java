0dfce9dfb2aba719e14fe31bb7224e72fd0fd715
nikuco <nikolce.mihajlovski@gmail.com>
2012-06-01 11:14:25 +0200

    Documented the Group methods.

--- src/main/java/com/tightdb/doc/GroupDesc.java
+++ src/main/java/com/tightdb/doc/GroupDesc.java
@@ -2,6 +2,8 @@ package com.tightdb.doc;
 
 import java.util.List;
 
+import com.tightdb.Group;
+
 public class GroupDesc extends AbstractDesc {
 
 	public GroupDesc(List<Method> methods) {
@@ -10,6 +12,18 @@ public class GroupDesc extends AbstractDesc {
 
 	public void describe() {
 
-		// Group g = null;
+		method("void", "close", "Close the group and release resources");
+		method("boolean", "commit", "Commit the changes");
+		method("TableBase", "getTable", "(Low-level) Create and get a table with the specified name");
+		method("int", "getTableCount", "Get the number of tables");
+		method("String", "getTableName", "Get the table at the specified position", "int", "index");
+		method("boolean", "hasTable", "Check if group contains a specific named table", "String", "name");
+		method("boolean", "isValid", "Validate the group");
+		method("ByteBuffer", "writeToByteBuffer", "Serialize the group to a ByteBuffer");
+		method("void", "writeToFile", "Serialize the group to disk", "File", "file");
+		method("void", "writeToFile", "Serialize the group to disk", "String", "fileName");
+		method("byte[]", "writeToMem", "Serialize the group to a memory buffer");
+		
+		Group g = null;
 	}
 }
