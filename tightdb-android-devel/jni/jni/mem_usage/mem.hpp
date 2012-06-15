022be4277ab1f75f96091585e8370778bdb1840f
Brian Munkholm <bm@tightdb.com>
2012-06-15 12:39:15 +0200

    Removed redundant code.

--- tightdb-android-devel/jni/jni/mem_usage/mem.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __SUPPORT_MEM__
-#define __SUPPORT_MEM__
-
-#include <cstdlib> // size_t
-
-size_t GetMemUsage();
-
-#endif //__SUPPORT_MEM__
--- tightdb-android-devel/jni/src/win32/types.h
+++
@@ -1,8 +0,0 @@
-#ifndef TYPES_H
-#define TYPES_H
-
-#ifdef _MSC_VER
-typedef ptrdiff_t ssize_t;
-#endif
-
-#endif
