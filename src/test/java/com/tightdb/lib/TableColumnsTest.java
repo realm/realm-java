1511a1be6ccea2aa03b1e721b1e14d8c6424f640
nikuco <nikolce.mihajlovski@gmail.com>
2012-07-11 16:53:43 +0200

    Added tests for "getAll" and "setAll" column methods (issue #36).

--- src/test/java/com/tightdb/lib/TableColumnsTest.java
+++ src/test/java/com/tightdb/lib/TableColumnsTest.java
@@ -1,11 +1,14 @@
 package com.tightdb.lib;
 
 import static org.testng.AssertJUnit.*;
+import static org.testng.internal.junit.ArrayAsserts.*;
 
+import java.nio.ByteBuffer;
 import java.util.Date;
 
 import org.testng.annotations.Test;
 
+import com.tightdb.Mixed;
 import com.tightdb.example.generated.Employee;
 import com.tightdb.example.generated.EmployeeView;
 import com.tightdb.test.EmployeesFixture;
@@ -15,7 +18,7 @@ public class TableColumnsTest extends AbstractTableTest {
 	@Test
 	public void shouldFindFirstRecordByColumnValue() throws IllegalAccessException {
 		Employee record = null;
-		
+
 		record = employees.firstName.findFirst(EmployeesFixture.EMPLOYEE[1].firstName);
 		assertEquals(1, record.getPosition());
 
@@ -53,19 +56,19 @@ public class TableColumnsTest extends AbstractTableTest {
 
 		view = employees.salary.findAll(12345);
 		assertEquals(0, view.size());
-		
+
 		view = employees.driver.findAll(false);
 		assertEquals(1, view.size());
-		
+
 		view = employees.driver.findAll(true);
 		assertEquals(2, view.size());
-		
+
 		view = employees.birthdate.findAll(EmployeesFixture.EMPLOYEE[2].birthdate);
 		assertEquals(1, view.size());
-		
+
 		view = employees.birthdate.findAll(EmployeesFixture.EMPLOYEE[1].birthdate);
 		assertEquals(1, view.size());
-		
+
 		view = employees.birthdate.findAll(new Date(0));
 		assertEquals(0, view.size());
 	}
@@ -74,17 +77,64 @@ public class TableColumnsTest extends AbstractTableTest {
 	public void shouldAggregateColumnValue() {
 		assertEquals(EmployeesFixture.EMPLOYEE[0].salary, employees.salary.minimum());
 		assertEquals(EmployeesFixture.EMPLOYEE[1].salary, employees.salary.maximum());
-		long sum = EmployeesFixture.EMPLOYEE[0].salary + EmployeesFixture.EMPLOYEE[1].salary +
-				EmployeesFixture.EMPLOYEE[2].salary;
+		long sum = EmployeesFixture.EMPLOYEE[0].salary + EmployeesFixture.EMPLOYEE[1].salary + EmployeesFixture.EMPLOYEE[2].salary;
 		assertEquals(sum, employees.salary.sum());
 	}
 
 	@Test
 	public void shouldAddValueToWholeColumn() {
 		employees.salary.addLong(123);
-		for (int i=0; i<EmployeesFixture.EMPLOYEE.length; ++i)
-			assertEquals(EmployeesFixture.EMPLOYEE[i].salary+123, employees.at(i).getSalary());
+		for (int i = 0; i < EmployeesFixture.EMPLOYEE.length; ++i)
+			assertEquals(EmployeesFixture.EMPLOYEE[i].salary + 123, employees.at(i).getSalary());
 	}
 
-	
+	@Test
+	public void shouldGetAllColumnValues() {
+		assertArrayEquals(EmployeesFixture.getAll(0), employees.firstName.getAll());
+		assertArrayEquals(EmployeesFixture.getAll(1), employees.lastName.getAll());
+		assertArrayEquals(EmployeesFixture.getAll(2), employees.salary.getAll());
+		assertArrayEquals(EmployeesFixture.getAll(3), employees.driver.getAll());
+		assertArrayEquals(EmployeesFixture.getAll(4), employees.photo.getAll());
+		assertArrayEquals(EmployeesFixture.getAll(5), employees.birthdate.getAll());
+		assertArrayEquals(EmployeesFixture.getAll(6), employees.extra.getAll());
+	}
+
+	@Test
+	public void shouldSetAllColumnValues() {
+		employees.firstName.setAll("A");
+		assertSameArrayElement("A", employees.firstName.getAll());
+
+		employees.lastName.setAll("B");
+		assertSameArrayElement("B", employees.lastName.getAll());
+
+		Long num = 12345L;
+		employees.salary.setAll(num);
+		assertSameArrayElement(num, employees.salary.getAll());
+
+		employees.driver.setAll(true);
+		assertSameArrayElement(true, employees.driver.getAll());
+
+		ByteBuffer buf = ByteBuffer.allocateDirect(2);
+		buf.put(new byte[] { 10, 20 });
+		employees.photo.setAll(buf);
+		for (ByteBuffer buffer : employees.photo.getAll()) {
+			ByteBuffer buf2 = ByteBuffer.wrap(new byte[] { 10, 20 });
+			assertEquals(buf2, buffer);
+		}
+
+		Date date = new Date(13579);
+		employees.birthdate.setAll(date);
+		assertSameArrayElement(date, employees.birthdate.getAll());
+
+		Mixed extra = Mixed.mixedValue("extra");
+		employees.extra.setAll(extra);
+		assertSameArrayElement(extra, employees.extra.getAll());
+	}
+
+	private void assertSameArrayElement(Object expected, Object[] arr) {
+		for (Object element : arr) {
+			assertEquals(expected, element);
+		}
+	}
+
 }
--- src/test/java/com/tightdb/test/EmployeeData.java
+++ src/test/java/com/tightdb/test/EmployeeData.java
@@ -1,6 +1,9 @@
 package com.tightdb.test;
 
+import java.nio.ByteBuffer;
 import java.util.Date;
+
+import com.tightdb.Mixed;
 
 public class EmployeeData {
 
@@ -20,6 +23,27 @@ public class EmployeeData {
 		this.photo = photo;
 		this.birthdate = birthdate;
 		this.extra = extra;
+	}
+
+	public Object get(int index) {
+		switch (index) {
+		case 0:
+			return firstName;
+		case 1:
+			return lastName;
+		case 2:
+			return new Long(salary);
+		case 3:
+			return new Boolean(driver);
+		case 4:
+			return ByteBuffer.wrap(photo);
+		case 5:
+			return birthdate;
+		case 6:
+			return Mixed.mixedValue(extra);
+		default:
+			throw new IllegalArgumentException("Incorrect index: " + index);
+		}
 	}
 
 }
