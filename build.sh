5c980f111348b1f28dd92924f3b7b748f4d5434c
Kristian Spangsege <ks@tightdb.com>
2012-08-16 19:27:19 +0200

    Working on script to build a complete distribution package including all language bindings

--- build.sh
+++ build.sh
@@ -1,25 +1,56 @@
-JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64 # Ununtu 12.04 64-bit
-TIGHTDB_JAVA_HOME="$HOME/tightdb_java2"
+cd "$(dirname "$0")"
+TIGHTDB_JAVA_HOME="$(pwd)"
+
+MAKE="make -j8"
+
+# Setup OS specific stuff
+OS="$(uname -o)" || exit 1
+if [ "$OS" = "Darwin" ]; then
+    MAKE="$MAKE CC=gcc"
+fi
+
+# Find 'jni.h', 'java' and 'javac'
+if [ -z "$JAVA_HOME" ]; then
+    if ! which javac >/dev/null; then
+        echo "No JAVA_HOME and no Java compiler in PATH" 1>&2
+        exit 1
+    fi
+    JAVAC=$(readlink -f "$(which javac)")
+    if ! echo "$JAVAC" | grep '/bin/javac$' >/dev/null 2>&1; then
+        echo "Could not determine JAVA_HOME from path of 'javac' command" 1>&2
+        exit 1
+    fi
+    JAVA_HOME="$(echo "$JAVAC" | sed 's|/bin/javac$||')"
+fi
+for x in include/jni.h bin/java bin/javac; do
+    if ! [ -f "$JAVA_HOME/$x" ]; then
+        echo "No '$x' in '$JAVA_HOME'" 1>&2
+        exit 1
+    fi
+done
+JAVA="$JAVA_HOME/bin/java"
+JAVAC="$JAVA_HOME/bin/javac"
+JAVA_VERSION="$(java -version 2>&1 | grep '^java version' | sed 's/.*"\(.*\).*"/\1/')"
+JAVA_MAJOR="$(echo "$JAVA_VERSION" | cut -d. -f1)"
+JAVA_MINOR="$(echo "$JAVA_VERSION" | cut -d. -f2)"
+if [ "$JAVA_MAJOR" -lt 1 -o \( "$JAVA_MAJOR" -eq 1 -a "$JAVA_MINOR" -lt 6 \) ]; then
+    echo "Need Java version 1.6 or newer (is '$JAVA_VERSION')" 1>&2
+    exit 1
+fi
+
 
 
 # Build libtightdb-jni.so
 cd "$TIGHTDB_JAVA_HOME/tightdb_jni/src" || exit 1
-make clean || exit 1
-make -j8 EXTRA_CFLAGS="-I\"$JAVA_HOME/include\"" || exit 1
+$MAKE clean || exit 1
+$MAKE EXTRA_CFLAGS="-I\"$JAVA_HOME/include\"" || exit 1
 
 # Build tightdb.jar
 cd "$TIGHTDB_JAVA_HOME/src/main/java" || exit 1
 find com/ -type f -name '*.class' -delete || exit 1
 export CLASSPATH=/usr/share/java/commons-io.jar:/usr/share/java/commons-lang.jar:/usr/share/java/freemarker.jar:.
-javac $(find com/ -type f -name '*.java' | fgrep -v /doc/ | fgrep -v /example/) || exit 1
+$JAVAC $(find com/ -type f -name '*.java' | fgrep -v /doc/ | fgrep -v /example/) || exit 1
 jar cf tightdb.jar $(find com/ -type f -name '*.class') || exit 1
-
-# Build and run example
-cd "$TIGHTDB_JAVA_HOME/tightdb-example/src/main/java" || exit 1
-find com/ -type f -name '*.class' -delete || exit 1
-export CLASSPATH="$TIGHTDB_JAVA_HOME/src/main/java/tightdb.jar:."
-javac com/tightdb/example/Example.java com/tightdb/example/generated/*.java || exit 1
-java -Djava.library.path="$TIGHTDB_JAVA_HOME/tightdb_jni/src" com.tightdb.example.Example || exit 1
 
 # Build and run test suite
 cd "$TIGHTDB_JAVA_HOME/src/test/java" || exit 1
@@ -27,5 +58,5 @@ find com/ -type f -name '*.class' -delete || exit 1
 test_sources="$(find * -type f -name '*Test.java')"
 test_classes="$(echo "$test_sources" | sed 's/\.java$/.class/')"
 export CLASSPATH="$TIGHTDB_JAVA_HOME/src/main/java/tightdb.jar:$TIGHTDB_JAVA_HOME/tightdb-example/src/main/java:/usr/share/java/testng.jar:/usr/share/java/qdox.jar:/usr/share/java/bsh.jar:."
-javac $test_sources || exit 1
-java -Djava.library.path="$TIGHTDB_JAVA_HOME/tightdb_jni/src" org.testng.TestNG -d "$TIGHTDB_JAVA_HOME/test_output" -testclass $test_classes || exit 1
+$JAVAC $test_sources || exit 1
+$JAVA -Djava.library.path="$TIGHTDB_JAVA_HOME/tightdb_jni/src" org.testng.TestNG -d "$TIGHTDB_JAVA_HOME/test_output" -testclass $test_classes || exit 1
--- generic.mk
+++ generic.mk
@@ -7,18 +7,16 @@ CONFIG_MK = $(ROOT)/config.mk
 SOURCE_DIR ?= src
 
 
-SUFFIX_OBJ_SHARED = .dyn.o
+SUFFIX_OBJ_SHARED = .pic.o
 SUFFIX_OBJ_STATIC = .o
 SUFFIX_OBJ_DEBUG  = .dbg.o
 SUFFIX_OBJ_COVER  = .cov.o
-
 SUFFIX_LIB_SHARED = .so
 SUFFIX_LIB_STATIC = .a
 SUFFIX_LIB_DEBUG  = -dbg.a
 SUFFIX_LIB_COVER  = -cov.a
-
-SUFFIX_PROG_DEBUG  = -dbg
-SUFFIX_PROG_COVER  = -cov
+SUFFIX_PROG_DEBUG = -dbg
+SUFFIX_PROG_COVER = -cov
 
 
 
@@ -32,7 +30,6 @@ GET_NOINST_LIBS_FOR_TARGET = $(foreach x,$($(call FOLD_TARGET,$(1))_NOINST_LIBAD
 GET_FLAGS_HELPER = $($(if $(filter undefined,$(origin $(1)$(2))),$(1),$(1)$(2)))
 GET_CFLAGS_FOR_OBJECT = $(call GET_FLAGS_HELPER,$(call FOLD_TARGET,$(1))_CFLAGS,$(2))
 GET_LDFLAGS_FOR_TARGET = $(call GET_FLAGS_HELPER,$(call FOLD_TARGET,$(1))_LDFLAGS,$(2))
-#GET_INST_LIB_TARGETS = $(foreach x,$($(1)_LIBRARIES),$(foreach y,$(SUFFIX_LIB_SHARED) $(SUFFIX_LIB_STATIC) $(SUFFIX_LIB_DEBUG),$(call GET_LIBRARY_NAME,$(x),$(y))))
 
 
 SOURCE_ROOT   = $(ROOT)/$(SOURCE_DIR)
@@ -59,14 +56,14 @@ TARGETS_NOINST_LIB        = $(foreach x,$(NOINST_LIBRARIES),$(call GET_LIBRARY_N
 TARGETS_NOINST_LIB_DEBUG  = $(foreach x,$(NOINST_LIBRARIES),$(call GET_LIBRARY_NAME,$(x),$(SUFFIX_LIB_DEBUG)))
 TARGETS_NOINST_LIB_COVER  = $(foreach x,$(NOINST_LIBRARIES),$(call GET_LIBRARY_NAME,$(x),$(SUFFIX_LIB_COVER)))
 TARGETS_PROG              = $(INST_PROGRAMS)
-TARGETS_PROG_DEBUG        = $(patsubst %,%$(SUFFIX_PROG_DEBUG),$(INST_PROGRAMS))
-TARGETS_PROG_COVER        = $(patsubst %,%$(SUFFIX_PROG_COVER),$(INST_PROGRAMS))
+TARGETS_PROG_DEBUG        = $(foreach x,$(INST_PROGRAMS),$(x)$(SUFFIX_PROG_DEBUG))
+TARGETS_PROG_COVER        = $(foreach x,$(INST_PROGRAMS),$(x)$(SUFFIX_PROG_COVER))
 TARGETS_NOINST_PROG       = $(NOINST_PROGRAMS)
-TARGETS_NOINST_PROG_DEBUG = $(patsubst %,%$(SUFFIX_PROG_DEBUG),$(NOINST_PROGRAMS))
-TARGETS_NOINST_PROG_COVER = $(patsubst %,%$(SUFFIX_PROG_COVER),$(NOINST_PROGRAMS))
+TARGETS_NOINST_PROG_DEBUG = $(foreach x,$(NOINST_PROGRAMS),$(x)$(SUFFIX_PROG_DEBUG))
+TARGETS_NOINST_PROG_COVER = $(foreach x,$(NOINST_PROGRAMS),$(x)$(SUFFIX_PROG_COVER))
 TARGETS_TEST_PROG         = $(TEST_PROGRAMS)
-TARGETS_TEST_PROG_DEBUG   = $(patsubst %,%$(SUFFIX_PROG_DEBUG),$(TEST_PROGRAMS))
-TARGETS_TEST_PROG_COVER   = $(patsubst %,%$(SUFFIX_PROG_COVER),$(TEST_PROGRAMS))
+TARGETS_TEST_PROG_DEBUG   = $(foreach x,$(TEST_PROGRAMS),$(x)$(SUFFIX_PROG_DEBUG))
+TARGETS_TEST_PROG_COVER   = $(foreach x,$(TEST_PROGRAMS),$(x)$(SUFFIX_PROG_COVER))
 
 TARGETS_DEFAULT    = $(TARGETS_LIB_SHARED) $(TARGETS_LIB_STATIC) $(TARGETS_LIB_DEBUG) $(TARGETS_NOINST_LIB) $(TARGETS_PROG) $(TARGETS_NOINST_PROG)
 TARGETS_DEBUG      = $(TARGETS_LIB_DEBUG) $(TARGETS_NOINST_LIB_DEBUG) $(TARGETS_PROG_DEBUG) $(TARGETS_NOINST_PROG_DEBUG)
@@ -92,15 +89,9 @@ clean/local:
 	$(RM) $(strip *.d *.o *.gcno *.gcda $(TARGETS))
 endif
 
-install/local: $(TARGETS_DEFAULT)
-#	$(INSTALL_LIB) $(call GET_INST_LIB_TARGETS,lib) $(DESTDIR)$(libdir)/
-# FIXME: Implement
-
-uninstall/local:
-# FIXME: Implement
-
 test/local: $(TARGETS_TEST)
-	$(subst ;,,$(subst ; ./,; && ./,$(patsubst %,./%;,$(TARGETS_TEST_PROG))))
+	$(foreach x,$(TARGETS_TEST_PROG),./$(x)
+	) $(subst ;,,$(subst ; ./,; && ./,$(patsubst %,./%;,)))
 
 test-debug/local: $(TARGETS_TEST_DEBUG)
 	$(subst ;,,$(subst ; ./,; && ./,$(patsubst %,./%;,$(TARGETS_TEST_PROG_DEBUG))))
@@ -156,6 +147,45 @@ $(1): $(1)/local $$(patsubst %,subdir/%/$(1),$$(SUBDIRS))
 endef
 
 $(foreach x,$(RECURSIVE_MODES),$(eval $(call REVUSIVE_MODE_RULES,$(x))))
+
+
+
+# INSTALL / UNINSTALL
+
+install/local: $(TARGETS_DEFAULT) install-libs install-progs
+uninstall/local: # FIXME: Implement
+
+# FIXME: Header installation
+
+GET_LIB_INSTALL_DIR  = $(if $($(1)_DIR),$($(1)_DIR),$(libdir))
+GET_PROG_INSTALL_DIR = $(if $($(1)_DIR),$($(1)_DIR),$(bindir))
+
+INSTALL_RECIPE_DIR  = $(if $(1),$(INSTALL_DIR) $(1),)
+INSTALL_RECIPE_LIB  = $(if $(2),$(INSTALL_LIBRARY) $(2) $(call GET_LIB_INSTALL_DIR,$(1)),)
+INSTALL_RECIPE_PROG = $(if $(2),$(INSTALL_PROGRAM) $(2) $(call GET_PROG_INSTALL_DIR,$(1)),)
+
+GET_INST_LIB_TARGETS  = $(foreach x,$($(1)_LIBRARIES),$(foreach y,$(SUFFIX_LIB_SHARED) $(SUFFIX_LIB_STATIC) $(SUFFIX_LIB_DEBUG),$(call GET_LIBRARY_NAME,$(x),$(y))))
+GET_INST_PROG_TARGETS = $($(1)_PROGRAMS)
+
+define INSTALL_RULES
+
+install-libs: $(TARGETS_LIB_SHARED) $(TARGETS_LIB_STATIC) $(TARGETS_LIB_DEBUG) install-lib-dirs
+	$(foreach x,lib $(EXTRA_INSTALL_PREFIXES),$(call INSTALL_RECIPE_LIB,$(x),$(call GET_INST_LIB_TARGETS,$(x)))
+	)
+
+install-progs: $(TARGETS_PROG) install-prog-dirs
+	$(foreach x,bin $(EXTRA_INSTALL_PREFIXES),$(call INSTALL_RECIPE_PROG,$(x),$(call GET_INST_PROG_TARGETS,$(x)))
+	)
+
+install-lib-dirs:
+	$(call INSTALL_RECIPE_DIR,$(strip $(foreach x,lib $(EXTRA_INSTALL_PREFIXES),$(if $($(x)_LIBRARIES),$(call GET_LIB_INSTALL_DIR,$(x)),))))
+
+install-prog-dirs:
+	$(call INSTALL_RECIPE_DIR,$(strip $(foreach x,bin $(EXTRA_INSTALL_PREFIXES),$(if $($(x)_PROGRAMS),$(call GET_PROG_INSTALL_DIR,$(x)),))))
+
+endef
+
+$(eval $(INSTALL_RULES))
 
 
 
@@ -215,7 +245,7 @@ $(foreach x,$(PROGRAMS),$(eval $(call PROGRAM_RULES,$(x))))
 
 
 
-# Flex and Bison
+# FLEX AND BISON
 
 %.flex.cpp %.flex.hpp: %.flex $(DEP_MAKEFILES)
 	flex --outfile=$*.flex.cpp --header-file=$*.flex.hpp $<
