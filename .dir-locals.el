09e2fed95f3fa191725264f9695b5b8182f857aa
Kristian Spangsege <kristian.spangsege@gmail.com>
2012-09-03 10:42:28 +0200

    Emacs indentation update

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,5 @@
 ;; Project specific Emacs settings
 ((nil . ((c-basic-offset . 4)
-         (indent-tabs-mode . nil))))
+         (indent-tabs-mode . nil)
+         (c-file-style . "ellemtel")
+         (c-file-offsets . ((innamespace . 0))))))
