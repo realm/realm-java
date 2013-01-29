6892df6eed75e81c361a60c7e7b723c056ced124
nikuco <nikolce.mihajlovski@gmail.com>
2013-01-29 13:56:42 +0100

    Added the missing tests after the package name refactoring.

---
+++ tightdb-java-test/src/test/java/com/tightdb/typed/AbstractNavigationTest.java
@@ -0,0 +1,67 @@
+package com.tightdb.typed;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertNull;
+
+import org.testng.annotations.Test;
+
+import com.tightdb.test.TestEmployeeQuery;
+import com.tightdb.test.TestEmployeeRow;
+import com.tightdb.test.TestEmployeeView;
+import com.tightdb.typed.AbstractTableOrView;
+
+public abstract class AbstractNavigationTest {
+
+	protected abstract AbstractTableOrView<TestEmployeeRow, TestEmployeeView, TestEmployeeQuery> getTableOrView();
+
+	@Test
+	public void shouldNavigateToFirstRecord() {
+		TestEmployeeRow first = getTableOrView().first();
+
+		assertEquals(0, first.getPosition());
+	}
+
+	@Test
+	public void shouldNavigateToLastRecord() {
+		TestEmployeeRow last = getTableOrView().last();
+
+		assertEquals(getTableOrView().size() - 1, last.getPosition());
+	}
+
+	@Test
+	public void shouldNavigateToNextRecord() {
+		TestEmployeeRow e = getTableOrView().at(0).next();
+
+		assertEquals(1, e.getPosition());
+	}
+
+	@Test
+	public void shouldNavigateToPreviousRecord() {
+		TestEmployeeRow e = getTableOrView().at(1).previous();
+
+		assertEquals(0, e.getPosition());
+	}
+
+	@Test
+	public void shouldNavigateAfterSpecifiedRecords() {
+		TestEmployeeRow e = getTableOrView().at(0).after(2);
+
+		assertEquals(2, e.getPosition());
+	}
+
+	@Test
+	public void shouldNavigateBeforeSpecifiedRecords() {
+		TestEmployeeRow e = getTableOrView().at(2).before(2);
+
+		assertEquals(0, e.getPosition());
+	}
+
+	@Test
+	public void shouldReturnNullOnInvalidPosition() {
+		assertNull(getTableOrView().at(0).previous());
+		assertNull(getTableOrView().last().next());
+		assertNull(getTableOrView().at(1).before(2));
+		assertNull(getTableOrView().at(2).after(1000));
+	}
+
+}
