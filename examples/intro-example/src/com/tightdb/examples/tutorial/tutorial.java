40ffc1506dec18ecce5eafc2fa21d3fbf60f15c4
Brian Munkhol <bm@tightdb.com>
2013-03-01 01:32:42 +0100

    updated tutorial

--- examples/intro-example/src/com/tightdb/examples/tutorial/tutorial.java
+++ examples/intro-example/src/com/tightdb/examples/tutorial/tutorial.java
@@ -6,76 +6,81 @@
 package com.tightdb.examples.tutorial;
 
 import java.io.IOException;
-
 import com.tightdb.*;
 
 @SuppressWarnings("unused")
+
 //@@Example: create_table @@
 public class tutorial {
 	// Define the TighDB table with columns "name", "age" and "hired"
 	@DefineTable(table = "PeopleTable")
 	class people {
-		String 	name;
-		int 	age;
-		boolean hired;
+	    String  name;
+	    int     age;
+	    boolean hired;
 	}
-
+	
 	public static void main(String[] args) {
-	PeopleTable peopletable = new PeopleTable();
+	PeopleTable peopleTable = new PeopleTable();
 	//@@EndExample@@
 
 	/****************************** BASIC OPERATIONS *************************/
 
 	// @@Example: insert_rows @@
-	peopletable.add("John", 20, true);
-	peopletable.add("Mary", 21, false);
-	peopletable.add("Lars", 32, true);
-	peopletable.add("Phil", 43, false);
-	peopletable.add("Anni", 54, true);
+	peopleTable.add("John", 20, true);
+	peopleTable.add("Mary", 21, false);
+	peopleTable.add("Lars", 32, true);
+	peopleTable.add("Phil", 43, false);
+	peopleTable.add("Anni", 54, true);
 	// @@EndExample@@
 	
 	// @@Example: insert_at_index @@
-	peopletable.insert(2, "Frank", 34, true);
+	peopleTable.insert(2, "Frank", 34, true);
 	// @@EndExample@@
 	
 	// @@Example: number_of_rows @@
-	if (!peopletable.isEmpty()) {
-		long s = peopletable.size(); // s => 6
+	if (!peopleTable.isEmpty()) {
+	    long s = peopleTable.size(); // s => 6
 	}
 	// @@EndExample@@
 	
-	System.out.println("Size = " + peopletable.size() + "\n");
+	System.out.println("Size = " + peopleTable.size() + "\n");
 	
 	/****************************** GETTERS AND SETTERS **********************/
 	
 	// @@Example: accessing_rows @@
 	// 2 ways to get the value
-	String name = peopletable.at(2).getName(); // name => "Mary"
+	String name = peopleTable.at(2).getName(); // name => "Mary"
 	// or
-	String name2 = peopletable.at(2).name.get();
+	String name2 = peopleTable.at(2).name.get();
 	
 	// 2 ways to set the value
-	peopletable.at(2).name.set("NewName");
+	peopleTable.at(2).name.set("NewName");
 	// or
-	peopletable.at(2).setName("NewName"); 
+	peopleTable.at(2).setName("NewName"); 
 	// @@EndExample@@
 	
 	System.out.println("at(2).getName -> " + name + " or " + name2);
-	System.out.println("at(2).setName('NewName') -> " + peopletable.at(2).getName());
+	System.out.println("at(2).setName('NewName') -> " + peopleTable.at(2).getName());
+	
+	// @@Example: last_row @@	
+	String lastRowName = peopleTable.last().getName();	// retrieve name for last row
+	// @@EndExample@@
+	
 	
 	/****************************** DATA REMOVAL *****************************/
 	// @@Example: deleting_row @@
-	peopletable.remove(2);
+	peopleTable.remove(2);
 	// @@EndExample@@
 	
-	System.out.println("\nRemoved row 2. Down to " + peopletable.size() + " rows.\n");
+	System.out.println("\nRemoved row 2. Down to " + peopleTable.size() + " rows.\n");
 	
 	/****************************** ITERATION OF ALL RECORDS *****************/
 	
 	// lazy iteration over the table
 	
 	// @@Example: iteration @@
-	for (PeopleRow person : peopletable) {
+	for (PeopleRow person : peopleTable) {
 	    System.out.println(person.getName() + " is " + person.getAge() + " years old.");
 	}
 	// @@EndExample@@
@@ -84,33 +89,33 @@ public class tutorial {
 	
 	System.out.println("\nFound: ");
 	// @@Example: simple_seach @@
-	PeopleRow p = peopletable.name.equal("John").findFirst();
+	PeopleRow p = peopleTable.name.equal("John").findFirst();
 	System.out.println( p );	
-	// prints: "People {name=John, age=20, hired=true}"
+	// prints: "Employee {name=John, age=20, hired=true}"
 	// @@EndExample@@
 	
 	/****************************** COMPLEX QUERY ****************************/
 	
 	// @@Example: advanced_search @@
 	// Define the query
-	PeopleQuery query = peopletable
-			.age.between(20, 35)		// Implicit AND with below
-			.name.contains("a")		// Implicit AND with below
-			.group()			// "("
-				.hired.equal(true)
-				.or()			// or
-				.name.endsWith("y")
-			.endGroup();			// ")"
+	PeopleQuery query = peopleTable
+	                       .age.between(20, 35)    // Implicit AND with below
+	                       .name.contains("a")     // Implicit AND with below
+	                       .group()                // "("
+	                           .hired.equal(true)
+	                           .or()               // or
+	                           .name.endsWith("y")
+	                       .endGroup();            // ")"
 	// Count matches
 	PeopleView match = query.findAll();
-	System.out.println(match.size() + " person(s) match query.");
+	System.out.println(match.size() + " employee(s) match query.");
 	
 	// Take the average age of the matches    
 	System.out.println(match.age.sum() + " years is the summed age.");
 	
 	// Perform query and use the result
 	for (PeopleRow person : match) {
-		// ... do something with matching 'person'
+	    // ... do something with matching 'person'
 	}
 	// @@EndExample
 	System.out.println("");
@@ -121,24 +126,24 @@ public class tutorial {
 	// @@Example: serialisation @@
 	// Create Table in Group
 	Group group = new Group();
-	PeopleTable people1 = new PeopleTable(group);
+	PeopleTable person1 = new PeopleTable(group);
 	
-	people1.add("John", 20, true);
-	people1.add("Mary", 21, false);
+	person1.add("John", 20, true);
+	person1.add("Mary", 21, false);
 	
 	// Write to disk
 	try {
-		group.writeToFile("people.tightdb");
+	    group.writeToFile("people.tightdb");
 	} catch (IOException e) {
-		e.printStackTrace();
+	    e.printStackTrace();
 	}
-	
+
 	// Load a group from disk (and print contents)
 	Group fromDisk = new Group("people.tightdb");
 	PeopleTable people2 = new PeopleTable(fromDisk);
 	
 	for (PeopleRow person : people2) {
-		System.out.println(person.getName() + " is " + person.getAge() + " years old");
+	    System.out.println(person.getName() + " is " + person.getAge() + " years old");
 	}
 	
 	// Write same group to memory buffer
@@ -149,8 +154,41 @@ public class tutorial {
 	PeopleTable people3 = new PeopleTable(fromMem);
 	
 	for (PeopleRow person : people3) {
-		System.out.println(person.getName() + " is " + person.getAge() + " years old");
+	    System.out.println(person.getName() + " is " + person.getAge() + " years old");
 	}
 	// @@EndExample@@
+
+	/****************************** TRANSACTIONS ********************************/
+
+	System.out.println("\nTransactions:");
+	// @@Example: transaction @@
+
+	// Open a shared group
+    SharedGroup db = new SharedGroup("people.tightdb");
+
+	// Write transaction:
+    WriteTransaction wrtTrans = db.beginWrite();	// Start transaction
+    try {
+        PeopleTable person = new PeopleTable(wrtTrans);
+	    // Add row to table
+	    person.add("Bill", 53, true);
+	    wrtTrans.commit();							// End transaction
+    } catch (Throwable e) {
+    	wrtTrans.rollback();						// or Rollback
+    }
+
+    // Read transaction:
+    ReadTransaction rdTrans = db.beginRead();		// Start transaction
+    PeopleTable people = new PeopleTable(rdTrans);
+	for (PeopleRow person2 : people) {
+	    System.out.println(person2.getName() + " is " + 
+	    				   person2.getAge() + " years old");
 	}
-}
+	rdTrans.endRead();								// End transaction
+    
+    // @@EndExample@@
+
+    
+	} // main
+
+} // class
