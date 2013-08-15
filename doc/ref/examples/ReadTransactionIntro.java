20fcac236ff1e641aafc786eb7b6f68a34822495
mekjaer <mk@tightdb.com>
2013-08-15 17:23:14 +0200

    Added method that takes a Group as parameter

--- doc/ref/examples/ReadTransactionIntro.java
+++ doc/ref/examples/ReadTransactionIntro.java
@@ -8,46 +8,44 @@ public class ReadTransactionIntro {
 
     public static void main(String[] args) {
         // @@Show@@
-        //Opens an existing database file. We assume the file is also being accessed by other processes, thats why we use a SharedGroup object
+        //Opens an existing database file. 
+        //We assume the file is also being accessed by other processes, 
+        //thats why we use a SharedGroup object
         SharedGroup group = new SharedGroup("mydatabase.tightdb");
-        
+
         //-------------------------------------------------------------------
         //Reading from the group using a transaction
         //-------------------------------------------------------------------
-        
+
         //Create a read transaction from the group
         ReadTransaction rt = group.beginRead();
-        
-        //Inside the read transaction we have a fully consistent and non mutable view of the group
-        try {
-            //Get the the name of the first table in the group, and retrieve the table using the name
-            String tableName = rt.getTableName(0);
-            Table table = rt.getTable(tableName);
 
-            //Get the size and column number of the table
-            long size = table.size();
-            long columnCount = table.getColumnCount();
-            
-            System.out.println(tableName + " has " + columnCount + " columns and " + size + " rows!");
-            
-            //Determine the type of the first column
-            ColumnType type = table.getColumnType(0);
-            
-            if(type.equals(ColumnType.ColumnTypeString)){
-                //Get the String value from column 0, row 0, if the type is a String
-                String value = table.getString(0, 0);
-                
-                System.out.println("Value is " + value);
-            }
+        //Inside the read transaction we have a fully consistent and immutable view of the group
+        try {
+            //Get a table from the group
+            Table table = rt.getTable("table");
+
+            //Do all table read operations here
+            //Actions inside a ReadTransacton will never affect the original group and tables
+            String value = table.getString(1, 0);
+
+            //As a Transaction extends Group, it can be passed as a Group parameter
+            analyzeGroup(rt);
+
         } finally {
-            //Always end the read transaction
+            //Always end the read transaction in a finally block, if something should go
+            //wrong inside the transaction. If it is not closed, a new transaction can not be initialized
             rt.endRead();
-        }  // @@EndShow@@
+        }  
     }
-    
+
+    private static void analyzeGroup(Group group){
+        String tableName = group.getTableName(0);
+    }// @@EndShow@@  //@@EndExample@@
+
     static void Assert(boolean check) {
         if (!check) {
             throw new RuntimeException();
         }
     }
-} //@@EndExample@@
+} 
