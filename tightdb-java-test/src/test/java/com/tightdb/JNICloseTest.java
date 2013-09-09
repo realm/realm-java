37d138722839586743c0a63a18cd9c0ecfa45ab0
mekjaer <mk@tightdb.com>
2013-09-09 12:39:03 +0200

    Test cases added for accessing queries and views on closed table

--- tightdb-java-test/src/test/java/com/tightdb/JNICloseTest.java
+++ tightdb-java-test/src/test/java/com/tightdb/JNICloseTest.java
@@ -1,12 +1,13 @@
 package com.tightdb;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.tightdb.test.TestHelper;
 
 public class JNICloseTest {
 
-    @Test (enabled=true, expectedExceptions = IllegalStateException.class)
+    @Test (expectedExceptions = IllegalStateException.class)
     public void shouldCloseTable() {
         // util.setDebugLevel(1);
         Table table = new Table();
@@ -30,7 +31,7 @@ public class JNICloseTest {
     }
     
     /**
-     * Make sure, that an illegavlStateException is thrown when trying to do queries on a closed table
+     * Make sure, that an illegalStateException is thrown when trying to do queries on a closed table
      */
     @Test(expectedExceptions = IllegalStateException.class)
     public void closeTableShouldThrowExceptionWhenQuery(){
@@ -43,7 +44,23 @@ public class JNICloseTest {
         
         query.findAll(); //Should throw exception, as table has been closed
     }
-
+    
+    
+    @Test()
+    public void closeTableShouldThrowExceptionWhenAccessingView(){
+        
+        Table table = TestHelper.getTableWithAllColumnTypes();
+        
+        TableQuery query = table.where();
+        
+        TableView view = query.findAll();
+        
+        table.close(); //Closes the table, should not be allowed to access the view
+        
+        try{ view.size(); assert(false); } catch (IllegalStateException e){} //size() should throw exception, as table is invalid
+        
+        
+    }
 
 
 }
