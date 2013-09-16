697fd0bdaccf589591376aa449debb76b44caf63
mekjaer <mk@tightdb.com>
2013-09-16 10:49:24 +0200

    tutorial rename github hack

--- examples/intro-example/src/com/tightdb/examples/tutorial/Tutorial_temp.java
+++ examples/intro-example/src/com/tightdb/examples/tutorial/Tutorial_temp.java
@@ -12,7 +12,7 @@ import com.tightdb.*;
 @SuppressWarnings("unused")
 
 //@@Example: create_table @@
-public class tutorial {
+public class Tutorial_temp {
     // Define the TighDB table with columns "name", "age" and "hired"
     @DefineTable(table = "PeopleTable")
     class people {
