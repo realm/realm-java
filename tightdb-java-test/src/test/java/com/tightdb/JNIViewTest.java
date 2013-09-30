81b0ad1b34382dc1a10b0e20970eceed2ee183ce
mekjaer <mk@tightdb.com>
2013-09-30 15:35:36 +0200

    fixed build.sh to TestTableModel

--- tightdb-java-test/src/test/java/com/tightdb/JNIViewTest.java
+++ tightdb-java-test/src/test/java/com/tightdb/JNIViewTest.java
@@ -119,7 +119,7 @@ public class JNIViewTest {
 
         byte[] arr1 = new byte[] {1,2,3};
         table.add(arr1);
-        assertArrayEquals(arr1, table.getBinaryByteArray(0, 0));
+        assertEquals(arr1, table.getBinaryByteArray(0, 0));
 
         TableView view = table.where().findAll();
 
@@ -127,7 +127,7 @@ public class JNIViewTest {
 
         view.setBinaryByteArray(0, 0, arr2);
 
-        assertArrayEquals(arr2, view.getBinaryByteArray(0, 0));
+        assertEquals(arr2, view.getBinaryByteArray(0, 0));
     }
     
     
