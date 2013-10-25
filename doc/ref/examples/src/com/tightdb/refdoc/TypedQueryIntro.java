ee6426547e308dc456738664bb41566c510e1b6e
Alexander Stigsen <astigsen@tightdb.com>
2013-10-25 14:31:41 -0700

    Small restructuring of example

--- doc/ref/examples/src/com/tightdb/refdoc/TypedQueryIntro.java
+++ doc/ref/examples/src/com/tightdb/refdoc/TypedQueryIntro.java
@@ -22,23 +22,24 @@ public class TypedQueryIntro {
         employees.add("Erik", "Lee", 30000);
         employees.add("Henry", "Anderson", 10000);
 
-        // View definition, will hold the result of our queries
-        EmployeeView view;
-
         // Find all employees with a first name of John.
-        view = employees.firstName.equalTo("John").findAll();
+        EmployeeView view = employees.firstName.equalTo("John").findAll();
 
         // Find the average salary of all employees with the last name Anderson.
         double avgSalary = employees.lastName.equalTo("Anderson").salary.average();
         System.out.println(avgSalary);
 
-        // Find the total salary of people named Jane and Erik.
-        double salary = employees.where().
-                        group().lastName.equalTo("Jane").or().lastName.equalTo("Erik")
-                        .endGroup().salary.sum();
+        // Find the total salary of all people named Jane and Erik.
+        double salary = employees.where()
+                        .group()
+                           .lastName.equalTo("Jane")
+                           .or()
+                           .lastName.equalTo("Erik")
+                        .endGroup()
+                        .salary.sum();
 
         // Find all employees with a last name of Lee and a salary less than 25000.
-        view = employees.lastName.equalTo("Lee").salary.lessThan(25000).findAll();
+        EmployeeView view2 = employees.lastName.equalTo("Lee").salary.lessThan(25000).findAll();
       //@@EndExample@@
     }
 
