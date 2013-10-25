394c3868ba75b9d0e3834eb3a22ea82a1f35ac48
Alexander Stigsen <astigsen@tightdb.com>
2013-10-25 12:42:26 -0700

    Added ref docs for findFirst() and some examples.

--- doc/ref/examples/src/com/tightdb/refdoc/TypedQueryIntro.java
+++ doc/ref/examples/src/com/tightdb/refdoc/TypedQueryIntro.java
@@ -14,9 +14,8 @@ public class TypedQueryIntro {
     public static void main(String[] args) {
      // @@Example: ex_java_typed_query_intro @@
 
+        // Create a table with some data
         EmployeeTable employees = new EmployeeTable();
-
-        // Add data to the table
         employees.add("John", "Lee", 10000);
         employees.add("Jane", "Lee", 15000);
         employees.add("John", "Anderson", 20000);
@@ -41,5 +40,46 @@ public class TypedQueryIntro {
         // Find all employees with a last name of Lee and a salary less than 25000.
         view = employees.lastName.equalTo("Lee").salary.lessThan(25000).findAll();
       //@@EndExample@@
+    }
+
+    public static void findFirstExample() {
+    // @@Example: ex_java_typed_query_find_first @@
+        // Create a new table
+        EmployeeTable employees = new EmployeeTable();
+        employees.add("John", "Lee", 10000);
+        employees.add("Jane", "Lee", 15000);
+        employees.add("John", "Anderson", 20000);
+        employees.add("Erik", "Lee", 30000);
+        employees.add("Henry", "Anderson", 10000);
+
+        // Search for a (non-existent) employee
+        EmployeeRow res1 = employees.where().firstName.equalTo("Susan").findFirst();
+        Assert(res1 == null); // not found
+
+        // Search for an existing employee
+        EmployeeRow res2 = employees.where().firstName.equalTo("Erik").findFirst();
+        Assert(res1.getSalary() == 30000);
+    //@@EndExample@@
+    }
+
+    public static void findAllExample() {
+    // @@Example: ex_java_typed_query_find_all @@
+        // Create a new table
+        EmployeeTable employees = new EmployeeTable();
+        employees.add("John", "Lee", 10000);
+        employees.add("Jane", "Lee", 15000);
+        employees.add("John", "Anderson", 20000);
+        employees.add("Erik", "Lee", 30000);
+        employees.add("Henry", "Anderson", 10000);
+
+        // Search for a all employees with a given salary
+        EmployeeView view = employees.where().salary.greaterThan("15000").findAll();
+        Assert(view.size() == 2);
+
+        // Show all matching employees
+        for (EmployeeRow employee : view) {
+            System.out.println(employee.getFirstName() + " earns " + employee.getSalary());
+        }
+    //@@EndExample@@
     }
 }
