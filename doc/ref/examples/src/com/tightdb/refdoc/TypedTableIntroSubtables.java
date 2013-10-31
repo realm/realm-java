5b094705cdf0482a2707e3ae0e0ef28a2c19c9f3
mekjaer <mk@tightdb.com>
2013-10-31 13:33:54 +0100

    subtable example in type table

--- doc/ref/examples/src/com/tightdb/refdoc/TypedTableIntroSubtables.java
+++ doc/ref/examples/src/com/tightdb/refdoc/TypedTableIntroSubtables.java
@@ -28,153 +28,49 @@ public class TypedTableIntroSubtables {
         //Create table instance from the generated class
         EmployeesTable employeesTable = new EmployeesTable();
 
-        // Add data to table
-        employeesTable.add("John", 20, true, null);
+        // Multiple approaches to add data to subtables
         
-        employeesTable.add("Mary", 21, false, null);
-        employeesTable.add("Lars", 32, true);
-        employeesTable.add("Phil", 43, false);
-        employeesTable.add("Anni", 54, true); 
+        // First approach:
+        // Add a row with values but insert null in the subtables column. An EmployeeRow is returned
+        EmployeesRow row = employeesTable.add("John", 20, true, null);
         
-        employeesTable.get(0).getAge();  
+        // Then retrieve the subtable from  EmployeeRow and add data
+        PhoneNumbersTable phonesTable = row.getPhones();
+        phonesTable.add("mobile", "121-121-121");
+        phonesTable.add("work", "232-232-232");
         
-        // Insert data at row index 2
-        employeesTable.insert(2, "Frank", 34, true);
-
-        if (!employeesTable.isEmpty()) {
-            long s = employeesTable.size(); // s => 6
-        }
-
-        System.out.println("Size = " + employeesTable.size() + "\n");
+        // Second approach:
+        // Insert subtable values as an Object[][]
+        Object[][] phones = new Object[][] {{"mobile", "343-343-343"},
+                                            {"work", "454-454-454"} };
+        
+        employeesTable.add("Mary", 21, false, phones);
+        
+        // Compact version
+        employeesTable.add("Lars", 32, true, new Object[][] {{"mobile", "565-565-565"},
+                                                             {"work", "676-676-676"},
+                                                             {"home", "787-787-787"}});
+        
+        
 
         /****************************** GETTERS AND SETTERS **********************/
 
-        // Get value from row 2 column Name
-        String name = employeesTable.get(2).getName(); // name => "Mary"
+        // Get phone number from row 0 in subtable in row 2 in parent table
+        String number = employeesTable.get(2).getPhones().get(0).getNumber(); // name => "565-565-565"
         
-        // Set the value from in row 2 column Name
-        employeesTable.get(2).setName("NewName"); 
+        // Set the number to new number
+        employeesTable.get(2).getPhones().get(0).setNumber("555-555-555");
 
-        String lastRowName = employeesTable.last().getName();  // retrieve name for last row
+        String lastPhoneDescription = employeesTable.get(2).getPhones().last().getDesc();  // desc => "home"
 
-        // Replace entire row 4 with new values
-        employeesTable.get(4).set("Eric", 50, true);
+        // Replace entire home number with beach house number
+        employeesTable.get(2).getPhones().last().set("beach hourse", "999-999-999");
 
         /****************************** DATA REMOVAL *****************************/
-        employeesTable.remove(2);
-
-        System.out.println("\nRemoved row 2. Down to " + employeesTable.size() + " rows.\n");
-
-        /****************************** ITERATION OF ALL RECORDS *****************/
-
-        // lazy iteration over the table
-
-        for (PeopleRow person : employeesTable) {
-            System.out.println(person.getName() + " is " + person.getAge() + " years old.");
-        }
-
-        /****************************** SIMPLE QUERY *****************************/
-
-        System.out.println("\nFound: ");
-        PeopleRow p = employeesTable.name.equalTo("John").findFirst();
-        System.out.println( p );
-        // prints: "Employee {name=John, age=20, hired=true}"
-
-        /****************************** COMPLEX QUERY ****************************/
-
-        // Define the query
-        PeopleQuery query = employeesTable
-                .age.between(20, 35)    // Implicit AND with below
-                .name.contains("a")     // Implicit AND with below
-                .group()                // "("
-                .hired.equalTo(true)
-                .or()               // or
-                .name.endsWith("y")
-                .endGroup();            // ")"
-        // Count matches
-        PeopleView match = query.findAll();
-        System.out.println(match.size() + " employee(s) match query.");
-
-        // Take the average age of the matches
-        System.out.println(match.age.sum() + " years is the sum of ages.");
-
-        // Perform query and use the result
-        for (PeopleRow person : match) {
-            // ... do something with matching 'person'
-        }
-        System.out.println("");
-
-        /****************************** SERIALIZE ********************************/
-
-        System.out.println("Serialize to file:");
-        new File("people.tightdb").delete(); // overwrites file if it already exists
-
-        // Create Table in Group
-        Group group = new Group();
-        PeopleTable person1 = new PeopleTable(group);
-
-        person1.add("John", 20, true);
-        person1.add("Mary", 21, false);
-
-        // Write to disk
-        try {
-            group.writeToFile("people.tightdb");
-        } catch (IOException e) {
-            // unable to write - handle...
-            System.exit(1);
-        }
-
-        // Load a group from disk (and print contents)
-        Group fromDisk = new Group("people.tightdb");
-        PeopleTable people2 = new PeopleTable(fromDisk);
-
-        for (PeopleRow person : people2) {
-            System.out.println(person.getName() + " is " + person.getAge() + " years old");
-        }
-
-        // Write same group to memory buffer
-        byte[] buffer = group.writeToMem();
-
-        // Load a group from memory (and print contents)
-        Group fromMem = new Group(buffer);
-        PeopleTable people3 = new PeopleTable(fromMem);
-
-        for (PeopleRow person : people3) {
-            System.out.println(person.getName() + " is " + person.getAge() + " years old");
-        }
-
-        /****************************** TRANSACTIONS ********************************/
-
-        System.out.println("\nTransactions:");
-
-        // Open a shared group
-        SharedGroup db = new SharedGroup("people.tightdb");
-
-        // Write transaction:
-        WriteTransaction wrtTrans = db.beginWrite();    // Start transaction
-        try {
-            PeopleTable person = new PeopleTable(wrtTrans);
-            // Add row to table
-            person.add("Bill", 53, true);
-            wrtTrans.commit();                          // End transaction
-        } catch (Throwable e) {
-            wrtTrans.rollback();                        // or Rollback
-        }
-
-        // Read transaction:
-        ReadTransaction rdTrans = db.beginRead();       // Start transaction
-        try{
-            PeopleTable people = new PeopleTable(rdTrans);
-            for (PeopleRow person2 : people) {
-                System.out.println(person2.getName() + " is " +
-                        person2.getAge() + " years old");
-            }
-        } finally {
-            rdTrans.endRead();                          // End transaction 
-        }
         
+        // Remove beach house row 
+        employeesTable.get(2).getPhones().removeLast();        
         // @@EndShow@@
-
     } 
 }
 //@@EndExample@@
