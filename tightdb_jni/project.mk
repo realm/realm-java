6fc54a9d279c51e93e826a2f0f50f0de373d0fb0
Kristian Spangsege <kristian.spangsege@gmail.com>
2014-03-18 00:42:40 +0100

    Upgrade `generic.mk` to latest version from core repo

--- tightdb_jni/project.mk
+++ tightdb_jni/project.mk
@@ -2,26 +2,35 @@ ENABLE_INSTALL_DEBUG_LIBS = 1
 
 # Construct fat binaries on Darwin when using Clang
 ifneq ($(TIGHTDB_ENABLE_FAT_BINARIES),)
-ifneq ($(call CC_CXX_AND_LD_ARE,clang),)
-ifeq ($(OS),Darwin)
-CFLAGS_ARCH  += -arch i386 -arch x86_64
-endif
-endif
+  ifeq ($(OS),Darwin)
+    ifeq ($(COMPILER_IS),clang)
+      CFLAGS_ARCH += -arch i386 -arch x86_64
+    endif
+  endif
 endif
 
 ifeq ($(OS),Darwin)
-CFLAGS_ARCH += -mmacosx-version-min=10.7
+  CFLAGS_ARCH += -mmacosx-version-min=10.7
 endif
 
 # FIXME: '-fno-elide-constructors' currently causes TightDB to fail
-#CFLAGS_DEBUG   += -fno-elide-constructors
-CFLAGS_PTHREAD += -pthread
+#CFLAGS_DEBUG += -fno-elide-constructors
+CFLAGS_PTHREADS += -pthread
 CFLAGS_GENERAL += -Wextra -ansi -pedantic -Wno-long-long
+
+# Avoid a warning from Clang when linking on OS X. By default,
+# `LDFLAGS_PTHREADS` inherits its value from `CFLAGS_PTHREADS`, so we
+# have to override that with an empty value.
+ifeq ($(OS),Darwin)
+  ifeq ($(LD_IS),clang)
+    LDFLAGS_PTHREADS = $(EMPTY)
+  endif
+endif
 
 # Load dynamic configuration
 ifeq ($(NO_CONFIG_MK),)
-CONFIG_MK = $(GENERIC_MK_DIR)/config.mk
-DEP_MAKEFILES += $(CONFIG_MK)
-include $(CONFIG_MK)
-LIB_SUFFIX_SHARED = $(JNI_SUFFIX)
+  CONFIG_MK = $(GENERIC_MK_DIR)/config.mk
+  DEP_MAKEFILES += $(CONFIG_MK)
+  include $(CONFIG_MK)
+  LIB_SUFFIX_SHARED = $(JNI_SUFFIX)
 endif
