74a37e360d471cd9bef017e7950fd0579f8275e1
Rasmus Andersen <hovoere@gmail.com>
2014-05-08 12:47:26 +0200

    example app

---
+++ RealmAndroid/lib/src/main/java/io/realm/example/MainActivity.java
@@ -0,0 +1,57 @@
+package io.realm.example;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+
+import io.realm.example.entities.User;
+import io.realm.testApp.R;
+import io.realm.typed.Realm;
+import io.realm.typed.Realms;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize  the Realm
+        Realm<User> users = Realms.newList(this, User.class);
+
+
+        // Setup the ListView
+        ListView listView = (ListView)findViewById(R.id.listView);
+        ListAdapter adapter = new ArrayAdapter<User>(this, R.id.textView, users);
+
+        listView.setAdapter(adapter);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
---
+++ RealmAndroid/lib/src/main/java/io/realm/example/entities/User.java
@@ -0,0 +1,33 @@
+package io.realm.example.entities;
+
+
+public class User {
+
+    private int id;
+    private String name;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
