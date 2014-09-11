9f8ba4da6c8363a23b44dc3964ae12094c643d32
Kenneth Geisshirt <kg@realm.io>
2014-09-11 14:44:16 +0200

    Distribute to example too

---
+++ distribution/RealmIntroExample/app/src/main/java/io/realm/realmintroexample/Person.java
@@ -0,0 +1,28 @@
+package io.realm.realmintroexample;
+
+import java.lang.String;
+
+import io.realm.RealmObject;
+import io.realm.annotations.RealmClass;
+
+@RealmClass
+public class Person extends RealmObject {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
