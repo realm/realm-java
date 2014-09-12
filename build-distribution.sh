3377a28cc2d00a8f9b81274f83f4a21c0af66ce9
Emanuele Zattin <ez@realm.io>
2014-09-12 16:27:27 +0200

    Fix version numbers in build scripts

--- build-distribution.sh
+++ build-distribution.sh
@@ -10,21 +10,21 @@ echo "Building the Javadocs"
 ./gradlew javadocReleaseJar
 
 echo "Copying files to the distribution folder"
-cp realm-annotations-processor/build/libs/realm-annotations-processor-0.80.jar distribution
-cp realm/build/outputs/aar/realm-0.80.aar distribution/realm
+cp realm-annotations-processor/build/libs/realm-annotations-processor-0.1.0.jar distribution
+cp realm/build/outputs/aar/realm-0.1.0.aar distribution/realm
 cp -R realm/build/docs/javadoc distribution
-cp realm/build/libs/realm-0.80-javadoc.jar distribution
+cp realm/build/libs/realm-0.1.0-javadoc.jar distribution
 
 
 echo "Copying files to the distribution/RealmBasicExample folder"
 mkdir -p distribution/RealmBasicExample/app/libs
-cp realm-annotations-processor/build/libs/realm-annotations-processor-0.80.jar distribution/RealmBasicExample/app/libs
+cp realm-annotations-processor/build/libs/realm-annotations-processor-0.1.0.jar distribution/RealmBasicExample/app/libs
 mkdir -p distribution/RealmBasicExample/realm
 cp -R distribution/realm distribution/RealmBasicExample
 
 echo "Copying files to the distribution/RealmGridViewExample folder"
 mkdir -p distribution/RealmGridViewExample/app/libs
-cp realm-annotations-processor/build/libs/realm-annotations-processor-0.80.jar distribution/RealmGridViewExample/app/libs
+cp realm-annotations-processor/build/libs/realm-annotations-processor-0.1.0.jar distribution/RealmGridViewExample/app/libs
 mkdir -p distribution/RealmGridViewExample/realm
 cp -R distribution/realm distribution/RealmGridViewExample
 
