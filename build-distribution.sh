f7bf4a0b95db6c51e6e04cb6c8d2cfed047fb62a
Emanuele Zattin <ez@realm.io>
2014-09-12 15:12:49 +0200

    Fix build-distribution file

--- build-distribution.sh
+++ build-distribution.sh
@@ -16,11 +16,11 @@ cp -R realm/build/docs/javadoc distribution
 cp realm/build/libs/realm-0.80-javadoc.jar distribution
 
 
-echo "Copying files to the distribution/RealmIntroExample folder"
+echo "Copying files to the distribution/RealmBasicExample folder"
 mkdir -p distribution/RealmBasicExample/app/libs
 cp realm-annotations-processor/build/libs/realm-annotations-processor-0.80.jar distribution/RealmBasicExample/app/libs
 mkdir -p distribution/RealmBasicExample/realm
-cp -R distribution/realm distribution/RealmIntroExample
+cp -R distribution/realm distribution/RealmBasicExample
 
 echo "Copying files to the distribution/RealmGridViewExample folder"
 mkdir -p distribution/RealmGridViewExample/app/libs
