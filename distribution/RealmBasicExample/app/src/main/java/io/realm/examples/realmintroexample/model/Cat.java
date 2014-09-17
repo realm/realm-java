f7d422aaabeba709ab5c764bd44613c2dce0f6ac
Emanuele Zattin <ez@realm.io>
2014-09-17 14:42:31 +0200

    Remove annotation from Cat class (introduced by git merge)

--- distribution/RealmBasicExample/app/src/main/java/io/realm/examples/realmintroexample/model/Cat.java
+++ distribution/RealmBasicExample/app/src/main/java/io/realm/examples/realmintroexample/model/Cat.java
@@ -1,9 +1,7 @@
 package io.realm.examples.realmintroexample.model;
 
 import io.realm.RealmObject;
-import io.realm.annotations.RealmClass;
 
-@RealmClass
 public class Cat extends RealmObject {
     private String name;
 
