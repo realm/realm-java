b04c9dcd249b88767c29e53b6be433a47cbbb992
Emanuele Zattin <ez@realm.io>
2014-10-21 10:11:28 -0700

    Postpone the methods checking in the annotation processor
    Eclipse’s custom annotations processor does not honour the order of fields and methods as in the source code.

--- realm-annotations-processor/src/main/java/io/realm/processor/RealmProcessor.java
+++ realm-annotations-processor/src/main/java/io/realm/processor/RealmProcessor.java
@@ -52,6 +52,7 @@ public class RealmProcessor extends AbstractProcessor {
             List<String> ignoredFields = new ArrayList<String>();
             List<String> expectedGetters = new ArrayList<String>();
             List<String> expectedSetters = new ArrayList<String>();
+            List<ExecutableElement> methods = new ArrayList<ExecutableElement>();
 
             // Check the annotation was applied to a Class
             if (!classElement.getKind().equals(ElementKind.CLASS)) {
@@ -100,7 +101,7 @@ public class RealmProcessor extends AbstractProcessor {
                         if (elementTypeCanonicalName.equals("java.lang.String")) {
                             indexedFields.add(variableElement);
                         } else {
-                            error("@Index is only appliable to String fields - got " + element);
+                            error("@Index is only applicable to String fields - got " + element);
                             return true;
                         }
                     }
@@ -114,44 +115,47 @@ public class RealmProcessor extends AbstractProcessor {
                     expectedSetters.add(fieldName);
                 } else if (elementKind.equals(ElementKind.METHOD)) {
                     ExecutableElement executableElement = (ExecutableElement) element;
+                    methods.add(executableElement);
+                }
+            }
 
-                    if (!executableElement.getModifiers().contains(Modifier.PUBLIC)) {
-                        error("The methods of the model must be public", executableElement);
-                    }
+            for (ExecutableElement executableElement : methods) {
+                if (!executableElement.getModifiers().contains(Modifier.PUBLIC)) {
+                    error("The methods of the model must be public", executableElement);
+                }
 
-                    String methodName = executableElement.getSimpleName().toString();
-                    String computedFieldName = methodName.startsWith("is")?lowerFirstChar(methodName.substring(2)):lowerFirstChar(methodName.substring(3));
-                    if (methodName.startsWith("get") || methodName.startsWith("is")) {
-                        boolean found = false;
-                        for (VariableElement field : fields) {
-                            if (field.getSimpleName().toString().equals(computedFieldName)) {
-                                found = true;
-                            }
-                        }
-                        if (ignoredFields.contains(computedFieldName)) {
+                String methodName = executableElement.getSimpleName().toString();
+                String computedFieldName = methodName.startsWith("is")?lowerFirstChar(methodName.substring(2)):lowerFirstChar(methodName.substring(3));
+                if (methodName.startsWith("get") || methodName.startsWith("is")) {
+                    boolean found = false;
+                    for (VariableElement field : fields) {
+                        if (field.getSimpleName().toString().equals(computedFieldName)) {
                             found = true;
                         }
-                        if (!found) {
-                            error(String.format("No field named %s for the getter %s", computedFieldName, methodName), executableElement);
-                        }
-                        expectedGetters.remove(computedFieldName);
-                    } else if (methodName.startsWith("set")) {
-                        boolean found = false;
-                        for (VariableElement field : fields) {
-                            if (field.getSimpleName().toString().equals(computedFieldName)) {
-                                found = true;
-                            }
-                        }
-                        if (ignoredFields.contains(computedFieldName)) {
+                    }
+                    if (ignoredFields.contains(computedFieldName)) {
+                        found = true;
+                    }
+                    if (!found) {
+                        error(String.format("No field named %s for the getter %s", computedFieldName, methodName), executableElement);
+                    }
+                    expectedGetters.remove(computedFieldName);
+                } else if (methodName.startsWith("set")) {
+                    boolean found = false;
+                    for (VariableElement field : fields) {
+                        if (field.getSimpleName().toString().equals(computedFieldName)) {
                             found = true;
                         }
-                        if (!found) {
-                            error(String.format("No field named %s for the setter %s", computedFieldName, methodName), executableElement);
-                        }
-                        expectedSetters.remove(computedFieldName);
-                    } else {
-                        error("Only getters and setters should be defined in model classes", executableElement);
                     }
+                    if (ignoredFields.contains(computedFieldName)) {
+                        found = true;
+                    }
+                    if (!found) {
+                        error(String.format("No field named %s for the setter %s", computedFieldName, methodName), executableElement);
+                    }
+                    expectedSetters.remove(computedFieldName);
+                } else {
+                    error("Only getters and setters should be defined in model classes", executableElement);
                 }
             }
 
