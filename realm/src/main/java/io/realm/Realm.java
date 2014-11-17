b191685bf02c0e5c5f79fc724e42edb721c146d4
Christian Melchior <christian@ilios.dk>
2014-11-17 13:44:50 +0100

    Fixed Realm cache not working.

--- realm/src/main/java/io/realm/Realm.java
+++ realm/src/main/java/io/realm/Realm.java
@@ -365,7 +365,7 @@ public class Realm {
 
     private static Realm createAndValidate(String absolutePath, byte[] key, boolean validateSchema, boolean autoRefresh) {
         Map<Integer, Realm> realms = realmsCache.get();
-        Realm realm = realms.get(absolutePath);
+        Realm realm = realms.get(absolutePath.hashCode());
 
         if (realm != null) {
             return realm;
