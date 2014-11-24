9cd2c7e8dcc6ff04d46a1ee862c64154545ccabc
Christian Melchior <christian@ilios.dk>
2014-11-24 14:19:08 +0100

    Fixed annotation-processor unit tests.

--- realm-annotations-processor/src/test/resources/io/realm/BooleansRealmProxy.java
+++ realm-annotations-processor/src/test/resources/io/realm/BooleansRealmProxy.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2014 Realm Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.realm;
 
 import io.realm.Realm;
@@ -116,23 +100,29 @@ public class BooleansRealmProxy extends Booleans {
         StringBuilder stringBuilder = new StringBuilder("Booleans = [");
         stringBuilder.append("{done:");
         stringBuilder.append(isDone());
-        stringBuilder.append("} ");
+        stringBuilder.append("}");
+        stringBuilder.append(",");
         stringBuilder.append("{isReady:");
         stringBuilder.append(isReady());
-        stringBuilder.append("} ");
+        stringBuilder.append("}");
+        stringBuilder.append(",");
         stringBuilder.append("{mCompleted:");
         stringBuilder.append(ismCompleted());
-        stringBuilder.append("} ");
+        stringBuilder.append("}");
         stringBuilder.append("]");
         return stringBuilder.toString();
     }
 
     @Override
     public int hashCode() {
+        String realmName = realm.getPath();
+        String tableName = row.getTable().getName();
+        long rowIndex = row.getIndex();
+
         int result = 17;
-        result = 31 * result + (isDone() ? 1 : 0);
-        result = 31 * result + (isReady() ? 1 : 0);
-        result = 31 * result + (ismCompleted() ? 1 : 0);
+        result = 31 * result + ((realmName != null) ? realmName.hashCode() : 0);
+        result = 31 * result + ((tableName != null) ? tableName.hashCode() : 0);
+        result = 31 * result + (int) (rowIndex ^ (rowIndex >>> 32));
         return result;
     }
 
@@ -141,9 +131,15 @@ public class BooleansRealmProxy extends Booleans {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         BooleansRealmProxy aBooleans = (BooleansRealmProxy)o;
-        if (isDone() != aBooleans.isDone()) return false;
-        if (isReady() != aBooleans.isReady()) return false;
-        if (ismCompleted() != aBooleans.ismCompleted()) return false;
+
+        String path = realm.getPath();
+        String otherPath = aBooleans.realm.getPath();
+        if (path != null ? !path.equals(otherPath) : otherPath != null) return false;;
+
+        String tableName = row.getTable().getName();
+        String otherTableName = aBooleans.row.getTable().getName();
+        if (tableName != null ? !tableName.equals(otherTableName) : otherTableName != null) return false;
+
         return true;
     }
 
