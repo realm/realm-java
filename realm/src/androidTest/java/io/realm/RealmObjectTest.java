0bf7c6f08e5a278c098154df4a0d565b359970cb
Christian Melchior <christian@ilios.dk>
2014-12-11 20:25:42 +0100

    Fixed bug in RealmObject.equals()

--- realm/src/androidTest/java/io/realm/RealmObjectTest.java
+++ realm/src/androidTest/java/io/realm/RealmObjectTest.java
@@ -241,7 +241,7 @@ public class RealmObjectTest extends AndroidTestCase {
         assertTrue(methodWrongThread(false));
     }
 
-    public void testEquals() {
+    public void testEqualsSameRealmObject() {
         testRealm.beginTransaction();
         CyclicType ct = testRealm.createObject(CyclicType.class);
         ct.setName("Foo");
@@ -252,6 +252,18 @@ public class RealmObjectTest extends AndroidTestCase {
 
         assertTrue(ct1.equals(ct1));
         assertTrue(ct2.equals(ct2));
+    }
+
+    public void testEqualsDifferentRealmObjects() {
+        testRealm.beginTransaction();
+        CyclicType objA = testRealm.createObject(CyclicType.class);
+        objA.setName("Foo");
+        CyclicType objB = testRealm.createObject(CyclicType.class);
+        objB.setName("Bar");
+        testRealm.commitTransaction();
+
+        assertFalse(objA.equals(objB));
+        assertFalse(objB.equals(objA));
     }
 
     public void testEqualsAfterModification() {
