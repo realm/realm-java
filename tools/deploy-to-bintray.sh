ef5e32e6358f16c1a33b91bde8b49e8efafced78
Emanuele Zattin <ez@realm.io>
2014-12-12 12:05:54 +0100

    Add more checks

--- tools/deploy-to-bintray.sh
+++ tools/deploy-to-bintray.sh
@@ -15,12 +15,20 @@ fi
 version="$1"
 
 if ! [[ $version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-	echo "The provided version is not in a valid format"
+	echo "The provided version is not in a valid format. eg 0.74.1"
 	exit 1
 fi
 
-if [[ ${version.txt:0:${#version}} == $version ]]; then
-	echo "The provided version argument does not match the beginning of the version.txt file"
+version_file_contents=$(<version.txt)
+if [[ $version_file_contents != $version ]]; then
+	echo "The provided version argument does not match the contents of the version.txt file"
+	exit 1
+fi
+
+length=${#version}
+beginning=$(head -c ${length} changelog.txt)
+if [[ $beginning != $version ]]; then
+	echo "The provided version argument does not match the beginning of the changelog.txt file"
 	exit 1
 fi
 
@@ -30,4 +38,4 @@ if ! grep -q "$version" "$version_checker_file"; then
 	echo "The version checker file does not contain the provided version"
 fi
 
-./gradlew realm:bintrayUpload
+#./gradlew realm:bintrayUpload
