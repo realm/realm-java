29ab93bf7aeaa5cd048cbf468a228046496ecb13
Christian Melchior <christian@ilios.dk>
2014-12-18 12:36:33 +0100

    Cleanup Json examples. Updated changelog.

---
+++ distribution/RealmAdapterExample/app/src/main/java/io/realm/examples/realmadapters/MyAdapter.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.realm.examples.realmadapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import io.realm.RealmBaseAdapter;
+import io.realm.RealmResults;
+import io.realm.examples.realmadapters.models.TimeStamp;
+
+public class MyAdapter extends RealmBaseAdapter<TimeStamp> implements ListAdapter {
+
+    private static class ViewHolder {
+        TextView timestamp;
+    }
+
+    public MyAdapter(Context context, int resId, RealmResults<TimeStamp> realmResults, boolean automaticUpdate) {
+        super(context, realmResults, automaticUpdate);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.timestamp = (TextView) convertView.findViewById(android.R.id.text1);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        TimeStamp item = realmResults.get(position);
+        viewHolder.timestamp.setText(item.getTimeStamp());
+        return convertView;
+    }
+
+    public RealmResults<TimeStamp> getRealmResults() {
+        return realmResults;
+    }
+}
---
+++ distribution/RealmJsonExample/app/src/main/java/io/realm/examples/json/RealmJsonExampleActivity.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2014 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.realm.examples.json;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.realm.Realm;
+
+public class RealmJsonExampleActivity extends Activity {
+
+    public static final String TAG = RealmJsonExampleActivity.class.getName();
+
+    private GridView mGridView;
+    private CityAdapter mAdapter;
+    private Realm realm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_realm_example);
+
+        Realm.deleteRealmFile(this);
+        realm = Realm.getInstance(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Load from file "cities.json" first time
+        if(mAdapter == null) {
+            List<City> cities = loadCities();
+
+            //This is the GridView adapter
+            mAdapter = new CityAdapter(this);
+            mAdapter.setData(cities);
+
+            //This is the GridView which will display the list of cities
+            mGridView = (GridView) findViewById(R.id.cities_list);
+            mGridView.setAdapter(mAdapter);
+            mAdapter.notifyDataSetChanged();
+            mGridView.invalidate();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realm.close();
+    }
+
+    public List<City> loadCities() {
+
+        loadJsonFromStream();
+        loadJsonFromJsonObject();
+        loadJsonFromString();
+
+        return realm.allObjects(City.class);
+    }
+
+    private void loadJsonFromStream() {
+        // Use streams if you are worried about the size of the JSON whether it was persisted on disk
+        // or received from the network.
+        InputStream stream = null;
+        try {
+            stream = getAssets().open("cities.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Store the retrieved items to the Realm
+        Realm realm = Realm.getInstance(this);
+
+        // Open a transaction to store items into the realm
+        realm.beginTransaction();
+        try {
+            realm.createAllFromJson(City.class, stream);
+            realm.commitTransaction();
+        } catch (IOException e) {
+            realm.cancelTransaction();
+        } finally {
+            try {
+                stream.close();
+            } catch (IOException ignore) {
+                // Ignore
+            }
+        }
+    }
+
+    private void loadJsonFromJsonObject() {
+        Map<String, String> city = new HashMap<String, String>();
+        city.put("name", "København");
+        city.put("votes", "9");
+        JSONObject json = new JSONObject(city);
+
+        realm.beginTransaction();
+        realm.createObjectFromJson(City.class, json);
+        realm.commitTransaction();
+    }
+
+    private void loadJsonFromString() {
+        String json = "{ name: \"Aarhus\", votes: 99 }";
+
+        realm.beginTransaction();
+        realm.createObjectFromJson(City.class, json);
+        realm.commitTransaction();
+    }
+}
