7443b1634827a127673dec66cb40f03fb78fef2b
Christian Melchior <christian@ilios.dk>
2015-01-12 12:11:06 +0100

    Return type for copyToRealm changed to List for collections.

--- realm/src/main/java/io/realm/Realm.java
+++ realm/src/main/java/io/realm/Realm.java
@@ -961,12 +961,12 @@ public final class Realm implements Closeable {
      * reflected in the Realm copies.
      *
      * @param objects RealmObjects to copy to the Realm.
-     * @return A collection of the the converted RealmObjects that all has their properties managed by the Realm.
+     * @return A list of the the converted RealmObjects that all has their properties managed by the Realm.
      *
      * @throws io.realm.exceptions.RealmException if any of the objects has already been added to Realm.
      * @throws java.lang.IllegalArgumentException if any of the elements in the input collection is {@code null}.
      */
-    public <E extends RealmObject> Collection<E> copyToRealm(Iterable<E> objects) {
+    public <E extends RealmObject> List<E> copyToRealm(Iterable<E> objects) {
         if (objects == null) new ArrayList<E>();
 
         ArrayList<E> realmObjects = new ArrayList<E>();
