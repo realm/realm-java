af3a3161eb240bf5470890480d9e3c9dc3661df3
Christian Melchior <christian@ilios.dk>
2015-01-12 09:22:52 +0100

    Updated JavaDoc.

--- realm/src/main/java/io/realm/Realm.java
+++ realm/src/main/java/io/realm/Realm.java
@@ -1080,9 +1080,9 @@ public final class Realm implements Closeable {
     /**
      * Executes a given transaction on the Realm. {@link #beginTransaction()} and
      * {@link #commitTransaction()} will be called automatically. If any exception is thrown
-     * during the transaction {@link #cancelTransaction()} will be called instead.
+     * during the transaction {@link #cancelTransaction()} will be called instead of {@link #commitTransaction()}.
      *
-     * @param transaction Transaction to execute.
+     * @param transaction {@link io.realm.Realm.Transaction} to execute.
      * @throws RealmException if any error happened during the transaction.
      */
     public void executeTransaction(Transaction transaction) {
@@ -1260,7 +1260,8 @@ public final class Realm implements Closeable {
      * Encapsulates a Realm transaction.
      *
      * Using this class will automatically handle {@link #beginTransaction()} and {@link #commitTransaction()}
-     * If any exception is thrown while inside the transaction {@link #cancelTransaction()} will be called instead.
+     * If any exception is thrown during the transaction {@link #cancelTransaction()} will be called
+     * instead of {@link #commitTransaction()}.
      */
     public interface Transaction {
         public void execute(Realm realm);
