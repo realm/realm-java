3f57071935d1fcaabdc3f1e85a030dffc1335c59
Christian Melchior <christian@ilios.dk>
2015-02-18 14:56:36 +0100

    Annotation processor unit tests added.

--- realm-annotations-processor/src/test/resources/io/realm/BooleansRealmProxy.java
+++ realm-annotations-processor/src/test/resources/io/realm/BooleansRealmProxy.java
@@ -141,7 +141,7 @@ public class BooleansRealmProxy extends Booleans {
     }
 
     public static Booleans copyOrUpdate(Realm realm, Booleans object, boolean update) {
-        return copy(realm, object, false);
+        return copy(realm, object, update);
     }
 
     public static Booleans copy(Realm realm, Booleans newObject, boolean update) {
