83f6fe04cfcb08911cf26cb2c5ddb65e820a4452
Christian Melchior <christian@ilios.dk>
2015-03-09 10:48:11 +0100

    Added description to annotation processor + additional error check

--- realm-annotations-processor/src/main/java/io/realm/processor/RealmProcessor.java
+++ realm-annotations-processor/src/main/java/io/realm/processor/RealmProcessor.java
@@ -37,7 +37,67 @@ import java.util.*;
 
 
 /**
- * TODO Description of what the processor do and what classes it generates and for what reason
+ * The RealmProcessor is responsible for creating the plumbing that connects the RealmObjects to a Realm. The process
+ * for doing so summarized below and then described in more detail:
+ *
+ * SUMMARY:
+ * 1 ) Create proxy classes for all classes marked with @RealmClass. They are named <modelClass>RealmProxy.java
+ * 2 ) Create a RealmProxyMediator that can map between model classes and static helper methods in the proxy classes.
+ *     It is named <UUID>.java
+ * 3 ) Create a binder class for all classes marked with @RealmModule. They are named <moduleName>Binder.java
+ * 4 ) Create DefaultRealmModule if no custom library module is defined.
+ * 5 ) Create DefaultRealmModuleBinder if needed.
+ *
+ *
+ * WHY:
+ * *
+ * 1) A RealmObjectProxy object is created for each class annotated with {@link io.realm.annotations.RealmClass}. This
+ * proxy extends the original model class and rewires all field access to point to the native Realm data instead of Java
+ * objects. It also adds a lot of static helper methods to the class.
+ *
+ * 2) A RealmProxyMediator class is generated. This class has an interface that matches the static helper methods for
+ * the proxy classes. All access to to these static helper methods should be done through this RealmProxyMediator.
+ *
+ * This is done as Realm otherwise would have no other option than using reflection for accessing the static methods
+ * as they are not known until after the annotation processor has run. It also allows ProGuard to obfuscate all model
+ * classes as all access to the methods happen through the RealmProxyMediator.
+ *
+ * To allow Realm to be used in both library projects and app code, the RealmProxyMediator will be given a random name,
+ * a UUID.
+ *
+ * 3) For each class annotated with @RealmModule a matching Binder class is created. The purpose of the Binder class
+ * is to add a reference to the created RealmProxyMediator. This is done by extending the original module class and
+ * overriding a getter method. All modules should extend the class RealmClassCollection.
+ *
+ * 4) The annotation processor is is either in "library" mode or in "app" mode. This is defined by having a class
+ * annotated with @RealmModule(library = true). Modules with library = true and library = false cannot be mixed, and
+ * will throw an error. If no library modules are defined, we will create a DefaultRealmModule containing all known
+ * RealmObjects. Realm automatically knows about this module.
+ *
+ * 5) Just like in step 3, a Binder class is created for the Default module if needed.
+ *
+ *
+ * This means the workflow when instantiating a Realm on runtime is the following:
+ *
+ * 1) Create a Realm.
+ *
+ * 2) It is assigned one or more modules. If no module is assigned, the default module is used.
+ *
+ * 3) From each module Realm retrieves the available classes and the RealmProxyMediator associated with these classes.
+ *
+ * 4) Each time a static helper method is needed, the Realm can now delegate these method calls to the appropriate
+ *    RealmProxyMediator which in turn will delegate the method call to the appropriate RealmObjectProxy class.
+ *
+ *
+ * DESIGN GOALS:
+ * While the above process might seem overly complicated it is necessary if we want to support the following design
+ * goals:
+ *
+ * - Minimize reflection.
+ * - As much as possible can be obfuscated.
+ * - Library projects must be able to use Realm without interfering with app code.
+ * - App code must be able to use model classes provided by library code.
+ * - It should work for app developers out of the box (ie. put the burden on the library developer)
  */
 @SupportedAnnotationTypes({
         "io.realm.annotations.RealmClass",
@@ -326,6 +386,13 @@ public class RealmProcessor extends AbstractProcessor {
         for (Element classElement : roundEnv.getElementsAnnotatedWith(RealmModule.class)) {
             if (!classElement.getKind().equals(ElementKind.CLASS)) {
                 error("The RealmModule annotation can only be applied to classes", classElement);
+            }
+
+            // Check that the class extends RealmClassCollection
+            TypeElement typeElement = (TypeElement) classElement;
+            TypeElement parentElement = (TypeElement) processingEnv.getTypeUtils().asElement(typeElement.getSuperclass());
+            if (!parentElement.toString().endsWith(".RealmCollectionClass")) {
+                error("A RealmModule annotated object must be derived from RealmCollectionClass", classElement);
             }
 
             RealmModule module = classElement.getAnnotation(RealmModule.class);
