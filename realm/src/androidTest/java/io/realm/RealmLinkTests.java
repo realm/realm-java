f107e34c220486d63d2398db4b55adb14dec7fdf
Kenneth Geisshirt <kg@realm.io>
2015-03-20 16:37:04 -0700

    Adding unit tests where links are null

--- realm/src/androidTest/java/io/realm/RealmLinkTests.java
+++ realm/src/androidTest/java/io/realm/RealmLinkTests.java
@@ -470,6 +470,18 @@ public class RealmLinkTests extends AndroidTestCase {
 
         RealmResults<Owner> owners2 = testRealm.where(Owner.class).isNull("cat").findAll();
         assertEquals(0, owners2.size());
+
+        // remove cat and dogs and query again
+        testRealm.beginTransaction();
+        testRealm.clear(Dog.class);
+        testRealm.clear(Cat.class);
+        testRealm.commitTransaction();
+
+        RealmResults<Owner> owners3 = testRealm.where(Owner.class).isNull("dogs").findAll();
+        assertEquals(1, owners3.size());
+
+        RealmResults<Owner> owners4 = testRealm.where(Owner.class).isNull("cat").findAll();
+        assertEquals(1, owners4.size());
     }
 
     public void testIsNotNull() {
@@ -478,6 +490,18 @@ public class RealmLinkTests extends AndroidTestCase {
 
         RealmResults<Owner> owners2 = testRealm.where(Owner.class).isNotNull("cat").findAll();
         assertEquals(1, owners2.size());
+
+        // remove cat and dogs and query again
+        testRealm.beginTransaction();
+        testRealm.clear(Dog.class);
+        testRealm.clear(Cat.class);
+        testRealm.commitTransaction();
+
+        RealmResults<Owner> owners3 = testRealm.where(Owner.class).isNotNull("dogs").findAll();
+        assertEquals(0, owners3.size());
+
+        RealmResults<Owner> owners4 = testRealm.where(Owner.class).isNotNull("cat").findAll();
+        assertEquals(0, owners4.size());
     }
 
     public void testIsNullWrongType() {
