801709fb06edbac3802540a43ec17b1141e76ddc
Kirill Rakhman <kirill.rakhman@gmail.com>
2015-05-31 13:29:35 +0200

    add explicit retention and target annotations to Ignore and Index annotations

--- realm-annotations/src/main/java/io/realm/annotations/Ignore.java
+++ realm-annotations/src/main/java/io/realm/annotations/Ignore.java
@@ -16,6 +16,13 @@
 
 package io.realm.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.FIELD)
 public @interface Ignore {
 
 }
