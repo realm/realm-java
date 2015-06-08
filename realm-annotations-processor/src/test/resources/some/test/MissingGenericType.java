e4a262f6298df6d115dfb4cbec06a931dacc89a6
Kenneth Geisshirt <kg@realm.io>
2015-06-08 12:55:54 +0200

    Add check on fields of type RealmList

--- realm-annotations-processor/src/test/resources/some/test/MissingGenericType.java
+++ realm-annotations-processor/src/test/resources/some/test/MissingGenericType.java
@@ -14,22 +14,19 @@
  * limitations under the License.
  */
 
-package io.realm.entities;
+package some.test;
 
-import java.util.Date;
-
-import io.realm.RealmList;
 import io.realm.RealmObject;
-import some.test.AllTypes;
+import io.realm.RealmList;
 
-public class NoAccessors extends RealmObject {
-    private String columnString;
-    private long columnLong;
-    private float columnFloat;
-    private double columnDouble;
-    private boolean columnBoolean;
-    private Date columnDate;
-    private byte[] columnBinary;
-    private AllTypes columnRealmObject;
-    private RealmList<AllTypes> columnRealmList;
+class MissingGenericType extends RealmObject {
+    private RealmList list; // this is an error!
+
+    public RealmList getList() {
+        return list;
+    }
+
+    public void setList(RealmList list) {
+        this.list = list;
+    }
 }
