3e2f20f45b61accdc4b12a196d807c6aee353c00
Nabil Hachicha <nabil.hachicha@gmail.com>
2015-06-25 17:59:39 +0200

    add missing Copyright

--- realm/src/main/java/io/realm/internal/async/RetryPolicyFactory.java
+++ realm/src/main/java/io/realm/internal/async/RetryPolicyFactory.java
@@ -1,9 +1,32 @@
+/*
+ * Copyright 2015 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.realm.internal.async;
 
 /**
- * Created by Nabil on 17/06/15.
+ * Build a {@link RetryPolicy}
  */
 public class RetryPolicyFactory {
+    /**
+     * Return the appropriate {@link RetryPolicy}
+     * @param mode one of the supported mode in {@link RetryPolicy}
+     * @param maxNumberOfRetries how many times we should retry before giving up
+     *                           (not applicable for {@link RetryPolicy#MODE_INDEFINITELY})
+     * @return new instance of {@code RetryPolicy}
+     */
     public static RetryPolicy get(int mode, int maxNumberOfRetries) {
         switch (mode) {
             case RetryPolicy.MODE_NO_RETRY: {
