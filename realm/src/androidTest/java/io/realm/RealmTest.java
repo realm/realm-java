5b96a3709501dfd8b30a0b9c11bb75fd7440acde
Emanuele Zattin <ez@realm.io>
2015-07-24 09:12:27 +0200

    Better error message and make test smaller and faster

--- realm/src/androidTest/java/io/realm/RealmTest.java
+++ realm/src/androidTest/java/io/realm/RealmTest.java
@@ -1704,7 +1704,7 @@ public class RealmTest extends AndroidTestCase {
 
         // insert some rows, then give the thread some time to cleanup
         // we have 8 reference so far let's add more
-        final int numberOfPopulateTest = 10000;
+        final int numberOfPopulateTest = 1000;
         final int totalNumberOfReferences = 8 + 20 * 2 * numberOfPopulateTest;
 
         long tic = System.currentTimeMillis();
@@ -1725,7 +1725,7 @@ public class RealmTest extends AndroidTestCase {
         // we can't guarantee that all references have been GC'd but we should detect a decrease
         boolean isDecreasing = rowReferences.size() < totalNumberOfReferences;
         if (!isDecreasing) {
-            fail("FinalizerRunnable is not closing all native resources");
+            fail("Native resources are not being closed");
 
         } else {
             android.util.Log.d(RealmTest.class.getName(), "References freed : "
