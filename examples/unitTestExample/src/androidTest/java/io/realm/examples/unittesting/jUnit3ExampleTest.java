8b1909656c9cf4bc377ca490407e288a20857e09
Donn Felker <donn@donnfelker.com>
2015-11-24 13:39:33 -0500

    Adding unit test example project with PowerMock, mockito, robolectric, and Junit4
    Adding more indepth Robolectric tests with Realm mocking.
    
    Clean up and renaming.
    
    Comments
    
    Creating final tests to click on button and verify state and behavior.
    
    Adding readme and image.
    
    Removing comment.
    
    Adding note about junit4 runner.
    
    Removing unnecessary product flavors.
    
    Updating copyright.
    
    Changing package name and adding copyright on test files. Also fixed issue wiht incorrect test name in the instrumentation tests.
    
    Changing package name
    
    Adding more notes for JVM -noverify
    
    Adding another source link.
    
    Adding note about running tests.
    
    Fixing typos that kneth found! :)

---
+++ examples/unitTestExample/src/androidTest/java/io/realm/examples/unittesting/jUnit3ExampleTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.realm.examples.unittesting;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class jUnit3ExampleTest extends ActivityInstrumentationTestCase2<ExampleActivity> {
+
+    public jUnit3ExampleTest() {
+        super(ExampleActivity.class);
+    }
+
+    public void testShouldBeAbleToLaunchActivityAndSeeRealmResults() {
+        getActivity();
+        onView(withText("Senior Person got older: 99")).check(matches(isDisplayed()));
+    }
+}
