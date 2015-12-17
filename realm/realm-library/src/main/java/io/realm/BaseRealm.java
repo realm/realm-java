86f2400c0e825875749a23552375ebdd11444757
Donn Felker <donn@donnfelker.com>
2015-12-17 07:07:43 -0500

    Adding @throws declarations to javadocs for BaseRealm, Realm and DynamicRealm
    Applying changes from review.
    
    Adding updated javadoc
    
    Removing RuntimeException declaration and adding grammar fix.

--- realm/realm-library/src/main/java/io/realm/BaseRealm.java
+++ realm/realm-library/src/main/java/io/realm/BaseRealm.java
@@ -87,6 +87,7 @@ abstract class BaseRealm implements Closeable {
      * thread.
      *
      * @param autoRefresh {@code true} will turn auto-refresh on, {@code false} will turn it off.
+     * @throws IllegalStateException if called from a non-Looper thread.
      */
     public void setAutoRefresh(boolean autoRefresh) {
         checkIfValid();
@@ -235,6 +236,7 @@ abstract class BaseRealm implements Closeable {
      * @param destination file to save the Realm to.
      * @param key a 64-byte encryption key.
      * @throws java.io.IOException if any write operation fails.
+     * @throws IllegalArgumentException if destination argument is null.
      */
     public void writeEncryptedCopyTo(File destination, byte[] key) throws java.io.IOException {
         if (destination == null) {
@@ -247,6 +249,8 @@ abstract class BaseRealm implements Closeable {
     /**
      * Refreshes the Realm instance and all the RealmResults and RealmObjects instances coming from it.
      * It also calls the listeners associated to the Realm instance.
+     *
+     * @throws IllegalStateException if attempting to refresh from within a transaction.
      */
     @SuppressWarnings("UnusedDeclaration")
     public void refresh() {
@@ -389,6 +393,8 @@ abstract class BaseRealm implements Closeable {
      * <p>
      * It's important to always remember to close Realm instances when you're done with it in order not to leak memory,
      * file descriptors or grow the size of Realm file out of measure.
+     *
+     * @throws IllegalStateException if attempting to close from another thread.
      */
     @Override
     public void close() {
@@ -416,6 +422,7 @@ abstract class BaseRealm implements Closeable {
      * Checks if the {@link io.realm.Realm} instance has already been closed.
      *
      * @return {@code true} if closed, {@code false} otherwise.
+     * @throws IllegalStateException if attempting to close from another thread.
      */
     public boolean isClosed() {
         if (this.threadId != Thread.currentThread().getId()) {
