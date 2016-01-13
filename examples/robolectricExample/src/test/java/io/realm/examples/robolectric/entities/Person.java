4d2c8d795299197a2d53821311fbb7c0c1f25f59
LYK <dalinaum@gmail.com>
2016-01-13 21:50:51 +0900

    Remove unused imports.

--- examples/robolectricExample/src/test/java/io/realm/examples/robolectric/entities/Person.java
+++ examples/robolectricExample/src/test/java/io/realm/examples/robolectric/entities/Person.java
@@ -17,8 +17,6 @@
 package io.realm.examples.robolectric.entities;
 
 import io.realm.RealmObject;
-//import io.realm.annotations.Index;
-//import io.realm.annotations.PrimaryKey;
 
 public class Person extends RealmObject {
     private String name;
