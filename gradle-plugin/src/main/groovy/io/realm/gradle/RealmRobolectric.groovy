057f50531c4d1c82eb16a84839a0afff1bf745e9
LYK <dalinaum@gmail.com>
2016-01-15 05:31:55 +0900

    The task of Gradle plugin has inputs and outputs.
    The task has inputs and outputs so that Gradle can figure out whether
    the task is UP-TO-DATE or not.

--- gradle-plugin/src/main/groovy/io/realm/gradle/RealmRobolectric.groovy
+++ gradle-plugin/src/main/groovy/io/realm/gradle/RealmRobolectric.groovy
@@ -8,15 +8,13 @@ class RealmRobolectric implements Plugin<Project> {
     @Override
     void apply(Project project) {
         project.task('downloadRealmRobolectric') {
+            def targetZip = "${project.projectDir}/realm-robolectric-darwin-${Version.VERSION}.zip"
+            def targetLib = "${project.projectDir}/src/test/libs/librealm-jni-darwin.dylib"
+
+            inputs.file project.file(targetZip)
+            outputs.file project.file(targetLib)
+
             doLast {
-                def targetZip = "${project.projectDir}/realm-robolectric-darwin-${Version.VERSION}.zip"
-                def targetLib = "${project.projectDir}/src/test/libs/librealm-jni-darwin.dylib"
-
-                if (project.file(targetZip).exists() && project.file(targetLib).exists()) {
-                    println "librealm-jni-darwin.dylib is already exsit. Skip download the Realm Robolectric."
-                    return
-                }
-
                 project.download {
                     src "http://static.realm.io/downloads/java/realm-robolectric-darwin-${Version.VERSION}.zip"
                     dest "${project.projectDir}"
