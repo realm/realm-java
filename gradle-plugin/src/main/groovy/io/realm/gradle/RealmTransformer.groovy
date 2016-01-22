dd207efdf2a04b2cb670df1a5749240f18eba370
Makoto Yamazaki <makoto1975@gmail.com>
2016-01-22 19:59:55 +0900

    Add license headers and reformat

--- gradle-plugin/src/main/groovy/io/realm/gradle/RealmTransformer.groovy
+++ gradle-plugin/src/main/groovy/io/realm/gradle/RealmTransformer.groovy
@@ -1,4 +1,21 @@
+/*
+ * Copyright 2016 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.realm.gradle
+
 import com.android.build.api.transform.*
 import com.google.common.collect.BiMap
 import com.google.common.collect.HashBiMap
@@ -20,7 +37,7 @@ class RealmTransformer extends Transform {
 
     @Override
     Set<ContentType> getInputTypes() {
-        return ImmutableSet.<ContentType>of(DefaultContentType.CLASSES)
+        return ImmutableSet.<ContentType> of(DefaultContentType.CLASSES)
     }
 
     @Override
@@ -40,12 +57,12 @@ class RealmTransformer extends Transform {
             throws IOException, TransformException, InterruptedException {
 
         final ArrayList<File> folders = []
-        inputs.each {TransformInput input ->
+        inputs.each { TransformInput input ->
             println "Directory inputs: ${input.directoryInputs*.file}"
             folders.addAll(input.directoryInputs*.file)
         }
 
-        referencedInputs.each {TransformInput input ->
+        referencedInputs.each { TransformInput input ->
             folders.addAll(input.directoryInputs*.file)
             folders.addAll(input.jarInputs*.file)
         }
@@ -79,7 +96,7 @@ class RealmTransformer extends Transform {
 
         println "Contains io.realm.RealmList: ${classPool.getOrNull('io.realm.RealmList')}"
 
-        def proxyClasses = classFiles.findAll {key, value ->  key.name.endsWith('RealmProxy.class') }
+        def proxyClasses = classFiles.findAll { key, value -> key.name.endsWith('RealmProxy.class') }
         println "Proxy Classes: ${proxyClasses*.value}"
 
         def modelClasses = proxyClasses.collect { key, value ->
@@ -89,7 +106,9 @@ class RealmTransformer extends Transform {
 
         def managedFields = []
         modelClasses.each {
-            managedFields.addAll(it.declaredFields.findAll { it.getAnnotation(Ignore.class) == null })
+            managedFields.addAll(it.declaredFields.findAll {
+                it.getAnnotation(Ignore.class) == null
+            })
         }
         println "Managed Fields: ${managedFields*.name}"
 
