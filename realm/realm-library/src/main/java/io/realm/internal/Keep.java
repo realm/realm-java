e819402700f3f4ea5818538b2702dd2c32c31626
Makoto Yamazaki <makoto1975@gmail.com>
2016-02-02 11:28:37 +0900

    removed Inherited annotation which was unintentionally added

--- realm/realm-library/src/main/java/io/realm/internal/Keep.java
+++ realm/realm-library/src/main/java/io/realm/internal/Keep.java
@@ -17,7 +17,6 @@
 package io.realm.internal;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -29,6 +28,5 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
-@Inherited
 public @interface Keep {
 }
