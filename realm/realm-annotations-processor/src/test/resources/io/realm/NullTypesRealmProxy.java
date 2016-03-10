59039f59f85d3d3c917a6961925b93a8ab128cdd
Chen Mulong <chenmulong@gmail.com>
2016-03-10 18:35:20 +0800

    Fix the cast in proxy generator for CacheData
    Close #2354

--- realm/realm-annotations-processor/src/test/resources/io/realm/NullTypesRealmProxy.java
+++ realm/realm-annotations-processor/src/test/resources/io/realm/NullTypesRealmProxy.java
@@ -1129,14 +1129,14 @@ public class NullTypesRealmProxy extends NullTypes
         if (currentDepth > maxDepth || realmObject == null) {
             return null;
         }
-        CacheData<NullTypes> cachedObject = (CacheData) cache.get(realmObject);
+        CacheData<RealmObject> cachedObject = cache.get(realmObject);
         NullTypes standaloneObject;
         if (cachedObject != null) {
             // Reuse cached object or recreate it because it was encountered at a lower depth.
             if (currentDepth >= cachedObject.minDepth) {
-                return cachedObject.object;
+                return (NullTypes)cachedObject.object;
             } else {
-                standaloneObject = cachedObject.object;
+                standaloneObject = (NullTypes)cachedObject.object;
                 cachedObject.minDepth = currentDepth;
             }
         } else {
