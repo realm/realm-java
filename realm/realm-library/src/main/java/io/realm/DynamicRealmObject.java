afb22921f287aa63f92747c0b7f018542bbe3c7f
SeongTaekC <stk@realm.io>
2016-05-10 19:20:48 +0900

    fix errata

--- realm/realm-library/src/main/java/io/realm/DynamicRealmObject.java
+++ realm/realm-library/src/main/java/io/realm/DynamicRealmObject.java
@@ -642,17 +642,17 @@ public final class DynamicRealmObject extends RealmObject implements RealmObject
     }
 
     /**
-     * Returns a hash code value for the object.
+     * Returns a hash code value for the {@link DynamicRealmObject} object.
      * <p>
-     * By the general contract of hashCode, any two objects for which {@link #equals} returns
-     * {@code true} must return the same hash code value.
+     * By the general contract of {@link Object#hashCode()}, any two objects for which {@link #equals}
+     * returns {@code true} must return the same hash code value.
      * <p>
      * Note that a {@link RealmObject} is <em>live</em> object, and it might be updated by changes from
-     * other threads. This means that the hash code value of the object is not stable, and the value
+     * other threads. This means that a hash code value of the object is not stable, and the value
      * should be neither used as a key in HashMap nor saved in HashSet.
      *
-     * @return  a hash code value for this object.
-     * @see     #equals(Object)
+     * @return  a hash code value for the object.
+     * @see     #equals
      */
     @Override
     public int hashCode() {
