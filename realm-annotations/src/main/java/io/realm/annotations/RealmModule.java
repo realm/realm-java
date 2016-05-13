5294a6770581d7682b5c0c138ed2c0b42b314d8f
Kenneth Geisshirt <kenneth@geisshirt.dk>
2016-05-13 15:46:26 +0200

    One RealmModule per file (#2799)
    * One RealmModule per file

--- realm-annotations/src/main/java/io/realm/annotations/RealmModule.java
+++ realm-annotations/src/main/java/io/realm/annotations/RealmModule.java
@@ -34,6 +34,10 @@ import java.lang.annotation.Target;
  * {@code library = true}. Setting {@code library = true} is normally only relevant for library authors. See below for
  * further details.
  *
+ * <p>
+ * Currently, it is not possible to have multiple RealmModule declarations in a single file. If you have more than one
+ * RealmModule, you will have to use separate Java files for each module.
+ *
  *
  * <h2>RealmModules and libraries</h2>
  *
