adc3bb7bd33e9c315602596be005a023c8ae3c1c
SeongTaekC <stk@realm.io>
2016-05-27 20:46:40 +0900

    more fix

--- realm/realm-library/src/main/java/io/realm/RealmResults.java
+++ realm/realm-library/src/main/java/io/realm/RealmResults.java
@@ -851,7 +851,7 @@ public final class RealmResults<E extends RealmModel> extends AbstractList<E> im
     }
 
     /**
-     * Returns {@code false} if the results are not yet loaded, or they are still being loaded. Synchronous
+     * Returns {@code false} if the results are not yet loaded or still being loaded. Synchronous
      * query methods like findAll() will always return {@code true}, while asynchronous query methods like
      * findAllAsync() will return {@code false} until the results are available.
      * This will return {@code true} if called for an unmanaged object (created outside of Realm).
