f4e340c6e4b482ac912609f28cc4f003806ea0f8
Christian Melchior <christian@ilios.dk>
2016-06-16 15:56:59 +0200

    Optimistic opening of a Realm file (#3013)
    We have anecdotal evidence that multiple processes might be open for the same app. We have not been able to verify this, but the result matches the "IncompatibleLockFile" errors reported in #2459. This happens if the two processes have two versions of Realm (e.g. during app upgrades).
    
    This PR is based on our assumption that such an overlap is not intentional and only happens because one process is started before the previous was completely shut down.
    
    So this PR introduces an optimistic opening scheme where we retry for 3 second before crashing as before.

--- realm/realm-library/src/main/java/io/realm/internal/SharedGroup.java
+++ realm/realm-library/src/main/java/io/realm/internal/SharedGroup.java
@@ -18,11 +18,20 @@ package io.realm.internal;
 
 import java.io.Closeable;
 import java.io.IOError;
+import java.util.concurrent.TimeUnit;
 
+import io.realm.exceptions.IncompatibleLockFileException;
+import io.realm.exceptions.RealmError;
 import io.realm.exceptions.RealmIOException;
 import io.realm.internal.async.BadVersionException;
+import io.realm.internal.log.RealmLog;
 
 public class SharedGroup implements Closeable {
+
+    // Keep these public so we can ask users to experiment with these values if needed.
+    // Should be locked down as soon as possible.
+    public static long[] INCREMENTAL_BACKOFF_MS = new long[] {1, 10, 20, 50, 100, 200, 400}; // Will keep re-using last value until LIMIT is hit
+    public static long INCREMENTAL_BACKOFF_LIMIT_MS = 3000;
 
     public static final boolean IMPLICIT_TRANSACTION = true;
     public static final boolean EXPLICIT_TRANSACTION = false;
@@ -51,6 +60,7 @@ public class SharedGroup implements Closeable {
         }
     }
 
+    // TODO Only used by Unit tests. Remove?
     public SharedGroup(String databaseFile) {
         context = new Context();
         path = databaseFile;
@@ -61,8 +71,7 @@ public class SharedGroup implements Closeable {
     public SharedGroup(String canonicalPath, boolean enableImplicitTransactions, Durability durability, byte[] key) {
         if (enableImplicitTransactions) {
             nativeReplicationPtr = nativeCreateReplication(canonicalPath, key);
-            nativePtr = createNativeWithImplicitTransactions(nativeReplicationPtr,
-                    durability.value, key);
+            nativePtr = openSharedGroupOrFail(durability, key);
             implicitTransactionsEnabled = true;
         } else {
             nativePtr = nativeCreate(canonicalPath, Durability.FULL.value, CREATE_FILE_YES, DISABLE_REPLICATION, key);
@@ -72,6 +81,51 @@ public class SharedGroup implements Closeable {
         checkNativePtrNotZero();
     }
 
+    private long openSharedGroupOrFail(Durability durability, byte[] key) {
+        // We have anecdotal evidence that on some versions of Android it is possible for two versions of an app
+        // to exist in two processes during an app upgrade. This is problematic since the lock file might not be
+        // compatible across two versions of Android. See https://github.com/realm/realm-java/issues/2459. If this
+        // happens we assume the overlap is really small so instead of failing outright we retry using incremental
+        // backoff.
+        int i = 0;
+        final long start = System.nanoTime();
+        RuntimeException lastError = null;
+        while (TimeUnit.MILLISECONDS.convert(System.nanoTime() - start, TimeUnit.NANOSECONDS) < INCREMENTAL_BACKOFF_LIMIT_MS) {
+            try {
+                long nativePtr = createNativeWithImplicitTransactions(nativeReplicationPtr, durability.value, key);
+                if (i > 0) {
+                    RealmLog.w("IncompatibleLockFile was detected. Error was resolved after " + i + " retries");
+                }
+                return nativePtr;
+            } catch (IncompatibleLockFileException e) {
+                i++;
+                lastError = e;
+                try {
+                    Thread.sleep(getSleepTime(i));
+                    RealmLog.d("Waiting for another process to release the Realm file: " + path);
+                } catch (InterruptedException ignored) {
+                    RealmLog.d("Waiting for Realm to open interrupted: " + path);
+                }
+            }
+        }
+
+        throw new RealmError("Could not open the Realm file: " + lastError.getMessage());
+    }
+
+    // Returns the time to sleep before retrying opening the SharedGroup.
+    private static long getSleepTime(int tries) {
+        if (INCREMENTAL_BACKOFF_MS == null) {
+            return 0;
+        } else {
+            if (tries > INCREMENTAL_BACKOFF_MS.length) {
+                return INCREMENTAL_BACKOFF_MS[INCREMENTAL_BACKOFF_MS.length - 1];
+            } else {
+                return INCREMENTAL_BACKOFF_MS[tries - 1];
+            }
+        }
+    }
+
+    // TODO Only used by Unit tests. Remove?
     public SharedGroup(String canonicalPath, Durability durability, byte[] key) {
         path = canonicalPath;
         context = new Context();
