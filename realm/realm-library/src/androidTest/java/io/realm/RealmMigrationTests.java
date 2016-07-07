29d4eed55de83ec081f68f7aaed988d0ecbb775b
SeongTaekC <stk@realm.io>
2016-07-07 22:03:12 +0900

    depedency cleanup

--- realm/realm-library/src/androidTest/java/io/realm/RealmMigrationTests.java
+++ realm/realm-library/src/androidTest/java/io/realm/RealmMigrationTests.java
@@ -60,7 +60,6 @@ import io.realm.rule.TestRealmConfigurationFactory;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
