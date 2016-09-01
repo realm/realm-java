a2c3a463fc5d42021f372c965b72eb0319783e25
Christian Melchior <christian@ilios.dk>
2016-09-01 07:47:54 +0200

    Remember lowest debug level correctly

--- realm/realm-library/src/main/java/io/realm/log/RealmLog.java
+++ realm/realm-library/src/main/java/io/realm/log/RealmLog.java
@@ -56,6 +56,7 @@ public final class RealmLog {
     }
 
     private static void setMinimumNativeDebugLevel(int nativeDebugLevel) {
+        minimumNativeLogLevel = nativeDebugLevel;
         Util.setDebugLevel(nativeDebugLevel); // Log level for Realm Core
     }
 
