9ec00fa73a06b5b7566131a4db54c12fe9186c49
Nabil Hachicha <nabil.hachicha@gmail.com>
2016-09-06 16:52:05 +0100

    Preparing for CI

--- integration-tests/sync/test_server/server.js
+++ integration-tests/sync/test_server/server.js
@@ -38,9 +38,9 @@ dispatcher.onGet("/start", function(req, res) {
         winston.info("Starting sync server in ", path);
         //TODO get the full path of the build by running
         //     s3cmd -c /tmp/.s3cfg ls  s3://realm-ci-artifacts/sync/
-//        syncServerChildProcess = spawn('/tmp/opt/realm-sync-0.27.4-101/bin/realm-server', ['-r', path, '-L', '127.0.0.1', '-l', 'all', '-k', './keys/public.pem', '-K', './keys/private.pem'], {env: {LD_LIBRARY_PATH: '/tmp/opt/realm-sync-0.27.4-101/lib/'}});
+       syncServerChildProcess = spawn('/tmp/opt/realm-sync-0.27.4-101/bin/realm-server', ['-r', path, '-L', '127.0.0.1', '-l', 'all', '-k', './keys/public.pem', '-K', './keys/private.pem'], {env: {LD_LIBRARY_PATH: '/tmp/opt/realm-sync-0.27.4-101/lib/'}});
         // local config:
-        syncServerChildProcess = spawn('./realm-sync-server-0.27.4/realm-server-dbg-noinst', ['-r', path, '-L', '127.0.0.1', '-l', 'all', '-k', './keys/public.pem', '-K', './keys/private.pem'], {env: {LD_LIBRARY_PATH: './realm-sync-0.27.4/'}});
+        //syncServerChildProcess = spawn('./realm-sync-server-0.27.4/realm-server-dbg-noinst', ['-r', path, '-L', '127.0.0.1', '-l', 'all', '-k', './keys/public.pem', '-K', './keys/private.pem'], {env: {LD_LIBRARY_PATH: './realm-sync-0.27.4/'}});
         syncServerChildProcess.stdout.on('data', (data) => {
           winston.info(`stdout: ${data}`);
         });
