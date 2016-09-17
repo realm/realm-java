3839337b4274c528b47c8d7145beab15905139e5
Makoto Yamazaki <makoto1975@gmail.com>
2016-09-18 01:49:03 +0900

    fix unstable test

--- realm/realm-library/src/androidTest/java/io/realm/RealmAsyncQueryTests.java
+++ realm/realm-library/src/androidTest/java/io/realm/RealmAsyncQueryTests.java
@@ -1125,7 +1125,7 @@ public class RealmAsyncQueryTests {
             public boolean onInterceptInMessage(int what) {
                 switch (what) {
                     case HandlerControllerConstants.COMPLETED_ASYNC_REALM_RESULTS: {
-                        if (numberOfIntercept.incrementAndGet() == 1) {
+                        if (numberOfIntercept.incrementAndGet() == 2 /* 2 queries are both completed */) {
                             // 6. The first time the async queries complete we start an update from
                             // another background thread. This will cause queries to rerun when the
                             // background thread notifies this thread.
