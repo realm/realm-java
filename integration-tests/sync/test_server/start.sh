cdc9b1dcedfa98e56a51fc92acfc332fd2ae86df
Chen Mulong <chenmulong@gmail.com>
2016-09-24 23:32:26 +0800

    remove useless files

--- integration-tests/sync/test_server/start.sh
+++
@@ -1,2 +0,0 @@
-npm install
-node server.js
--- tools/sync_test_server/install_ros_de.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-apt-get update -qq && \
-apt-get install -y curl npm && \
-# Setup Realm repo
-curl -s https://packagecloud.io/install/repositories/realm/realm/script.deb.sh | bash && \
-# Install realm object server
-apt-get install -y realm-object-server-de=1.0.0-beta-18.0-203 && \
-
