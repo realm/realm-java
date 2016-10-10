80970eb6a65020439648114316e230e1eb2b2b82
Christian Melchior <christian@ilios.dk>
2016-10-10 11:54:18 +0200

    Iterator not used

--- realm/realm-library/src/objectServer/java/io/realm/User.java
+++ realm/realm-library/src/objectServer/java/io/realm/User.java
@@ -30,7 +30,6 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
