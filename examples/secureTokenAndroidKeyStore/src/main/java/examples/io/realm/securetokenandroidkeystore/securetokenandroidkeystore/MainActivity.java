213c65a1922860a55a00c71f5ea93e884346b349
Nabil Hachicha <nabil.hachicha@gmail.com>
2016-10-25 17:33:59 +0100

    fix secureToken example

--- examples/secureTokenAndroidKeyStore/src/main/java/examples/io/realm/securetokenandroidkeystore/securetokenandroidkeystore/MainActivity.java
+++ examples/secureTokenAndroidKeyStore/src/main/java/examples/io/realm/securetokenandroidkeystore/securetokenandroidkeystore/MainActivity.java
@@ -30,18 +30,19 @@ import org.json.JSONObject;
 import java.security.KeyStoreException;
 import java.util.UUID;
 
-import io.realm.Realm;
-import io.realm.SyncConfiguration;
-import io.realm.SyncManager;
+import realm.io.android.CipherClient;
+import realm.io.android.SecureUserStore;
 import io.realm.SyncUser;
-import io.realm.android.SecureUserStore;
-import io.realm.internal.android.crypto.CipherClient;
-import io.realm.internal.objectserver.ObjectServerUser;
+import realm.io.android.SecureUserStore;
+import io.realm.SyncManager;
+import io.realm.SyncConfiguration;
+import io.realm.Realm;
 import io.realm.internal.objectserver.Token;
+import io.realm.internal.objectserver.ObjectServerUser;
 
 /**
  * Activity responsible of unlocking the KeyStore
- * before using the {@link io.realm.android.SecureUserStore} to encrypt
+ * before using the {@link realm.io.android.SecureUserStore} to encrypt
  * the Token we get from the session
  */
 public class MainActivity extends AppCompatActivity {
