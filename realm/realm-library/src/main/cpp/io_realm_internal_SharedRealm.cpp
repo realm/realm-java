b2ff5624fb2484644afb626b92156d792b42a83b
Christian Melchior <christian@ilios.dk>
2017-01-05 12:33:02 +0100

    Fix merge mistake

--- realm/realm-library/src/main/cpp/io_realm_internal_SharedRealm.cpp
+++ realm/realm-library/src/main/cpp/io_realm_internal_SharedRealm.cpp
@@ -40,7 +40,7 @@ Java_io_realm_internal_SharedRealm_nativeInit(JNIEnv *env, jclass, jstring tempo
 JNIEXPORT jlong JNICALL
 Java_io_realm_internal_SharedRealm_nativeCreateConfig(JNIEnv *env, jclass, jstring realm_path, jbyteArray key,
         jbyte schema_mode, jboolean in_memory, jboolean cache, jboolean disable_format_upgrade,
-        jboolean auto_change_notification, REALM_UNUSED jstring sync_server_url, jstring /*sync_user_token*/)
+        jboolean auto_change_notification, REALM_UNUSED jstring sync_server_url, REALM_UNUSED jstring sync_user_identity)
 {
     TR_ENTER()
 
