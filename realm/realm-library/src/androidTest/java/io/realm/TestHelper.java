bdc40a11baeb64e65bbc46dc2489b5f93c40d91a
Chen Mulong <chenmulong@gmail.com>
2017-01-05 19:58:46 +0800

    Life is limited

--- realm/realm-library/src/androidTest/java/io/realm/TestHelper.java
+++ realm/realm-library/src/androidTest/java/io/realm/TestHelper.java
@@ -778,7 +778,7 @@ public class TestHelper {
     }
 
     public static void awaitOrFail(CountDownLatch latch) {
-        awaitOrFail(latch, 700000);
+        awaitOrFail(latch, 7);
     }
 
     public static void awaitOrFail(CountDownLatch latch, int numberOfSeconds) {
