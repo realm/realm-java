1244a36da5d322e4c57774a3b07bdfa8c8987007
Chen Mulong <chenmulong@gmail.com>
2017-01-12 17:49:16 +0800

    Realm could be closed in the listener

--- realm/realm-library/src/main/java/io/realm/BaseRealm.java
+++ realm/realm-library/src/main/java/io/realm/BaseRealm.java
@@ -320,8 +320,13 @@ abstract class BaseRealm implements Closeable {
     public void commitTransaction() {
         checkIfValid();
         sharedRealm.commitTransaction();
-        ObjectServerFacade.getFacade(configuration.isSyncConfiguration())
-                .notifyCommit(configuration, sharedRealm.getLastSnapshotVersion());
+        if (!isClosed()) {
+            // The checking is because of the global listener is being called in commitTransaction from object store.
+            // The Realm could be closed inside the listener. In this case, we have no way to handle it. Moving
+            // SyncManger to Object Store will solve this.
+            ObjectServerFacade.getFacade(configuration.isSyncConfiguration())
+                    .notifyCommit(configuration, sharedRealm.getLastSnapshotVersion());
+        }
     }
 
     /**
