0a18f2a10dcb5af8a48d5ca24242dc54b19af7c8
Christian Melchior <christian@ilios.dk>
2017-03-14 13:43:43 +0100

    Refactor native code as well

--- realm/realm-library/src/main/cpp/io_realm_ClientResetError.cpp
+++ realm/realm-library/src/main/cpp/io_realm_ClientResetError.cpp
@@ -19,11 +19,11 @@
 #include "object-store/src/sync/sync_manager.hpp"
 
 #include "util.hpp"
-#include "io_realm_ClientResetError.h"
+#include "io_realm_ClientResetRequiredError.h"
 
 using namespace realm;
 
-JNIEXPORT void JNICALL Java_io_realm_ClientResetError_nativeExecuteClientReset(JNIEnv* env, jobject,
+JNIEXPORT void JNICALL Java_io_realm_ClientResetRequiredError_nativeExecuteClientReset(JNIEnv* env, jobject,
                                                                                jstring localRealmPath)
 {
     TR_ENTER()
