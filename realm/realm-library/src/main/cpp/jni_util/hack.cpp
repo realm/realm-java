dcae93b7ca591955fe100b9c6a30b95295692488
Chen Mulong <chenmulong@gmail.com>
2017-03-30 16:10:39 +0800

    Work around the memmove bug on sumsung device
    There was a bug for memmove on some Samsung devices. The functions
    returns "dest + n" instead of "dest".
    This has been identified earlier by QT. See:
    https://bugreports.qt.io/browse/QTBUG-34984
    The relevant android bug:
    https://code.google.com/p/android/issues/detail?id=81692
    
    This fix try to test if the device have this issue first, if yes, switch
    to a own implementation of memmove.
    
    This fix works for most cases, but since the memmove bug existing in the
    system, there would be other lib/system call the buggy version of
    memmove which could still corrupt the memory -- which means there are
    still some possibilities that app gets crashed on those devices.
    
    The more hacky way is hooking into the process's code space and modify
    the function point to our own implementation which could solve the
    problem for other lib/system calls. But there is a risk that google
    might reject the apk for play store.

---
+++ realm/realm-library/src/main/cpp/jni_util/hack.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2017 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "hack.hpp"
+#include "log.hpp"
+
+#include <string.h>
+
+#include <realm/util/assert.hpp>
+
+using namespace realm::jni_util;
+
+extern "C" {
+void* __wrap_memmove(void* dest, const void* src, size_t n);
+void* __real_memmove(void* dest, const void* src, size_t n);
+void* __builtin_memmove(void* dest, const void* src, size_t n);
+}
+
+typedef void* (*MemMoveFunc)(void* dest, const void* src, size_t n);
+static MemMoveFunc s_wrap_memmove_ptr = &__real_memmove;
+
+// Pure C implementation for memmove. Copied from kernel.
+static void* hacked_memmove(void* dest, const void* src, size_t count)
+{
+    uint8_t* tmp;
+    const uint8_t* s;
+
+    if (dest <= src) {
+        tmp = (uint8_t*)dest;
+        s = (uint8_t*)src;
+        while (count--)
+            *tmp++ = *s++;
+    }
+    else {
+        tmp = (uint8_t*)dest;
+        tmp += count;
+        s = (uint8_t*)src;
+        s += count;
+        while (count--)
+            *--tmp = *--s;
+    }
+    return dest;
+}
+
+void* __wrap_memmove(void* dest, const void* src, size_t n)
+{
+    return (*s_wrap_memmove_ptr)(dest, src, n);
+}
+
+// See https://github.com/realm/realm-java/issues/3651#issuecomment-290290228
+// There is a bug in memmove for some Samsung devices which will return "dest-n" instead of dest.
+// The bug was originally found by QT, see https://bugreports.qt.io/browse/QTBUG-34984 .
+// To work around it, we use linker's wrap feature to use a pure C implementation of memmove if the device has the
+// problem.
+static void check_memmove()
+{
+    char* array = strdup("Foobar");
+    void* ptr = __real_memmove(array + 1, array, sizeof("Foobar") - 2);
+    if (ptr != array + 1) {
+        Log::e("memmove is broken on this device. switch to the builtin implementation.");
+        s_wrap_memmove_ptr = &hacked_memmove;
+    }
+    else {
+        Log::i("memmove is not broken on this device - lucky you.");
+    }
+    free(array);
+}
+
+void hack_init()
+{
+    check_memmove();
+}
