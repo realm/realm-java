887e31948d7352d6095eca64afe508dd111c4fdd
Christian Melchior <christian@ilios.dk>
2017-05-12 23:57:08 +0200

    Add preliminary project template.

---
+++ realm/kotlin-extensions/src/androidTest/java/io/realm/RealmTests.java
@@ -0,0 +1,26 @@
+package io.realm;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class RealmTests {
+
+    @Test
+    public void where() {
+        Realm realm = Realm.getDefaultInstance();
+    }
+
+}
