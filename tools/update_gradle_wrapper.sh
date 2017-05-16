2f76f2b96bedeca340958297ada5e6b5158959af
Makoto Yamazaki <makoto1975@gmail.com>
2017-05-16 03:10:52 -0700

    move update_gradle_wrapper.sh into tools/

--- tools/update_gradle_wrapper.sh
+++ tools/update_gradle_wrapper.sh
@@ -2,6 +2,7 @@
 
 # This script updates Gradle Wrappers in this repository. You need to update the version number in realm.properties first, then execute ./update_gradle_wrapper.sh .
 
-pushd .
+pushd "$(dirname $0)/.."
+
 for i in $(find $(pwd) -type f -name gradlew); do cd $(dirname $i); pwd; ./gradlew wrapper; sed -E -i '' s/-bin\\.zip/-all.zip/ gradle/wrapper/gradle-wrapper.properties; done
 popd
