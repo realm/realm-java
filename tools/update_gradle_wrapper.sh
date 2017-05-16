a15a3fac22ae8e56fc38c07f0f019e35ce7766bc
Makoto Yamazaki <makoto1975@gmail.com>
2017-05-16 03:36:22 -0700

    stop using pushd/popd (no need to do that since the script is executed in a shubshell)

--- tools/update_gradle_wrapper.sh
+++ tools/update_gradle_wrapper.sh
@@ -2,7 +2,7 @@
 
 # This script updates Gradle Wrappers in this repository. You need to update the version number in realm.properties first, then execute ./update_gradle_wrapper.sh .
 
-pushd "$(dirname $0)/.."
+cd "$(dirname $0)/.."
 
 for i in $(find $(pwd) -type f -name gradlew); do
     cd $(dirname $i)
@@ -10,5 +10,3 @@ for i in $(find $(pwd) -type f -name gradlew); do
     ./gradlew wrapper
     sed -E -i '' s/-bin\\.zip\$/-all.zip/ gradle/wrapper/gradle-wrapper.properties
 done
-
-popd
