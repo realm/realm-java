e30e0f36efc20fa5b9a1519cd25978ca10844885
Makoto Yamazaki <makoto1975@gmail.com>
2017-05-18 12:07:47 -0700

    fix rwong variable name

--- realm-transformer/src/main/groovy/io/realm/transformer/RealmTransformer.groovy
+++ realm-transformer/src/main/groovy/io/realm/transformer/RealmTransformer.groovy
@@ -67,9 +67,9 @@ class RealmTransformer extends Transform {
     @Override
     Set<Scope> getReferencedScopes() {
         // TODO ↓ should depend on the version of Android Gradle Plugin
-        final boolean supportOld = true;
+        final boolean newScope = true;
 
-        return supportOld ? Sets.immutableEnumSet(Scope.EXTERNAL_LIBRARIES, Scope.SUB_PROJECTS, Scope.TESTED_CODE)
+        return newScope ? Sets.immutableEnumSet(Scope.EXTERNAL_LIBRARIES, Scope.SUB_PROJECTS, Scope.TESTED_CODE)
                 : Sets.immutableEnumSet(Scope.EXTERNAL_LIBRARIES, Scope.PROJECT_LOCAL_DEPS,
                 Scope.SUB_PROJECTS, Scope.SUB_PROJECTS_LOCAL_DEPS, Scope.TESTED_CODE)
     }
