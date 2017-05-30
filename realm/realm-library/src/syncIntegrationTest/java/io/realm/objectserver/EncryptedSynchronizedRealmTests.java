11fb2cd1e7679ce1446b52fbd0e4c520ac658553
Nabil Hachicha <nabil.hachicha@gmail.com>
2017-05-30 13:56:04 +0100

    WIP

--- realm/realm-library/src/syncIntegrationTest/java/io/realm/objectserver/EncryptedSynchronizedRealmTests.java
+++ realm/realm-library/src/syncIntegrationTest/java/io/realm/objectserver/EncryptedSynchronizedRealmTests.java
@@ -1,23 +1,29 @@
 package io.realm.objectserver;
 
 import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
 
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import io.realm.ObjectServerError;
 import io.realm.Realm;
+import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
 import io.realm.SyncConfiguration;
 import io.realm.SyncCredentials;
+import io.realm.SyncManager;
 import io.realm.SyncSession;
 import io.realm.SyncUser;
 import io.realm.TestHelper;
 import io.realm.entities.StringOnly;
 import io.realm.exceptions.RealmFileException;
+import io.realm.log.LogLevel;
+import io.realm.log.RealmLog;
 import io.realm.objectserver.utils.Constants;
 import io.realm.objectserver.utils.StringOnlyModule;
 import io.realm.rule.TestSyncConfigurationFactory;
@@ -30,19 +36,20 @@ public class EncryptedSynchronizedRealmTests extends BaseIntegrationTest {
 //    @Rule
 //    public Timeout globalTimeout = Timeout.seconds(10);
 
-    @Rule
-    public final TestSyncConfigurationFactory configurationFactory = new TestSyncConfigurationFactory();
-
+//    @Rule
+//    public final TestSyncConfigurationFactory configurationFactory = new TestSyncConfigurationFactory();
+//
     @Test
     public void setEncryptionKey_canOpenRealmWithout() {
+//        SyncManager.reset();
+
         String username = UUID.randomUUID().toString();
         String password = "password";
         SyncUser user = SyncUser.login(SyncCredentials.usernamePassword(username, password, true), Constants.AUTH_URL);
 
         final byte[] randomKey = TestHelper.getRandomKey();
 
-        SyncConfiguration configWithEncryption = configurationFactory
-                .createSyncConfigurationBuilder(user, Constants.USER_REALM)
+        SyncConfiguration configWithEncryption = new SyncConfiguration.Builder(user, Constants.USER_REALM)
                 .modules(new StringOnlyModule())
                 .waitForInitialRemoteData()
                 .errorHandler(new SyncSession.ErrorHandler() {
@@ -68,11 +75,9 @@ public class EncryptedSynchronizedRealmTests extends BaseIntegrationTest {
         user.logout();
         Realm.deleteRealm(configWithEncryption);
 
-
         // Try to open again the Realm without the encryption key should fail
         user = SyncUser.login(SyncCredentials.usernamePassword(username, password, false), Constants.AUTH_URL);
-        SyncConfiguration configWithoutEncryption = configurationFactory
-                .createSyncConfigurationBuilder(user, Constants.USER_REALM)
+        SyncConfiguration configWithoutEncryption = new SyncConfiguration.Builder(user, Constants.USER_REALM)
                 .modules(new StringOnlyModule())
                 .waitForInitialRemoteData()
                 .errorHandler(new SyncSession.ErrorHandler() {
@@ -92,17 +97,16 @@ public class EncryptedSynchronizedRealmTests extends BaseIntegrationTest {
         user.logout();
     }
 
-
     @Test
     public void setEncryptionKey_shouldCrashIfKeyNotProvided() {
+        RealmLog.setLevel(LogLevel.ALL);
         String username = UUID.randomUUID().toString();
         String password = "password";
         SyncUser user = SyncUser.login(SyncCredentials.usernamePassword(username, password, true), Constants.AUTH_URL);
 
         final byte[] randomKey = TestHelper.getRandomKey();
 
-        SyncConfiguration configWithEncryption = configurationFactory
-                .createSyncConfigurationBuilder(user, Constants.USER_REALM)
+        SyncConfiguration configWithEncryption = new SyncConfiguration.Builder(user, Constants.USER_REALM)
                 .modules(new StringOnlyModule())
                 .waitForInitialRemoteData()
                 .errorHandler(new SyncSession.ErrorHandler() {
@@ -129,8 +133,7 @@ public class EncryptedSynchronizedRealmTests extends BaseIntegrationTest {
 
         // Try to open again the Realm without the encryption key should fail
         user = SyncUser.login(SyncCredentials.usernamePassword(username, password, false), Constants.AUTH_URL);
-        SyncConfiguration configWithoutEncryption = configurationFactory
-                .createSyncConfigurationBuilder(user, Constants.USER_REALM)
+        SyncConfiguration configWithoutEncryption = new SyncConfiguration.Builder(user, Constants.USER_REALM)
                 .modules(new StringOnlyModule())
                 .waitForInitialRemoteData()
                 .errorHandler(new SyncSession.ErrorHandler() {
@@ -141,12 +144,73 @@ public class EncryptedSynchronizedRealmTests extends BaseIntegrationTest {
                 })
                 .build();
 
+        Thread.setDefaultUncaughtExceptionHandler( new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println(">>>>>>>>>>>>>>>>>>>>> HANDLER <<<<<<<<<<<<<<<<<<<<<<");
+                e.printStackTrace();
+            }
+        });
+
         try {
             realm = Realm.getInstance(configWithoutEncryption);
             fail("It should not be possible to open the Realm without the encryption key set previously.");
-        } catch (RealmFileException ignored){
+        } catch (RealmFileException ignored) {
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> STILL: " + ignored.getMessage());
+
+//            final Thread.UncaughtExceptionHandler uncaughtExceptionHandler =;
+//            Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+//            Thread[] threadArray = threadSet.toArray(new Thread[threadSet.size()]);
+//            for (Thread thread : threadArray) {
+//                thread.setDefaultUncaughtExceptionHandler();
+//            }
+
+
+//            Thread.setDefaultUncaughtExceptionHandler();
+//            realm = Realm.getInstance(configurationFactory
+//                    .createSyncConfigurationBuilder(user, Constants.USER_REALM)
+//                    .modules(new StringOnlyModule())
+//                    .waitForInitialRemoteData()
+//                    .errorHandler(new SyncSession.ErrorHandler() {
+//                        @Override
+//                        public void onError(SyncSession session, ObjectServerError error) {
+//                            System.out.println(">>>>>>>>>>>>>>>>>>>>> HANDLER <<<<<<<<<<<<<<<<<<<<<<");
+//                            fail(error.getErrorMessage());
+//                        }
+//                    })
+//                    .encryptionKey(randomKey)
+//                    .build());
+//            System.out.println(">>>>>>>>>>>>>>> REOPENING WITH EXNCRYPTION SUCCEED!");
+//            SyncManager.reset();
         }
     }
+
+//    @Test
+    public void test() {
+        Realm.init(InstrumentationRegistry.getTargetContext());
+
+        final byte[] randomKey = TestHelper.getRandomKey();
+        RealmConfiguration configuration = new RealmConfiguration.Builder().modules(new StringOnlyModule()).encryptionKey(randomKey).build();
+
+        Realm instance = Realm.getInstance(configuration);
+        instance.beginTransaction();
+        instance.createObject(StringOnly.class).setChars("foo");
+        instance.commitTransaction();
+
+        instance.close();
+
+        RealmConfiguration configurationWithout = new RealmConfiguration.Builder().modules(new StringOnlyModule()).build();
+
+        try {
+            instance = Realm.getInstance(configurationWithout);
+        } catch (RealmFileException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals("foo", instance.where(StringOnly.class).findFirst().getChars());
+        instance.close();
+    }
+
     // If client B encrypts its synced Realm, client A should be able to access that Realm with a different encryption key.
 
     // If an encrypted synced Realm is re-opened with the wrong key, throw an exception.
