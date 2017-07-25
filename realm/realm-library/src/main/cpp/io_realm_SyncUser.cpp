0e380cfe418b1b14ea5187be28639ad3fda36a0b
Nabil Hachicha <nabil.hachicha@gmail.com>
2017-07-25 13:03:15 +0100

    add multiple session tests

---
+++ realm/realm-library/src/main/cpp/io_realm_SyncUser.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2017 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <jni.h>
+#include <string>
+
+#include "io_realm_SyncUser.h"
+
+#include "object-store/src/sync/sync_manager.hpp"
+#include "object-store/src/sync/sync_session.hpp"
+
+#include "util.hpp"
+
+using namespace realm;
+
+static const char* ERR_COULD_NOT_ALLOCATE_MEMORY = "Could not allocate memory to return all sessions path.";
+
+JNIEXPORT jobjectArray JNICALL
+Java_io_realm_SyncUser_nativeAllSessionsPath(JNIEnv *env, jclass, jstring sync_user_identity) {
+    TR_ENTER()
+    try {
+        JStringAccessor user_identity(env, sync_user_identity);
+        std::shared_ptr<SyncUser> user = SyncManager::shared().get_existing_logged_in_user(user_identity);
+
+        if (!user) {
+            // TODO throw  maybe  already logged out
+            return nullptr;
+        }
+        auto sessions = user.get()->all_sessions();
+        if (!sessions.empty()) {
+            size_t len = sessions.size();
+            jobjectArray sessions_path = env->NewObjectArray(len, java_lang_string, 0);
+            if (sessions_path == nullptr) {
+                ThrowException(env, OutOfMemory, ERR_COULD_NOT_ALLOCATE_MEMORY);
+                return nullptr;
+            }
+            for (size_t i = 0; i < len; ++i) {
+                env->SetObjectArrayElement(sessions_path, i, to_jstring(env, sessions[i]->path().data()));
+            }
+
+            return sessions_path;
+        }
+    }
+    CATCH_STD()
+    return nullptr;
+}
--- realm/realm-library/src/syncIntegrationTest/java/io/realm/objectserver/SyncSessionTests.java
+++ realm/realm-library/src/syncIntegrationTest/java/io/realm/objectserver/SyncSessionTests.java
@@ -21,6 +21,7 @@ import io.realm.objectserver.utils.UserFactory;
 import io.realm.rule.TestSyncConfigurationFactory;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.fail;
 
 @RunWith(AndroidJUnit4.class)
@@ -105,5 +106,53 @@ public class SyncSessionTests extends BaseIntegrationTest {
         adminRealm.refresh();
         assertEquals(1, adminRealm.where(AllTypes.class).count());
         adminRealm.close();
+    }
+
+    // check that logging out a SyncUser used by different sessions will
+    // all corresponding Realms are closed
+    @Test
+    public void logout_sameSyncUserMultipleSessions() {
+        SyncUser user = UserFactory.createUniqueUser(Constants.AUTH_URL);
+
+        SyncConfiguration syncConfiguration1 = configFactory
+                .createSyncConfigurationBuilder(user, Constants.SYNC_SERVER_URL)
+                .build();
+        Realm realm1 = Realm.getInstance(syncConfiguration1);
+
+        SyncConfiguration syncConfiguration2 = configFactory
+                .createSyncConfigurationBuilder(user, Constants.SYNC_SERVER_URL_2)
+                .build();
+        Realm realm2 = Realm.getInstance(syncConfiguration2);
+
+        SyncSession session1 = SyncManager.getSession(syncConfiguration1);
+        SyncSession session2 = SyncManager.getSession(syncConfiguration2);
+
+        // make sure the `access_token` is acquired. otherwise we can still be
+        // in WAITING_FOR_ACCESS_TOKEN state
+        SystemClock.sleep(TimeUnit.SECONDS.toMillis(2));
+
+        assertNotEquals(session1, session2);
+        assertEquals(SyncSession.State.ACTIVE, session1.getState());
+        assertEquals(SyncSession.State.ACTIVE, session2.getState());
+        assertEquals(session1.getUser(), session2.getUser());
+
+        try {
+            user.logout();
+            fail("Should not be able to logout with two open Realm instances");
+        } catch (IllegalStateException e) {
+            assertEquals("A Realm controlled by this user is still open. Close all Realms before logging out: " + syncConfiguration1.getPath(), e.getMessage());
+        }
+
+        realm1.close();
+
+        try {
+            user.logout();
+            fail("Should not be able to logout with one open Realm instance");
+        } catch (IllegalStateException e) {
+            assertEquals("A Realm controlled by this user is still open. Close all Realms before logging out: " + syncConfiguration2.getPath(), e.getMessage());
+        }
+
+        realm2.close();
+        user.logout();
     }
 }
