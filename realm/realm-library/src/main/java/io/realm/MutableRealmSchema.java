ee7463a88efa7ababa9183e34e4fe46c8c9c1536
Makoto Yamazaki <makoto1975@gmail.com>
2017-07-29 18:53:24 +0900

    add target class name check

--- realm/realm-library/src/main/java/io/realm/MutableRealmSchema.java
+++ realm/realm-library/src/main/java/io/realm/MutableRealmSchema.java
@@ -98,7 +98,7 @@ class MutableRealmSchema extends RealmSchema {
         }
 
         RealmObjectSchema objectSchema = dynamicClassToSchema.remove(oldInternalName);
-        if (objectSchema == null) {
+        if (objectSchema == null || !objectSchema.getTable().isValid() || !objectSchema.getClassName().equals(newInternalName)) {
             objectSchema = new MutableRealmObjectSchema(realm, this, table);
         }
         dynamicClassToSchema.put(newInternalName, objectSchema);
