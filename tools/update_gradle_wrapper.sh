995f8bfbfa52858896db05437c8d5e597be261e0
G. Blake Meike <blake.meike@gmail.com>
2017-07-31 16:57:08 -0700

    Update update_gradle_wrapper.sh

--- tools/update_gradle_wrapper.sh
+++ tools/update_gradle_wrapper.sh
@@ -3,7 +3,7 @@
 # This script updates Gradle Wrappers in this repository.
 # To run it:
 #   1. Make sure that you have run ./gradlew assemble in the root directory
-#   2. Replace the version number in realm.properties in the root directory, with the new version number.
+#   2. Replace the gradle version number in realm.properties in the root directory, with the new version number.
 #   3. Run tools/update_gradle_wrapper.sh
 
 HERE=`pwd`
