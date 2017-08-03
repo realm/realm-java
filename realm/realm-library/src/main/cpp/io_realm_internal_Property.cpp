ba3be19f76e0fd8ce557d457689203dfa348db0b
Chen Mulong <chenmulong@gmail.com>
2017-08-03 10:11:19 +0800

    Fix license

--- realm/realm-library/src/main/cpp/io_realm_internal_Property.cpp
+++ realm/realm-library/src/main/cpp/io_realm_internal_Property.cpp
@@ -1,7 +1,8 @@
 /*
  * Copyright 2016 Realm Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
