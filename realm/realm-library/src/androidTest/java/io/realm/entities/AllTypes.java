c728d3bed4b606397b8505c3c491d7db7b8d0985
Makoto Yamazaki <makoto1975@gmail.com>
2017-08-24 20:47:20 +0900

    Add Primitive List fields to test classes

--- realm/realm-library/src/androidTest/java/io/realm/entities/AllTypes.java
+++ realm/realm-library/src/androidTest/java/io/realm/entities/AllTypes.java
@@ -37,6 +37,13 @@ public class AllTypes extends RealmObject {
     public static final String FIELD_REALMOBJECT = "columnRealmObject";
     public static final String FIELD_REALMLIST = "columnRealmList";
 
+    public static final String FIELD_STRING_LIST = "stringList";
+    public static final String FIELD_LONG_LIST = "longList";
+    public static final String FIELD_FLOAT_LIST = "floatList";
+    public static final String FIELD_DOUBLE_LIST = "doubleList";
+    public static final String FIELD_BOOLEAN_LIST = "booleanList";
+    public static final String FIELD_DATE_LIST = "dateList";
+
     public static final String[] INVALID_TYPES_FIELDS_FOR_DISTINCT
             = new String[] {FIELD_REALMOBJECT, FIELD_REALMLIST, FIELD_DOUBLE, FIELD_FLOAT};
 
@@ -54,6 +61,13 @@ public class AllTypes extends RealmObject {
     private final MutableRealmInteger columnMutableRealmInteger = MutableRealmInteger.ofNull();
     private Dog columnRealmObject;
     private RealmList<Dog> columnRealmList;
+
+    public RealmList<String> stringList;
+    public RealmList<Long> longList;
+    public RealmList<Float> floatList;
+    public RealmList<Double> doubleList;
+    public RealmList<Boolean> booleanList;
+    public RealmList<Date> dateList;
 
     public String getColumnString() {
         return columnString;
