90f3779874f3496311dc4ef82fb31f5ee7b87eb4
Christian Melchior <christian@ilios.dk>
2017-11-10 01:47:09 +0100

    Fixed unit tests

--- realm/kotlin-extensions/src/androidTest/kotlin/io/realm/KotlinRealmTests.kt
+++ realm/kotlin-extensions/src/androidTest/kotlin/io/realm/KotlinRealmTests.kt
@@ -1,29 +1,37 @@
 package io.realm
 
+import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import io.realm.entities.PrimaryKeyClass
 import io.realm.entities.SimpleClass
 import io.realm.kotlin.createObject
 import io.realm.kotlin.where
 import io.realm.rule.TestRealmConfigurationFactory
-import org.junit.After
+import org.junit.*
 import org.junit.Assert.assertEquals
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 
 @Suppress("FunctionName")
 @RunWith(AndroidJUnit4::class)
 class KotlinRealmTests {
 
+//    companion object {
+//        @BeforeClass
+//        @JvmStatic
+//        fun before() {
+//            Realm.init(InstrumentationRegistry.getTargetContext())
+//        }
+//    }
+
+    @Suppress("MemberVisibilityCanPrivate")
     @get:Rule
-    private val configFactory = TestRealmConfigurationFactory()
+    val configFactory = TestRealmConfigurationFactory()
 
     private lateinit var realm: Realm
 
     @Before
     fun setUp() {
+        Realm.init(InstrumentationRegistry.getTargetContext())
         realm = Realm.getInstance(configFactory.createConfiguration())
     }
 
