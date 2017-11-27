7d1a1a4e19945dee22b7105e38fb18bfcde847d9
Eric Maxwell <ericmaxwell2003@gmail.com>
2017-11-27 09:57:46 -0500

    Added KDoc for oneOf.

--- realm/kotlin-extensions/src/main/kotlin/io/realm/kotlin/RealmQueryExtensions.kt
+++ realm/kotlin-extensions/src/main/kotlin/io/realm/kotlin/RealmQueryExtensions.kt
@@ -7,49 +7,133 @@ import io.realm.RealmModel
 import io.realm.RealmQuery
 import java.util.*
 
-// in
 
+/**
+ * In comparison. This allows you to test if objects match any value in an array of values.
+ *
+ * @param fieldName the field to compare.
+ * @param values array of values to compare with and it cannot be null or empty.
+ * @param casing how casing is handled. [Case.INSENSITIVE] works only for the Latin-1 characters.
+ * @return the query object.
+ * @throws java.lang.IllegalArgumentException if the field isn't a String field or `values` is `null` or
+ * empty.
+ */
 fun <T : RealmModel> RealmQuery<T>.oneOf(propertyName: String,
                                          value: Array<String?>,
                                          casing: Case = Case.SENSITIVE): RealmQuery<T> {
     return this.`in`(propertyName, value, casing)
 }
 
+
+/**
+ * In comparison. This allows you to test if objects match any value in an array of values.
+ *
+ * @param fieldName the field to compare.
+ * @param values array of values to compare with and it cannot be null or empty.
+ * @return the query object.
+ * @throws java.lang.IllegalArgumentException if the field isn't a Byte field or `values` is `null` or
+ * empty.
+ */
 fun <T : RealmModel> RealmQuery<T>.oneOf(propertyName: String,
                                          value: Array<Byte?>): RealmQuery<T> {
     return this.`in`(propertyName, value)
 }
 
+/**
+ * In comparison. This allows you to test if objects match any value in an array of values.
+ *
+ * @param fieldName the field to compare.
+ * @param values array of values to compare with and it cannot be null or empty.
+ * @return the query object.
+ * @throws java.lang.IllegalArgumentException if the field isn't a Short field or `values` is `null` or
+ * empty.
+ */
 fun <T : RealmModel> RealmQuery<T>.oneOf(propertyName: String,
                                          value: Array<Short?>): RealmQuery<T> {
     return this.`in`(propertyName, value)
 }
 
+/**
+ * In comparison. This allows you to test if objects match any value in an array of values.
+ *
+ * @param fieldName the field to compare.
+ * @param values array of values to compare with and it cannot be null or empty.
+ * @return the query object.
+ * @throws java.lang.IllegalArgumentException if the field isn't a Integer field or `values` is `null`
+ * or empty.
+ */
 fun <T : RealmModel> RealmQuery<T>.oneOf(propertyName: String,
                                          value: Array<Int?>): RealmQuery<T> {
     return this.`in`(propertyName, value)
 }
 
+/**
+ * In comparison. This allows you to test if objects match any value in an array of values.
+ *
+ * @param fieldName the field to compare.
+ * @param values array of values to compare with and it cannot be null or empty.
+ * @return the query object.
+ * @throws java.lang.IllegalArgumentException if the field isn't a Long field or `values` is `null` or
+ * empty.
+ */
 fun <T : RealmModel> RealmQuery<T>.oneOf(propertyName: String,
                                          value: Array<Long?>): RealmQuery<T> {
     return this.`in`(propertyName, value)
 }
 
+/**
+ * In comparison. This allows you to test if objects match any value in an array of values.
+ *
+ * @param fieldName the field to compare.
+ * @param values array of values to compare with and it cannot be null or empty.
+ * @return the query object.
+ * @throws java.lang.IllegalArgumentException if the field isn't a Double field or `values` is `null` or
+ * empty.
+ */
 fun <T : RealmModel> RealmQuery<T>.oneOf(propertyName: String,
                                          value: Array<Double?>): RealmQuery<T> {
     return this.`in`(propertyName, value)
 }
 
+
+/**
+ * In comparison. This allows you to test if objects match any value in an array of values.
+ *
+ * @param fieldName the field to compare.
+ * @param values array of values to compare with and it cannot be null or empty.
+ * @return the query object.
+ * @throws java.lang.IllegalArgumentException if the field isn't a Float field or `values` is `null` or
+ * empty.
+ */
 fun <T : RealmModel> RealmQuery<T>.oneOf(propertyName: String,
                                          value: Array<Float?>): RealmQuery<T> {
     return this.`in`(propertyName, value)
 }
 
+
+/**
+ * In comparison. This allows you to test if objects match any value in an array of values.
+ *
+ * @param fieldName the field to compare.
+ * @param values array of values to compare with and it cannot be null or empty.
+ * @return the query object.
+ * @throws java.lang.IllegalArgumentException if the field isn't a Boolean field or `values` is `null`
+ * or empty.
+ */
 fun <T : RealmModel> RealmQuery<T>.oneOf(propertyName: String,
                                          value: Array<Boolean?>): RealmQuery<T> {
     return this.`in`(propertyName, value)
 }
 
+/**
+ * In comparison. This allows you to test if objects match any value in an array of values.
+ *
+ * @param fieldName the field to compare.
+ * @param values array of values to compare with and it cannot be null or empty.
+ * @return the query object.
+ * @throws java.lang.IllegalArgumentException if the field isn't a Date field or `values` is `null` or
+ * empty.
+ */
 fun <T : RealmModel> RealmQuery<T>.oneOf(propertyName: String,
                                          value: Array<Date?>): RealmQuery<T> {
     return this.`in`(propertyName, value)
