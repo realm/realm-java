f0f737972d966ac1f857428b9ce2b690762836fd
Christian Melchior <christian@ilios.dk>
2018-10-09 23:09:45 +0200

    Add ImportFlag docs

--- realm/realm-library/src/main/java/io/realm/ImportFlag.java
+++ realm/realm-library/src/main/java/io/realm/ImportFlag.java
@@ -16,12 +16,47 @@
 package io.realm;
 
 /**
- * FIXME
+ * This class describe how data is saved to Realm when saving full objects.
  */
 public enum ImportFlag {
+
     /**
-     * FIXME
+     * With this flag enabled, fields will not be written to the Realm file if they contain the same
+     * value as the value already present in the Realm.
+     * <p>
+     * For local Realms this only has an impact on change listeners which will not report changes to
+     * those fields that was not written.
+     * <p>
+     * For synchronized Realms this also impacts the server, which will see improved performance as
+     * there is less changes to upload and merge into the server Realm.
+     * <p>
+     * It also impact how the server merges changes from different devices. Realm uses a
+     * last-write-wins approach when merging individual fields in an object, so if a field is not
+     * written it will be considered "older" than other fields modified.
+     * <p>
+     * E.g:
+     * <ol>
+     *     <li>
+     *         Server starts out with (Field A = 1, Field B = 1)
+     *     </li>
+     *     <li>
+     *         Device 1 writes (Field A = 2, Field B = 2).
+     *     </li>
+     *     <li>
+     *         Device 2 writes (Field A = 3, Field B = 1) but ignores (Field B = 1), because that is
+     *         the value in the Realm file at this point.
+     *     </li>
+     *     <li>
+     *         Device 1 uploads its changes to the server making the server (Field A = 2, Field B = 2).
+     *         Then Device 2 uploads its changes. Due to last-write-wins, the server version now
+     *         becomes (Field A = 3, Field B = 2).
+     *     </li>
+     * <ol>
+     * This is normally the desired behaviour as the final object is the merged result of the latest
+     * changes from both devices, however if all the fields in an object are considered an atomic
+     * unit, then this flag should not be set as it will ensure that all fields are set and thus have
+     * the same "age" when importing data using methods like {@link Realm#copyToRealmOrUpdate(RealmModel, ImportFlag...)}.
      */
-    DO_NOT_SET_SAME_VALUE,
+    DO_NOT_SET_SAME_VALUES,
 
 }
