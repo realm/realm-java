2b555bb0db9677bfeb56b88c71b4a2c10da73f2a
Rémi Latapy <RemiLatapy@users.noreply.github.com>
2018-11-15 16:54:23 +0100

    Update LiveData value when becoming active
    Allow LiveData to stay up to date, especially when resuming an activity

--- examples/architectureComponentsExample/src/main/java/io/realm/examples/arch/livemodel/LiveRealmObject.java
+++ examples/architectureComponentsExample/src/main/java/io/realm/examples/arch/livemodel/LiveRealmObject.java
@@ -87,6 +87,7 @@ public class LiveRealmObject<T extends RealmModel> extends LiveData<T> {
         super.onActive();
         T object = getValue();
         if (object != null && RealmObject.isValid(object)) {
+            setValue(object);
             RealmObject.addChangeListener(object, listener);
         }
     }
