084d9e003464a6790a6d62a6ed71c2aa3ddc8dcd
Christian Melchior <christian@ilios.dk>
2019-01-04 10:08:58 +0100

    Add types to all errors

--- realm/realm-library/src/objectServer/java/io/realm/ErrorCode.java
+++ realm/realm-library/src/objectServer/java/io/realm/ErrorCode.java
@@ -36,116 +36,150 @@ public enum ErrorCode {
     UNKNOWN(Type.UNKNOWN, -1),
 
     // Realm Java errors (0-49)
-    IO_EXCEPTION(Type.JAVA, 0, Category.RECOVERABLE),    // Some IO error while either contacting the server or reading the response
-    JSON_EXCEPTION(Type.JAVA, 1),                        // JSON input could not be parsed correctly
-    CLIENT_RESET(Type.OBJECT_STORE, 7),               // Client Reset required. Don't change this value without modifying io_realm_internal_OsSharedRealm.cpp
+    IO_EXCEPTION(Type.AUTH, 0, Category.RECOVERABLE),   // Some IO error while either contacting the server or reading the response
+    JSON_EXCEPTION(Type.AUTH, 1),                       // JSON input could not be parsed correctly
+    CLIENT_RESET(Type.PROTOCOL, 7),                     // Client Reset required. Don't change this value without modifying io_realm_internal_OsSharedRealm.cpp
 
-    // Realm Object Server errors (100 - 199)
-    // Connection level and protocol errors.
-    CONNECTION_CLOSED(Type.SYNC_CLIENT, 100),          // Connection closed (no error)
-    OTHER_ERROR(Type.SYNC_CLIENT, 101),                // Other connection level error
-    UNKNOWN_MESSAGE(Type.SYNC_CLIENT, 102),            // Unknown type of input message
-    BAD_SYNTAX(Type.SYNC_CLIENT, 103),                 // Bad syntax in input message head
-    LIMITS_EXCEEDED(Type.SYNC_CLIENT, 104),            // Limits exceeded in input message
-    WRONG_PROTOCOL_VERSION(Type.SYNC_CLIENT, 105),     // Wrong protocol version (Type.SYNC_CLIENT, CLIENT)
-    BAD_SESSION_IDENT(Type.SYNC_CLIENT, 106),          // Bad session identifier in input message
-    REUSE_OF_SESSION_IDENT(Type.SYNC_CLIENT, 107),     // Overlapping reuse of session identifier (Type.SYNC_CLIENT, BIND)
-    BOUND_IN_OTHER_SESSION(Type.SYNC_CLIENT, 108),     // Client file bound in other session (Type.SYNC_CLIENT, IDENT)
-    BAD_MESSAGE_ORDER(Type.SYNC_CLIENT, 109),          // Bad input message order
-    BAD_ORIGIN_FILE_IDENT(Type.SYNC_CLIENT, 110),      // Bad origin file identifier in changeset header (Type.SYNC_CLIENT, DOWNLOAD)
-    BAD_SERVER_VERSION_DOWNLOAD(Type.SYNC_CLIENT, 111),// Bad server version in changeset header (Type.SYNC_CLIENT, DOWNLOAD)
-    BAD_CHANGESET_DOWNLOAD(Type.SYNC_CLIENT, 112),     // Bad changeset (Type.SYNC_CLIENT, DOWNLOAD)
-    BAD_REQUEST_IDENT(Type.SYNC_CLIENT, 113),          // Bad request identifier (Type.SYNC_CLIENT, MARK)
-    BAD_ERROR_CODE(Type.SYNC_CLIENT, 114),             // Bad error code (Type.SYNC_CLIENT, ERROR)
-    BAD_COMPRESSION(Type.SYNC_CLIENT, 115),            // Bad compression (Type.SYNC_CLIENT, DOWNLOAD)
-    BAD_CLIENT_VERSION_DOWNLOAD(Type.SYNC_CLIENT, 116),// Bad last integrated client version in changeset header (Type.SYNC_CLIENT, DOWNLOAD)
-    SSL_SERVER_CERT_REJECTED(Type.SYNC_CLIENT, 117),   // SSL server certificate rejected
-    PONG_TIMEOUT(Type.SYNC_CLIENT, 118),               // Timeout on reception of PONG response messsage
+    // Connection level and protocol errors from the native Sync Client
+    CONNECTION_CLOSED(Type.PROTOCOL, 100, Category.RECOVERABLE), // Connection closed (no error)
+    OTHER_ERROR(Type.PROTOCOL, 101),                 // Other connection level error
+    UNKNOWN_MESSAGE(Type.PROTOCOL, 102),             // Unknown type of input message
+    BAD_SYNTAX(Type.PROTOCOL, 103),                  // Bad syntax in input message head
+    LIMITS_EXCEEDED(Type.PROTOCOL, 104),             // Limits exceeded in input message
+    WRONG_PROTOCOL_VERSION(Type.PROTOCOL, 105),      // Wrong protocol version (CLIENT)
+    BAD_SESSION_IDENT(Type.PROTOCOL, 106),           // Bad session identifier in input message
+    REUSE_OF_SESSION_IDENT(Type.PROTOCOL, 107),      // Overlapping reuse of session identifier (BIND)
+    BOUND_IN_OTHER_SESSION(Type.PROTOCOL, 108),      // Client file bound in other session (IDENT)
+    BAD_MESSAGE_ORDER(Type.PROTOCOL, 109),           // Bad input message order
+    BAD_ORIGIN_FILE_IDENT(Type.PROTOCOL, 110),       // Bad origin file identifier in changeset header (DOWNLOAD)
+    BAD_CHANGESET_HEADER_SYNTAX(Type.PROTOCOL, 111), // Bad server version in changeset header (DOWNLOAD)
+    BAD_CHANGESET_SIZE(Type.PROTOCOL, 112),          // Bad size specified in changeset header (UPLOAD)
+    BAD_CHANGESETS(Type.PROTOCOL, 113),              // Bad changesets (UPLOAD)
 
-    // Session level errors ("", 200 - 299)
-    SESSION_CLOSED("", 200, Category.RECOVERABLE),      // Session closed ("", no error)
+    // These errors are now reported as Client errors instead
+    @Deprecated
+    BAD_REQUEST_IDENT(Type.DEPRECATED, 113),          // Bad request identifier (MARK)
+    @Deprecated
+    BAD_ERROR_CODE(Type.DEPRECATED, 114),             // Bad error code (ERROR)
+    @Deprecated
+    BAD_COMPRESSION(Type.DEPRECATED, 115),            // Bad compression (DOWNLOAD)
+    @Deprecated
+    BAD_CLIENT_VERSION_DOWNLOAD(Type.DEPRECATED, 116),// Bad last integrated client version in changeset header (DOWNLOAD)
+    @Deprecated
+    SSL_SERVER_CERT_REJECTED(Type.DEPRECATED, 117),   // SSL server certificate rejected
+    @Deprecated
+    PONG_TIMEOUT(Type.DEPRECATED, 118),               // Timeout on reception of PONG response messsage
+
+    // Session level errors from the native Sync Client
+    SESSION_CLOSED("", 200, Category.RECOVERABLE),      // Session closed (no error)
     OTHER_SESSION_ERROR("", 201, Category.RECOVERABLE), // Other session level error
     TOKEN_EXPIRED("", 202, Category.RECOVERABLE),       // Access token expired
 
     // Session fatal: Auth wrong. Cannot be fixed without a new User/SyncConfiguration.
-    BAD_AUTHENTICATION("", 203),                        // Bad user authentication ("", BIND, REFRESH)
-    ILLEGAL_REALM_PATH("", 204),                        // Illegal Realm path ("", BIND)
-    NO_SUCH_PATH("", 205),                              // No such Realm ("", BIND)
-    PERMISSION_DENIED("", 206),                         // Permission denied ("", BIND, REFRESH)
+    BAD_AUTHENTICATION("", 203),                        // Bad user authentication (BIND, REFRESH)
+    ILLEGAL_REALM_PATH("", 204),                        // Illegal Realm path (BIND)
+    NO_SUCH_PATH("", 205),                              // No such Realm (BIND)
+    PERMISSION_DENIED("", 206),                         // Permission denied (BIND, REFRESH)
 
     // Fatal: Wrong server/client versions. Trying to sync incompatible files or the file was corrupted.
-    BAD_SERVER_FILE_IDENT("", 207),                     // Bad server file identifier ("", IDENT)
-    BAD_CLIENT_FILE_IDENT("", 208),                     // Bad client file identifier ("", IDENT)
-    BAD_SERVER_VERSION("", 209),                        // Bad server version ("", IDENT, UPLOAD)
-    BAD_CLIENT_VERSION("", 210),                        // Bad client version ("", IDENT, UPLOAD)
-    DIVERGING_HISTORIES("", 211),                       // Diverging histories ("", IDENT)
-    BAD_CHANGESET("", 212),                             // Bad changeset ("", UPLOAD)
+    BAD_SERVER_FILE_IDENT("", 207),                     // Bad server file identifier (IDENT)
+    BAD_CLIENT_FILE_IDENT("", 208),                     // Bad client file identifier (IDENT)
+    BAD_SERVER_VERSION("", 209),                        // Bad server version (IDENT, UPLOAD)
+    BAD_CLIENT_VERSION("", 210),                        // Bad client version (IDENT, UPLOAD)
+    DIVERGING_HISTORIES("", 211),                       // Diverging histories (IDENT)
+    BAD_CHANGESET("", 212),                             // Bad changeset (UPLOAD)
     DISABLED_SESSION("", 213),                          // Disabled session
-    PARTIAL_SYNC_DISABLED("", 214),                     // Partial sync disabled ("", BIND)
+    PARTIAL_SYNC_DISABLED("", 214),                     // Partial sync disabled (BIND)
+    SESSION_UNSUPPORTED_SESSION_FEATURE(Type.SESSION, 215),       // Unsupported session-level feature
+    SESSION_BAD_ORIGIN_FILE_IDENT(Type.SESSION, 216),       // Bad origin file identifier (UPLOAD)
+
+    // Sync Network Client errors.
+    CLIENT_CONNECTION_CLOSED(Type.SESSION, 100), // Connection closed (no error)
+    CLIENT_UNKNOWN_MESSAGE(Type.SESSION, 101), // Unknown type of input message
+    CLIENT_LIMITS_EXCEEDED(Type.SESSION, 103), // Limits exceeded in input message
+    CLIENT_BAD_SESSION_IDENT(Type.SESSION, 104), // Bad session identifier in input message
+    CLIENT_BAD_MESSAGE_ORDER(Type.SESSION, 105), // Bad input message order
+    CLIENT_BAD_CLIENT_FILE_IDENT(Type.SESSION, 106), // Bad client file identifier (IDENT)
+    CLIENT_BAD_PROGRESS(Type.SESSION, 107), // Bad progress information (DOWNLOAD)
+    CLIENT_BAD_CHANGESET_HEADER_SYNTAX(Type.SESSION, 108), // Bad syntax in changeset header (DOWNLOAD)
+    CLIENT_BAD_CHANGESET_SIZE(Type.SESSION, 109), // Bad changeset size in changeset header (DOWNLOAD)
+    CLIENT_BAD_ORIGIN_FILE_IDENT(Type.SESSION, 110), // Bad origin file identifier in changeset header (DOWNLOAD)
+    CLIENT_BAD_SERVER_VERSION(Type.SESSION, 111), // Bad server version in changeset header (DOWNLOAD)
+    CLIENT_BAD_CHANGESET(Type.SESSION, 112), // Bad changeset (DOWNLOAD)
+    CLIENT_BAD_REQUEST_IDENT(Type.SESSION, 113), // Bad request identifier (MARK)
+    CLIENT_BAD_ERROR_CODE(Type.SESSION, 114), // Bad error code (ERROR)
+    CLIENT_BAD_COMPRESSION(Type.SESSION, 115), // Bad compression (DOWNLOAD)
+    CLIENT_BAD_CLIENT_VERSION(Type.SESSION, 116), // Bad last integrated client version in changeset header (DOWNLOAD)
+    CLIENT_SSL_SERVER_CERT_REJECTED(Type.SESSION, 117), // SSL server certificate rejected
+    CLIENT_PONG_TIMEOUT(Type.SESSION, 118), // Timeout on reception of PONG respone message
+    CLIENT_BAD_CLIENT_FILE_IDENT_SALT(Type.SESSION, 119), // Bad client file identifier salt (IDENT)
+    CLIENT_FILE_IDENT(Type.SESSION, 120), // Bad file identifier (ALLOC)
+    CLIENT_CONNECT_TIMEOUT(Type.SESSION, 121), // Sync connection was not fully established in time
+    CLIENT_BAD_TIMESTAMP(Type.SESSION, 122), // Bad timestamp (PONG)
 
     // 300 - 599 Reserved for Standard HTTP error codes
-    MULTIPLE_CHOICES("", 300),
-    MOVED_PERMANENTLY("", 301),
-    FOUND("", 302),
-    SEE_OTHER("", 303),
-    NOT_MODIFIED("", 304),
-    USE_PROXY("", 305),
-    TEMPORARY_REDIRECT("", 307),
-    PERMANENT_REDIRECT("", 308),
-    BAD_REQUEST("", 400),
-    UNAUTHORIZED("", 401),
-    PAYMENT_REQUIRED("", 402),
-    FORBIDDEN("", 403),
-    NOT_FOUND("", 404),
-    METHOD_NOT_ALLOWED("", 405),
-    NOT_ACCEPTABLE("", 406),
-    PROXY_AUTHENTICATION_REQUIRED("", 407),
-    REQUEST_TIMEOUT("", 408),
-    CONFLICT("", 409),
-    GONE("", 410),
-    LENGTH_REQUIRED("", 411),
-    PRECONDITION_FAILED("", 412),
-    PAYLOAD_TOO_LARGE("", 413),
-    URI_TOO_LONG("", 414),
-    UNSUPPORTED_MEDIA_TYPE("", 415),
-    RANGE_NOT_SATISFIABLE("", 416),
-    EXPECTATION_FAILED("", 417),
-    MISDIRECTED_REQUEST("", 421),
-    UNPROCESSABLE_ENTITY("", 422),
-    LOCKED("", 423),
-    FAILED_DEPENDENCY("", 424),
-    UPGRADE_REQUIRED("", 426),
-    PRECONDITION_REQUIRED("", 428),
-    TOO_MANY_REQUESTS("", 429),
-    REQUEST_HEADER_FIELDS_TOO_LARGE("", 431),
-    UNAVAILABLE_FOR_LEGAL_REASONS("", 451),
-    INTERNAL_SERVER_ERROR("", 500),
-    NOT_IMPLEMENTED("", 501),
-    BAD_GATEWAY("", 502),
-    SERVICE_UNAVAILABLE("", 503),
-    GATEWAY_TIMEOUT("", 504),
-    HTTP_VERSION_NOT_SUPPORTED("", 505),
-    VARIANT_ALSO_NEGOTIATES("", 506),
-    INSUFFICIENT_STORAGE("", 507),
-    LOOP_DETECTED("", 508),
-    NOT_EXTENDED("", 510),
-    NETWORK_AUTHENTICATION_REQUIRED("", 511),
+    MULTIPLE_CHOICES(Type.HTTP, 300),
+    MOVED_PERMANENTLY(Type.HTTP, 301),
+    FOUND(Type.HTTP, 302),
+    SEE_OTHER(Type.HTTP, 303),
+    NOT_MODIFIED(Type.HTTP, 304),
+    USE_PROXY(Type.HTTP, 305),
+    TEMPORARY_REDIRECT(Type.HTTP, 307),
+    PERMANENT_REDIRECT(Type.HTTP, 308),
+    BAD_REQUEST(Type.HTTP, 400),
+    UNAUTHORIZED(Type.HTTP, 401),
+    PAYMENT_REQUIRED(Type.HTTP, 402),
+    FORBIDDEN(Type.HTTP, 403),
+    NOT_FOUND(Type.HTTP, 404),
+    METHOD_NOT_ALLOWED(Type.HTTP, 405),
+    NOT_ACCEPTABLE(Type.HTTP, 406),
+    PROXY_AUTHENTICATION_REQUIRED(Type.HTTP, 407),
+    REQUEST_TIMEOUT(Type.HTTP, 408),
+    CONFLICT(Type.HTTP, 409),
+    GONE(Type.HTTP, 410),
+    LENGTH_REQUIRED(Type.HTTP, 411),
+    PRECONDITION_FAILED(Type.HTTP, 412),
+    PAYLOAD_TOO_LARGE(Type.HTTP, 413),
+    URI_TOO_LONG(Type.HTTP, 414),
+    UNSUPPORTED_MEDIA_TYPE(Type.HTTP, 415),
+    RANGE_NOT_SATISFIABLE(Type.HTTP, 416),
+    EXPECTATION_FAILED(Type.HTTP, 417),
+    MISDIRECTED_REQUEST(Type.HTTP, 421),
+    UNPROCESSABLE_ENTITY(Type.HTTP, 422),
+    LOCKED(Type.HTTP, 423),
+    FAILED_DEPENDENCY(Type.HTTP, 424),
+    UPGRADE_REQUIRED(Type.HTTP, 426),
+    PRECONDITION_REQUIRED(Type.HTTP, 428),
+    TOO_MANY_REQUESTS(Type.HTTP, 429),
+    REQUEST_HEADER_FIELDS_TOO_LARGE(Type.HTTP, 431),
+    UNAVAILABLE_FOR_LEGAL_REASONS(Type.HTTP, 451),
+    INTERNAL_SERVER_ERROR(Type.HTTP, 500),
+    NOT_IMPLEMENTED(Type.HTTP, 501),
+    BAD_GATEWAY(Type.HTTP, 502),
+    SERVICE_UNAVAILABLE(Type.HTTP, 503),
+    GATEWAY_TIMEOUT(Type.HTTP, 504),
+    HTTP_VERSION_NOT_SUPPORTED(Type.HTTP, 505),
+    VARIANT_ALSO_NEGOTIATES(Type.HTTP, 506),
+    INSUFFICIENT_STORAGE(Type.HTTP, 507),
+    LOOP_DETECTED(Type.HTTP, 508),
+    NOT_EXTENDED(Type.HTTP, 510),
+    NETWORK_AUTHENTICATION_REQUIRED(Type.HTTP, 511),
 
-    // Realm Authentication Server response errors ("", 600 - 699)
-    INVALID_PARAMETERS(Type.REALM_OBJECT_SERVER, 601),
-    MISSING_PARAMETERS(Type.REALM_OBJECT_SERVER, 602),
-    INVALID_CREDENTIALS(Type.REALM_OBJECT_SERVER, 611),
-    UNKNOWN_ACCOUNT(Type.REALM_OBJECT_SERVER, 612),
-    EXISTING_ACCOUNT(Type.REALM_OBJECT_SERVER, 613),
-    ACCESS_DENIED(Type.REALM_OBJECT_SERVER, 614),
-    EXPIRED_REFRESH_TOKEN(Type.REALM_OBJECT_SERVER, 615),
-    INVALID_HOST(Type.REALM_OBJECT_SERVER, 616),
+    // Realm Authentication Server response errors (600 - 699)
+    INVALID_PARAMETERS(Type.AUTH, 601),
+    MISSING_PARAMETERS(Type.AUTH, 602),
+    INVALID_CREDENTIALS(Type.AUTH, 611),
+    UNKNOWN_ACCOUNT(Type.AUTH, 612),
+    EXISTING_ACCOUNT(Type.AUTH, 613),
+    ACCESS_DENIED(Type.AUTH, 614),
+    EXPIRED_REFRESH_TOKEN(Type.AUTH, 615),
+    INVALID_HOST(Type.AUTH, 616),
 
     // Other Realm Object Server response errors
-    EXPIRED_PERMISSION_OFFER(Type.REALM_OBJECT_SERVER, 701),
-    AMBIGUOUS_PERMISSION_OFFER_TOKEN(Type.REALM_OBJECT_SERVER, 702),
-    FILE_MAY_NOT_BE_SHARED(Type.REALM_OBJECT_SERVER, 703),
-    SERVER_MISCONFIGURATION(Type.REALM_OBJECT_SERVER, 801);
+    EXPIRED_PERMISSION_OFFER(Type.AUTH, 701),
+    AMBIGUOUS_PERMISSION_OFFER_TOKEN(Type.AUTH, 702),
+    FILE_MAY_NOT_BE_SHARED(Type.AUTH, 703),
+    SERVER_MISCONFIGURATION(Type.AUTH, 801);
 
     // Generic system errors we want to enumerate specifically
 
@@ -258,11 +292,13 @@ public enum ErrorCode {
     }
 
     public static class Type {
-        public static final String JAVA = "java";
-        public static final String OBJECT_STORE = "object-store";
-        public static final String REALM_OBJECT_SERVER = "realm-object-server";
-        public static final String UNKNOWN = "unknown";
-        public static final String SYNC_CLIENT = "realm::sync::Client::Error";
+        public static final String AUTH = "auth"; // Errors from the Realm Object Server
+        public static final String DEPRECATED = "deprecated"; // Deprecated errors
+        public static final String HTTP = "http"; // Errors from the HTTP layer
+        public static final String OBJECT_STORE = "object-store"; // Errors from object store
+        public static final String PROTOCOL = "realm::sync::ProtocolError"; // Protocol level errors from the native Sync Client
+        public static final String SESSION = "realm::sync::Client::Error"; // Session level errors from the native Sync Client
+        public static final String UNKNOWN = "unknown"; // Catch-all category
     }
 
 
