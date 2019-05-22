5ac7f26cb7b843cacec2c320a69695cbbea4e3b4
Christian Melchior <christian@ilios.dk>
2019-05-22 08:59:51 +0200

    Fix license year

--- library-benchmarks/src/androidTest/java/io/realm/benchmarks/RealmBenchmarks.kt
+++ library-benchmarks/src/androidTest/java/io/realm/benchmarks/RealmBenchmarks.kt
@@ -16,14 +16,10 @@
 
 package io.realm.benchmarks
 
-import android.os.SystemClock
 import androidx.benchmark.BenchmarkRule
 import androidx.benchmark.measureRepeated
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
-import org.junit.runner.RunWith
-
+import androidx.test.platform.app.InstrumentationRegistry
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import io.realm.benchmarks.entities.AllTypes
@@ -31,6 +27,7 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class RealmBenchmarks {
