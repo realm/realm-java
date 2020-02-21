6f05d27f1d0a40f6cd2967e21d8fe025a6fec701
Christian Melchior <christian@ilios.dk>
2020-02-21 08:54:10 +0100

    Also print AAR size

--- tools/analyze_realm_metrics.sh
+++ tools/analyze_realm_metrics.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# This script will print the size of the Realm libraries.
+# This script will print metrics for the Realm library being deployed to end users.
 # To run it:
 #   1. Make sure that $D8 is defined in your environment, e.g. `D8="$ANDROID_SDK_ROOT/build-tools/29.0.2/d8"`
-#   2. Make sure that you have built the library artifacts using ./gradlew assemble from the realm folder.
-#   3. Run the script: `> sh ./print_library_size.sh`
+#   2. Make sure that you have built the library artifacts using `./gradlew assembleRelease` from the realm folder.
+#   3. Run the script: `> sh ./analyze_realm_metrics.sh`
 #
 # Note: This script has only been tested on MacOS
 
@@ -16,6 +16,7 @@ cd realm/realm-library/build/outputs/aar
 
 # Base variant
 echo "Analyzing Base..."
+stat -f"AAR size: %z" realm-android-library-base-release.aar
 rm -rf unzippedBase
 unzip -qq realm-android-library-base-release.aar -d unzippedBase
 sh "$D8" --release --output ./unzippedBase unzippedBase/classes.jar > /dev/null 2>&1
@@ -24,6 +25,7 @@ find ./unzippedBase -name '*.so' -exec stat -f"%z %N" {} \;
 
 # ObjectServer variant
 echo "\nAnalyzing ObjectServer..."
+stat -f"AAR size: %z" realm-android-library-objectServer-release.aar
 rm -rf unzippedObjectServer
 unzip -qq realm-android-library-objectServer-release.aar -d unzippedObjectServer
 sh "$D8" --release --output ./unzippedObjectServer unzippedObjectServer/classes.jar > /dev/null 2>&1
