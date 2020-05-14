34378700c7ec6c600dae600f8a1ec2928efeba23
Christian Melchior <christian@ilios.dk>
2020-05-14 22:03:00 +0200

    Fix merge mistake.

--- realm/realm-library/src/main/java/io/realm/MutableRealmObjectSchema.java
+++ realm/realm-library/src/main/java/io/realm/MutableRealmObjectSchema.java
@@ -303,9 +303,9 @@ class MutableRealmObjectSchema extends RealmObjectSchema {
             if (original_size > Integer.MAX_VALUE) {
                 throw new UnsupportedOperationException("Too many results to iterate: " + original_size);
             }
-            int size = (int) results.size();
+            int size = (int) result.size();
             for (int i = 0; i < size; i++) {
-                DynamicRealmObject obj = new DynamicRealmObject(realm, new CheckedRow(results.getUncheckedRow(i)));
+                DynamicRealmObject obj = new DynamicRealmObject(realm, new CheckedRow(result.getUncheckedRow(i)));
                 if (obj.isValid()) {
                     function.apply(obj);
                 }
