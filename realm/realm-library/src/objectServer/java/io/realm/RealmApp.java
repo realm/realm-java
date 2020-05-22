0dcfd57f939acf4ab610560077a5f8208f21a42d
Christian Melchior <christian@ilios.dk>
2020-05-22 14:21:49 +0200

    Use better platform version

--- realm/realm-library/src/objectServer/java/io/realm/RealmApp.java
+++ realm/realm-library/src/objectServer/java/io/realm/RealmApp.java
@@ -126,7 +126,7 @@ public class RealmApp {
                 userAgentBindingInfo,
                 appDefinedUserAgent,
                 "android",
-                Integer.toString(android.os.Build.VERSION.SDK_INT),
+                android.os.Build.VERSION.RELEASE,
                 io.realm.BuildConfig.VERSION_NAME);
     }
 
