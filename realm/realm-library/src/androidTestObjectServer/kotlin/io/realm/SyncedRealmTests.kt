565ce63530f725b2b093b04d31140246e1875f06
Claus Rørbech <claus.rorbech@mongodb.com>
2020-05-25 15:07:27 +0200

    Updated SyncedRealmTests to `use`-pattern

--- realm/realm-library/src/androidTestObjectServer/kotlin/io/realm/SyncedRealmTests.kt
+++ realm/realm-library/src/androidTestObjectServer/kotlin/io/realm/SyncedRealmTests.kt
@@ -39,7 +39,6 @@ class SyncedRealmTests {
     val configFactory = TestSyncConfigurationFactory()
 
     private lateinit var app: RealmApp
-    private lateinit var realm: Realm
     private lateinit var partitionValue: String
 
     @Before
@@ -52,9 +51,6 @@ class SyncedRealmTests {
 
     @After
     fun tearDown() {
-        if (this::realm.isInitialized) {
-            realm.close()
-        }
         if (this::app.isInitialized) {
             app.close()
         }
@@ -66,10 +62,13 @@ class SyncedRealmTests {
     fun connectWithInitialSchema() {
         val user: RealmUser = createNewUser()
         val config = createDefaultConfig(user)
-        realm = Realm.getInstance(config)
-        app.syncManager.getSession(config).uploadAllLocalChanges()
-        app.syncManager.getSession(config).downloadAllServerChanges()
-        assertTrue(realm.isEmpty)
+        Realm.getInstance(config).use { realm ->
+            with(realm.syncSession) {
+                uploadAllLocalChanges()
+                downloadAllServerChanges()
+            }
+            assertTrue(realm.isEmpty)
+        }
     }
 
     // Smoke test for Sync
@@ -78,23 +77,24 @@ class SyncedRealmTests {
         // User 1 creates an object an uploads it to MongoDB Realm
         val user1: RealmUser = createNewUser()
         val config1: SyncConfiguration = createCustomConfig(user1, partitionValue)
-        realm = Realm.getInstance(config1)
-        realm.executeTransaction {
-            for (i in 1..10) {
-                it.insert(SyncColor())
+        Realm.getInstance(config1).use { realm ->
+            realm.executeTransaction {
+                for (i in 1..10) {
+                    it.insert(SyncColor())
+                }
             }
+            realm.syncSession.uploadAllLocalChanges()
+            assertEquals(10, realm.where<SyncColor>().count())
         }
-        app.syncManager.getSession(config1).uploadAllLocalChanges()
-        assertEquals(10, realm.where<SyncColor>().count())
-        realm.close()
 
         // User 2 logs and using the same partition key should see the object
         val user2: RealmUser = createNewUser()
         val config2 = createCustomConfig(user2, partitionValue)
-        realm = Realm.getInstance(config2)
-        realm.syncSession.downloadAllServerChanges()
-        realm.refresh()
-        assertEquals(10, realm.where<SyncColor>().count())
+        Realm.getInstance(config2).use { realm ->
+            realm.syncSession.downloadAllServerChanges()
+            realm.refresh()
+            assertEquals(10, realm.where<SyncColor>().count())
+        }
     }
 
     // Smoke test for sync
@@ -104,34 +104,34 @@ class SyncedRealmTests {
         // User 1 creates an object an uploads it to MongoDB Realm
         val user1: RealmUser = createNewUser()
         val config1: SyncConfiguration = createDefaultConfig(user1, partitionValue)
-        realm = Realm.getInstance(config1)
-        realm.executeTransaction {
-            val person = SyncPerson()
-            person.firstName = "Jane"
-            person.lastName = "Doe"
-            person.age = 42
-            realm.insert(person);
-            for (i in 0..9) {
-                val dog = SyncDog()
-                dog.name = "Fido $i"
-                it.insert(dog)
+        Realm.getInstance(config1).use { realm ->
+            realm.executeTransaction {
+                val person = SyncPerson()
+                person.firstName = "Jane"
+                person.lastName = "Doe"
+                person.age = 42
+                realm.insert(person);
+                for (i in 0..9) {
+                    val dog = SyncDog()
+                    dog.name = "Fido $i"
+                    it.insert(dog)
+                }
             }
+            realm.syncSession.uploadAllLocalChanges()
+            assertEquals(10, realm.where<SyncDog>().count())
+            assertEquals(1, realm.where<SyncPerson>().count())
         }
-        realm.syncSession.uploadAllLocalChanges()
-        assertEquals(10, realm.where<SyncDog>().count())
-        assertEquals(1, realm.where<SyncPerson>().count())
-        realm.close()
 
         // User 2 logs and using the same partition key should see the object
         val user2: RealmUser = createNewUser()
         val config2 = createDefaultConfig(user2, partitionValue)
-        realm = Realm.getInstance(config2)
-        realm.syncSession.downloadAllServerChanges()
-        realm.refresh()
-        assertEquals(10, realm.where<SyncDog>().count())
-        assertEquals(1, realm.where<SyncPerson>().count())
+        Realm.getInstance(config2).use { realm ->
+            realm.syncSession.downloadAllServerChanges()
+            realm.refresh()
+            assertEquals(10, realm.where<SyncDog>().count())
+            assertEquals(1, realm.where<SyncPerson>().count())
+        }
     }
-
 
     // Smoke test for sync
     // Insert objects with links between them
@@ -141,44 +141,45 @@ class SyncedRealmTests {
         // User 1 creates an object an uploads it to MongoDB Realm
         val user1: RealmUser = createNewUser()
         val config1: SyncConfiguration = createDefaultConfig(user1, partitionValue)
-        realm = Realm.getInstance(config1)
-        realm.executeTransaction {
-            val person = SyncPerson()
-            person.firstName = "Jane"
-            person.lastName = "Doe"
-            person.age = 42
-            for (i in 0..9) {
-                val dog = SyncDog()
-                dog.name = "Fido $i"
-                it.insert(dog)
-                person.dogs.add(dog)
+        Realm.getInstance(config1).use { realm ->
+            realm.executeTransaction {
+                val person = SyncPerson()
+                person.firstName = "Jane"
+                person.lastName = "Doe"
+                person.age = 42
+                for (i in 0..9) {
+                    val dog = SyncDog()
+                    dog.name = "Fido $i"
+                    it.insert(dog)
+                    person.dogs.add(dog)
+                }
+                realm.insert(person)
             }
-            realm.insert(person)
+            realm.syncSession.uploadAllLocalChanges()
+            assertEquals(10, realm.where<SyncDog>().count())
+            assertEquals(1, realm.where<SyncPerson>().count())
         }
-        realm.syncSession.uploadAllLocalChanges()
-        assertEquals(10, realm.where<SyncDog>().count())
-        assertEquals(1, realm.where<SyncPerson>().count())
-        realm.close()
 
         // User 2 logs and using the same partition key should see the object
         val user2: RealmUser = createNewUser()
         val config2 = createDefaultConfig(user2, partitionValue)
-        realm = Realm.getInstance(config2)
-        realm.syncSession.downloadAllServerChanges()
-        realm.refresh()
-        assertEquals(10, realm.where<SyncDog>().count())
-        assertEquals(1, realm.where<SyncPerson>().count())
+        Realm.getInstance(config2).use { realm ->
+            realm.syncSession.downloadAllServerChanges()
+            realm.refresh()
+            assertEquals(10, realm.where<SyncDog>().count())
+            assertEquals(1, realm.where<SyncPerson>().count())
+        }
     }
 
     @Test
     fun session() {
         val user: RealmUser = app.login(RealmCredentials.anonymous())
-        realm = Realm.getInstance(createDefaultConfig(user))
-        assertNotNull(realm.syncSession)
-        assertEquals(SyncSession.State.ACTIVE, realm.syncSession.state)
-        assertEquals(user, realm.syncSession.user)
+        Realm.getInstance(createDefaultConfig(user)).use { realm ->
+            assertNotNull(realm.syncSession)
+            assertEquals(SyncSession.State.ACTIVE, realm.syncSession.state)
+            assertEquals(user, realm.syncSession.user)
+        }
     }
-
 
     @Test
     @Ignore("Flaky, seems like Realm.compactRealm(config) sometimes returns false")
