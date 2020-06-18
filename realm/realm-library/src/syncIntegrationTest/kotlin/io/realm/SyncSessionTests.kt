4d774afa03af62dddd82fc94e2d9cccc51635adc
Christian Melchior <christian@ilios.dk>
2020-06-18 15:56:16 +0200

    Re-enable test

--- realm/realm-library/src/syncIntegrationTest/kotlin/io/realm/SyncSessionTests.kt
+++ realm/realm-library/src/syncIntegrationTest/kotlin/io/realm/SyncSessionTests.kt
@@ -229,7 +229,6 @@ class SyncSessionTests {
     }
 
     @Test
-    @Ignore("FIXME")
     fun uploadDownloadAllChanges() {
         Realm.getInstance(syncConfiguration).use { realm ->
             realm.executeTransaction {
