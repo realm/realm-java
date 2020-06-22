cd31d947c88385e8f388e1b5779c298f7f111eb7
Claus Rørbech <claus.rorbech@mongodb.com>
2020-06-22 10:44:19 +0200

    Fix EncryptionSynchronizedRealmTests

--- realm/realm-library/src/syncIntegrationTest/kotlin/io/realm/EncryptedSynchronizedRealmTests.kt
+++ realm/realm-library/src/syncIntegrationTest/kotlin/io/realm/EncryptedSynchronizedRealmTests.kt
@@ -15,199 +15,185 @@
  */
 package io.realm
 
-import io.realm.ObjectServerError
-import io.realm.StandardIntegrationTest
-import io.realm.SyncConfiguration
-import io.realm.SyncManager
-import io.realm.SyncSession
-import io.realm.SyncTestUtils
-import io.realm.SyncUser
-import io.realm.entities.StringOnly
+import androidx.test.platform.app.InstrumentationRegistry
+import io.realm.entities.SyncStringOnly
 import io.realm.exceptions.RealmFileException
-import io.realm.objectserver.utils.Constants
-import io.realm.objectserver.utils.StringOnlyModule
-import io.realm.objectserver.utils.UserFactory
+import io.realm.kotlin.syncSession
+import io.realm.log.LogLevel
+import io.realm.log.RealmLog
+import io.realm.mongodb.App
+import io.realm.mongodb.Credentials
+import io.realm.mongodb.close
+import io.realm.mongodb.registerUserAndLogin
+import io.realm.mongodb.sync.SyncConfiguration
+import io.realm.mongodb.sync.testSchema
+import org.bson.BsonObjectId
+import org.bson.types.ObjectId
+import org.junit.After
 import org.junit.Assert
-import org.junit.Rule
+import org.junit.Before
 import org.junit.Test
-import org.junit.rules.Timeout
-import java.util.*
+import kotlin.test.assertFailsWith
 
-class EncryptedSynchronizedRealmTests : StandardIntegrationTest() {
-    @Rule
-    var globalTimeout = Timeout.seconds(30)
+private val SECRET_PASSWORD = "123456"
+
+class EncryptedSynchronizedRealmTests {
+
+    private lateinit var app: App
+
+    private val configurationFactory: TestSyncConfigurationFactory = TestSyncConfigurationFactory()
+
+    @Before
+    fun setup() {
+        Realm.init(InstrumentationRegistry.getInstrumentation().targetContext)
+        RealmLog.setLevel(LogLevel.ALL)
+        app = TestApp()
+    }
+
+    @After
+    fun teardown() {
+        if (this::app.isInitialized) {
+            app.close()
+        }
+        RealmLog.setLevel(LogLevel.WARN)
+    }
 
     // Make sure the encryption is local, i.e after deleting a synced Realm
     // re-open it again with no (or different) key, should be possible.
     @Test
-    @Throws(InterruptedException::class)
     fun setEncryptionKey_canReOpenRealmWithoutKey() {
 
         // STEP 1: open a synced Realm using a local encryption key
-        val username = UUID.randomUUID().toString()
-        val password = "password"
-        var user: SyncUser = SyncUser.logIn(SyncCredentials.usernamePassword(username, password, true), Constants.AUTH_URL)
+        var user = app.registerUserAndLogin(TestHelper.getRandomEmail(), SECRET_PASSWORD)
         val randomKey = TestHelper.getRandomKey()
-        val configWithEncryption: SyncConfiguration = configurationFactory.createSyncConfigurationBuilder(user, Constants.USER_REALM)
-                .modules(StringOnlyModule())
+        val configWithEncryption: SyncConfiguration = configurationFactory.createSyncConfigurationBuilder(user, BsonObjectId())
+                .testSchema(SyncStringOnly::class.java)
                 .waitForInitialRemoteData()
-                .errorHandler(object : ErrorHandler() {
-                    fun onError(session: SyncSession?, error: ObjectServerError) {
-                        Assert.fail(error.getErrorMessage())
-                    }
-                })
+                .errorHandler { session, error -> Assert.fail(error.getErrorMessage()) }
                 .encryptionKey(randomKey)
                 .build()
-        var realm = Realm.getInstance(configWithEncryption)
-        Assert.assertTrue(realm.isEmpty)
-        realm.beginTransaction()
-        realm.createObject(StringOnly::class.java).chars = "Hi Alice"
-        realm.commitTransaction()
 
-        // STEP 2:  make sure the changes gets to the server
-        SyncManager.getSession(configWithEncryption).uploadAllLocalChanges()
-        realm.close()
+        Realm.getInstance(configWithEncryption).use { realm ->
+            Assert.assertTrue(realm.isEmpty)
+            realm.executeTransaction {
+                realm.createObject(SyncStringOnly::class.java, ObjectId()).chars = "Hi Alice"
+            }
+
+            // STEP 2:  make sure the changes gets to the server
+            realm.syncSession.uploadAllLocalChanges()
+        }
         user.logOut()
 
         // STEP 3: try to open again the same sync Realm but different local name without the encryption key should not
         // fail
-        user = SyncUser.logIn(SyncCredentials.usernamePassword(username, password, false), Constants.AUTH_URL)
-        val configWithoutEncryption: SyncConfiguration = configurationFactory.createSyncConfigurationBuilder(user, Constants.USER_REALM)
-                .name("newName")
-                .modules(StringOnlyModule())
+        var user2 = app.registerUserAndLogin(TestHelper.getRandomEmail(), SECRET_PASSWORD)
+        val configWithoutEncryption: SyncConfiguration = configurationFactory.createSyncConfigurationBuilder(user2, configWithEncryption.partitionValue)
+                // Using different user with same partition value to trigger a different path instead of
+                // .name("newName")
+                .testSchema(SyncStringOnly::class.java)
                 .waitForInitialRemoteData()
-                .errorHandler(object : ErrorHandler() {
-                    fun onError(session: SyncSession?, error: ObjectServerError) {
-                        Assert.fail(error.getErrorMessage())
-                    }
-                })
+                .errorHandler { session, error -> Assert.fail(error.getErrorMessage()) }
                 .build()
-        realm = Realm.getInstance(configWithoutEncryption)
-        val all = realm.where(StringOnly::class.java).findAll()
-        Assert.assertEquals(1, all.size.toLong())
-        Assert.assertEquals("Hi Alice", all[0]!!.chars)
-        realm.close()
+
+        Realm.getInstance(configWithoutEncryption).use { realm ->
+            val all = realm.where(SyncStringOnly::class.java).findAll()
+            Assert.assertEquals(1, all.size.toLong())
+            Assert.assertEquals("Hi Alice", all[0]!!.chars)
+        }
         user.logOut()
     }
 
     // If an encrypted synced Realm is re-opened with the wrong key, throw an exception.
     @Test
-    @Throws(InterruptedException::class)
     fun setEncryptionKey_shouldCrashIfKeyNotProvided() {
         // STEP 1: open a synced Realm using a local encryption key
-        val username = UUID.randomUUID().toString()
-        val password = "password"
-        var user: SyncUser = SyncUser.logIn(SyncCredentials.usernamePassword(username, password, true), Constants.AUTH_URL)
+        var user = app.registerUserAndLogin(TestHelper.getRandomEmail(), SECRET_PASSWORD)
         val randomKey = TestHelper.getRandomKey()
-        val configWithEncryption: SyncConfiguration = configurationFactory.createSyncConfigurationBuilder(user, Constants.USER_REALM)
-                .modules(StringOnlyModule())
+        val configWithEncryption: SyncConfiguration = configurationFactory.createSyncConfigurationBuilder(user, BsonObjectId())
+                .testSchema(SyncStringOnly::class.java)
                 .waitForInitialRemoteData()
-                .errorHandler(object : ErrorHandler() {
-                    fun onError(session: SyncSession?, error: ObjectServerError) {
-                        Assert.fail(error.getErrorMessage())
-                    }
-                })
+                .errorHandler { session, error -> Assert.fail(error.getErrorMessage()) }
                 .encryptionKey(randomKey)
                 .build()
-        var realm = Realm.getInstance(configWithEncryption)
-        Assert.assertTrue(realm!!.isEmpty)
-        realm.beginTransaction()
-        realm.createObject(StringOnly::class.java).chars = "Hi Alice"
-        realm.commitTransaction()
 
-        // STEP 2: Close the Realm and log the user out to forget about it.
-        realm.close()
+        Realm.getInstance(configWithEncryption).use { realm ->
+            Assert.assertTrue(realm!!.isEmpty)
+            realm.executeTransaction {
+                realm.createObject(SyncStringOnly::class.java, ObjectId()).chars = "Hi Alice"
+            }
+            // STEP 2: Close the Realm and log the user out to forget about it.
+        }
         user.logOut()
 
         // STEP 3: try to open again the Realm without the encryption key should fail
-        user = SyncUser.logIn(SyncCredentials.usernamePassword(username, password, false), Constants.AUTH_URL)
-        val configWithoutEncryption: SyncConfiguration = configurationFactory.createSyncConfigurationBuilder(user, Constants.USER_REALM)
-                .modules(StringOnlyModule())
+        user = app.login(Credentials.emailPassword(user.email, SECRET_PASSWORD))
+        val configWithoutEncryption: SyncConfiguration = configurationFactory.createSyncConfigurationBuilder(user, configWithEncryption.partitionValue)
+                .testSchema(SyncStringOnly::class.java)
                 .waitForInitialRemoteData()
-                .errorHandler(object : ErrorHandler() {
-                    fun onError(session: SyncSession?, error: ObjectServerError) {
-                        Assert.fail(error.getErrorMessage())
-                    }
-                })
+                .errorHandler { session, error -> Assert.fail(error.getErrorMessage()) }
                 .build()
-        try {
-            realm = Realm.getInstance(configWithoutEncryption)
-            Assert.fail("It should not be possible to open the Realm without the encryption key set previously.")
-        } catch (ignored: RealmFileException) {
-        } finally {
-            realm?.close()
+
+        assertFailsWith<RealmFileException> {
+            Realm.getInstance(configWithoutEncryption).close()
         }
     }
 
     // If client B encrypts its synced Realm, client A should be able to access that Realm with a different encryption key.
     @Test
-    @Throws(InterruptedException::class)
     fun setEncryptionKey_differentClientsWithDifferentKeys() {
         // STEP 1: prepare a synced Realm for client A
-        val username = UUID.randomUUID().toString()
-        val password = "password"
-        val user: SyncUser = SyncUser.logIn(SyncCredentials.usernamePassword(username, password, true), Constants.AUTH_URL)
+        var user = app.registerUserAndLogin(TestHelper.getRandomEmail(), SECRET_PASSWORD)
         val randomKey = TestHelper.getRandomKey()
-        val configWithEncryption: SyncConfiguration = configurationFactory.createSyncConfigurationBuilder(user, Constants.USER_REALM)
-                .modules(StringOnlyModule())
+        val configWithEncryption: SyncConfiguration = configurationFactory.createSyncConfigurationBuilder(user, BsonObjectId())
+                .testSchema(SyncStringOnly::class.java)
                 .waitForInitialRemoteData()
-                .errorHandler(object : ErrorHandler() {
-                    fun onError(session: SyncSession?, error: ObjectServerError) {
-                        Assert.fail(error.getErrorMessage())
-                    }
-                })
+                .errorHandler { session, error -> Assert.fail(error.getErrorMessage()) }
                 .encryptionKey(randomKey)
                 .build()
-        var realm = Realm.getInstance(configWithEncryption)
-        Assert.assertTrue(realm.isEmpty)
-        realm.beginTransaction()
-        realm.createObject(StringOnly::class.java).chars = "Hi Alice"
-        realm.commitTransaction()
 
-        // STEP 2: make sure the changes gets to the server
-        SyncManager.getSession(configWithEncryption).uploadAllLocalChanges()
-        realm.close()
+        Realm.getInstance(configWithEncryption).use { realm ->
+            Assert.assertTrue(realm.isEmpty)
+            realm.executeTransaction {
+                realm.createObject(SyncStringOnly::class.java, ObjectId()).chars = "Hi Alice"
+            }
+            // STEP 2: make sure the changes gets to the server
+            realm.syncSession.uploadAllLocalChanges()
+        }
 
-        // STEP 3: prepare a synced Realm for client B (admin user)
-        val admin: SyncUser = UserFactory.createAdminUser(Constants.AUTH_URL)
-        val credentials: SyncCredentials = SyncCredentials.accessToken(SyncTestUtils.getRefreshToken(admin).value(), "custom-admin-user")
-        val adminUser: SyncUser = SyncUser.logIn(credentials, Constants.AUTH_URL)
-        val adminRandomKey = TestHelper.getRandomKey()
-        val adminConfigWithEncryption: SyncConfiguration = configurationFactory.createSyncConfigurationBuilder(adminUser, configWithEncryption.getServerUrl().toString())
-                .modules(StringOnlyModule())
+        // STEP 3: prepare a synced Realm for client B
+        var user2 = app.registerUserAndLogin(TestHelper.getRandomEmail(), SECRET_PASSWORD)
+        val key2 = TestHelper.getRandomKey()
+        val configWithEncryption2: SyncConfiguration = configurationFactory.createSyncConfigurationBuilder(user2, configWithEncryption.partitionValue)
+                .testSchema(SyncStringOnly::class.java)
                 .waitForInitialRemoteData()
-                .errorHandler(object : ErrorHandler() {
-                    fun onError(session: SyncSession?, error: ObjectServerError) {
-                        Assert.fail(error.getErrorMessage())
-                    }
-                })
-                .encryptionKey(adminRandomKey)
+                .errorHandler { session, error -> Assert.fail(error.getErrorMessage()) }
+                .encryptionKey(key2)
                 .build()
-        var adminRealm = Realm.getInstance(adminConfigWithEncryption)
-        val all = adminRealm.where(StringOnly::class.java).findAll()
-        Assert.assertEquals(1, all.size.toLong())
-        Assert.assertEquals("Hi Alice", all[0]!!.chars)
-        adminRealm.beginTransaction()
-        adminRealm.createObject(StringOnly::class.java).chars = "Hi Bob"
-        adminRealm.commitTransaction()
-        SyncManager.getSession(adminConfigWithEncryption).uploadAllLocalChanges()
-        adminRealm.close()
+
+        Realm.getInstance(configWithEncryption2).use { realm ->
+            val all = realm.where(SyncStringOnly::class.java).findAll()
+            Assert.assertEquals(1, all.size.toLong())
+            Assert.assertEquals("Hi Alice", all[0]!!.chars)
+            realm.executeTransaction {
+                realm.createObject(SyncStringOnly::class.java, ObjectId()).chars = "Hi Bob"
+            }
+            realm.syncSession.uploadAllLocalChanges()
+        }
 
         // STEP 4: client A can see changes from client B (although they're using different encryption keys)
-        realm = Realm.getInstance(configWithEncryption)
-        SyncManager.getSession(configWithEncryption).downloadAllServerChanges() // force download latest commits from ROS
-        realm.refresh() // Not calling refresh will still point to the previous version of the Realm without the latest admin commit  "Hi Bob"
-        Assert.assertEquals(2, realm.where(StringOnly::class.java).count())
-        adminRealm = Realm.getInstance(adminConfigWithEncryption)
-        val allSorted = realm.where(StringOnly::class.java).sort(StringOnly.FIELD_CHARS).findAll()
-        val allSortedAdmin = adminRealm.where(StringOnly::class.java).sort(StringOnly.FIELD_CHARS).findAll()
-        Assert.assertEquals("Hi Alice", allSorted[0]!!.chars)
-        Assert.assertEquals("Hi Bob", allSorted[1]!!.chars)
-        Assert.assertEquals("Hi Alice", allSortedAdmin[0]!!.chars)
-        Assert.assertEquals("Hi Bob", allSortedAdmin[1]!!.chars)
-        adminRealm.close()
-        adminUser.logOut()
-        realm.close()
+        Realm.getInstance(configWithEncryption).use { realm ->
+            realm.syncSession.downloadAllServerChanges() // force download latest commits from remote realm
+            realm.refresh() // Not calling refresh will still point to the previous version of the Realm without the latest admin commit  "Hi Bob"
+            Assert.assertEquals(2, realm.where(SyncStringOnly::class.java).count())
+            val allSorted = realm.where(SyncStringOnly::class.java).sort(SyncStringOnly.FIELD_CHARS).findAll()
+            val allSortedAdmin = realm.where(SyncStringOnly::class.java).sort(SyncStringOnly.FIELD_CHARS).findAll()
+            Assert.assertEquals("Hi Alice", allSorted[0]!!.chars)
+            Assert.assertEquals("Hi Bob", allSorted[1]!!.chars)
+            Assert.assertEquals("Hi Alice", allSortedAdmin[0]!!.chars)
+            Assert.assertEquals("Hi Bob", allSortedAdmin[1]!!.chars)
+        }
+
         user.logOut()
+        user2.logOut()
     }
 }
