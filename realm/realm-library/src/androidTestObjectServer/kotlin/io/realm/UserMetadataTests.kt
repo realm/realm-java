3c20da69240da49988c89ce606e6d1afd5ec71e2
Clemente Tort <clementetb@gmail.com>
2020-08-11 10:12:44 +0200

    Add missing tests for RealmUser.

---
+++ realm/realm-library/src/androidTestObjectServer/kotlin/io/realm/UserMetadataTests.kt
@@ -0,0 +1,203 @@
+/*
+ * Copyright 2020 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.realm
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import io.realm.admin.ServerAdmin
+import io.realm.internal.objectstore.OsJavaNetworkTransport
+import io.realm.mongodb.*
+import io.realm.rule.BlockingLooperThread
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class UserMetadataTests {
+
+    val looperThread = BlockingLooperThread()
+
+    private lateinit var app: App
+    private lateinit var admin: ServerAdmin
+
+    @Before
+    fun setUp() {
+        Realm.init(InstrumentationRegistry.getInstrumentation().targetContext)
+
+        val successHeaders: Map<String, String> = mapOf(Pair("Content-Type", "application/json"))
+        app = TestApp(object : OsJavaNetworkTransport() {
+            override fun sendRequest(method: String, url: String, timeoutMs: Long, headers: MutableMap<String, String>, body: String): Response {
+                var result = ""
+                if (url.endsWith("/providers/${Credentials.IdentityProvider.EMAIL_PASSWORD.id}/login")) {
+                    result = """
+                        {
+                            "access_token": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjVlNjk2M2RmYWZlYTYzMjU0NTgxYzAyNiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODM5NjcyMDgsImlhdCI6MTU4Mzk2NTQwOCwiaXNzIjoiNWU2OTY0ZTBhZmVhNjMyNTQ1ODFjMWEzIiwic3RpdGNoX2RldklkIjoiMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwIiwic3RpdGNoX2RvbWFpbklkIjoiNWU2OTYzZGVhZmVhNjMyNTQ1ODFjMDI1Iiwic3ViIjoiNWU2OTY0ZTBhZmVhNjMyNTQ1ODFjMWExIiwidHlwIjoiYWNjZXNzIn0.J4mp8LnlsxTQRV_7W2Er4qY0tptR76PJGG1k6HSMmUYqgfpJC2Fnbcf1VCoebzoNolH2-sr8AHDVBBCyjxRjqoY9OudFHmWZKmhDV1ysxPP4XmID0nUuN45qJSO8QEAqoOmP1crXjrUZWedFw8aaCZE-bxYfvcDHyjBcbNKZqzawwUw2PyTOlrNjgs01k2J4o5a5XzYkEsJuzr4_8UqKW6zXvYj24UtqnqoYatW5EzpX63m2qig8AcBwPK4ZHb5wEEUdf4QZxkRY5QmTgRHP8SSqVUB_mkHgKaizC_tSB3E0BekaDfLyWVC1taAstXJNfzgFtLI86AzuXS2dCiCfqQ",
+                            "refresh_token": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjVlNjk2M2RmYWZlYTYzMjU0NTgxYzAyNiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODkxNDk0MDgsImlhdCI6MTU4Mzk2NTQwOCwic3RpdGNoX2RhdGEiOm51bGwsInN0aXRjaF9kZXZJZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsInN0aXRjaF9kb21haW5JZCI6IjVlNjk2M2RlYWZlYTYzMjU0NTgxYzAyNSIsInN0aXRjaF9pZCI6IjVlNjk2NGUwYWZlYTYzMjU0NTgxYzFhMyIsInN0aXRjaF9pZGVudCI6eyJpZCI6IjVlNjk2NGUwYWZlYTYzMjU0NTgxYzFhMC1oaWF2b3ZkbmJxbGNsYXBwYnl1cmJpaW8iLCJwcm92aWRlcl90eXBlIjoiYW5vbi11c2VyIiwicHJvdmlkZXJfaWQiOiI1ZTY5NjNlMGFmZWE2MzI1NDU4MWMwNGEifSwic3ViIjoiNWU2OTY0ZTBhZmVhNjMyNTQ1ODFjMWExIiwidHlwIjoicmVmcmVzaCJ9.FhLdpmL48Mw0SyUKWuaplz3wfeS8TCO8S7I9pIJenQww9nPqQ7lIvykQxjCCtinGvsZIJKt_7R31xYCq4Jp53Nw81By79IwkXtO7VXHPsXXZG5_2xV-s0u44e85sYD5su_H-xnx03sU2piJbWJLSB8dKu3rMD4mO-S0HNXCCAty-JkYKSaM2-d_nS8MNb6k7Vfm7y69iz_uwHc-bb_1rPg7r827K6DEeEMF41Hy3Nx1kCdAUOM9-6nYv3pZSU1PFrGYi2uyTXPJ7R7HigY5IGHWd0hwONb_NUr4An2omqfvlkLEd77ut4V9m6mExFkoKzRz7shzn-IGkh3e4h7ECGA",
+                            "user_id": "5e6964e0afea63254581c1a1",
+                            "device_id": "000000000000000000000000"
+                        }            
+                    """.trimIndent()
+                } else if (url.endsWith("/auth/profile")) {
+                    result = """
+                        {
+                            "user_id": "5e6964e0afea63254581c1a1",
+                            "domain_id": "000000000000000000000000",
+                            "identities": [
+                                {
+                                    "id": "5e68f51ade5ba998bb17500d",
+                                    "provider_type": "local-userpass",
+                                    "provider_id": "000000000000000000000003",
+                                    "provider_data": {
+                                        "email": "unique_user@domain.com"
+                                    }
+                                }
+                            ],
+                            "data": {
+                                "name": "NAME",
+                                "email": "unique_user@domain.com",
+                                "picture_url": "PICTURE_URL",
+                                "first_name": "FIRST_NAME",
+                                "last_name": "LAST_NAME",
+                                "gender": "GENDER",
+                                "birthday": "BIRTHDAY",
+                                "min_age": "1",
+                                "max_age": "99"
+                            },
+                            "type": "normal",
+                            "roles": [
+                                {
+                                    "role_name": "GROUP_OWNER",
+                                    "group_id": "5e68f51e087b1b33a53f56d5"
+                                }
+                            ]
+                        }
+                    """.trimIndent()
+                } else if (url.endsWith("/location")) {
+                    return Response.httpResponse(200, mapOf(), """
+                        { "deployment_model" : "GLOBAL",
+                          "location": "US-VA", 
+                          "hostname": "http://localhost:9090",
+                          "ws_hostname": "ws://localhost:9090"
+                        }
+                        """.trimIndent())
+                } else if (url.endsWith("/providers/${Credentials.IdentityProvider.EMAIL_PASSWORD.id}/register")) {
+                    result = ""
+                } else {
+                    fail("Unexpected request url: $url")
+                }
+                return Response.httpResponse(200, successHeaders, result)
+            }
+        })
+
+        admin = ServerAdmin()
+    }
+
+    @After
+    fun tearDown() {
+        if (this::app.isInitialized) {
+            app.close()
+        }
+    }
+
+    @Test
+    fun getUserId() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals("5e6964e0afea63254581c1a1", user.id)
+    }
+
+    @Test
+    fun getDeviceId() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals("000000000000000000000000", user.deviceId)
+    }
+
+    @Test
+    fun accessToken() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals("eyJhbGciOiJSUzI1NiIsImtpZCI6IjVlNjk2M2RmYWZlYTYzMjU0NTgxYzAyNiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODM5NjcyMDgsImlhdCI6MTU4Mzk2NTQwOCwiaXNzIjoiNWU2OTY0ZTBhZmVhNjMyNTQ1ODFjMWEzIiwic3RpdGNoX2RldklkIjoiMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwIiwic3RpdGNoX2RvbWFpbklkIjoiNWU2OTYzZGVhZmVhNjMyNTQ1ODFjMDI1Iiwic3ViIjoiNWU2OTY0ZTBhZmVhNjMyNTQ1ODFjMWExIiwidHlwIjoiYWNjZXNzIn0.J4mp8LnlsxTQRV_7W2Er4qY0tptR76PJGG1k6HSMmUYqgfpJC2Fnbcf1VCoebzoNolH2-sr8AHDVBBCyjxRjqoY9OudFHmWZKmhDV1ysxPP4XmID0nUuN45qJSO8QEAqoOmP1crXjrUZWedFw8aaCZE-bxYfvcDHyjBcbNKZqzawwUw2PyTOlrNjgs01k2J4o5a5XzYkEsJuzr4_8UqKW6zXvYj24UtqnqoYatW5EzpX63m2qig8AcBwPK4ZHb5wEEUdf4QZxkRY5QmTgRHP8SSqVUB_mkHgKaizC_tSB3E0BekaDfLyWVC1taAstXJNfzgFtLI86AzuXS2dCiCfqQ",
+                user.accessToken)
+    }
+
+    @Test
+    fun refreshToken() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals("eyJhbGciOiJSUzI1NiIsImtpZCI6IjVlNjk2M2RmYWZlYTYzMjU0NTgxYzAyNiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODkxNDk0MDgsImlhdCI6MTU4Mzk2NTQwOCwic3RpdGNoX2RhdGEiOm51bGwsInN0aXRjaF9kZXZJZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsInN0aXRjaF9kb21haW5JZCI6IjVlNjk2M2RlYWZlYTYzMjU0NTgxYzAyNSIsInN0aXRjaF9pZCI6IjVlNjk2NGUwYWZlYTYzMjU0NTgxYzFhMyIsInN0aXRjaF9pZGVudCI6eyJpZCI6IjVlNjk2NGUwYWZlYTYzMjU0NTgxYzFhMC1oaWF2b3ZkbmJxbGNsYXBwYnl1cmJpaW8iLCJwcm92aWRlcl90eXBlIjoiYW5vbi11c2VyIiwicHJvdmlkZXJfaWQiOiI1ZTY5NjNlMGFmZWE2MzI1NDU4MWMwNGEifSwic3ViIjoiNWU2OTY0ZTBhZmVhNjMyNTQ1ODFjMWExIiwidHlwIjoicmVmcmVzaCJ9.FhLdpmL48Mw0SyUKWuaplz3wfeS8TCO8S7I9pIJenQww9nPqQ7lIvykQxjCCtinGvsZIJKt_7R31xYCq4Jp53Nw81By79IwkXtO7VXHPsXXZG5_2xV-s0u44e85sYD5su_H-xnx03sU2piJbWJLSB8dKu3rMD4mO-S0HNXCCAty-JkYKSaM2-d_nS8MNb6k7Vfm7y69iz_uwHc-bb_1rPg7r827K6DEeEMF41Hy3Nx1kCdAUOM9-6nYv3pZSU1PFrGYi2uyTXPJ7R7HigY5IGHWd0hwONb_NUr4An2omqfvlkLEd77ut4V9m6mExFkoKzRz7shzn-IGkh3e4h7ECGA",
+                user.refreshToken)
+    }
+
+    @Test
+    fun getName() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals("NAME", user.name)
+    }
+
+    @Test
+    fun getEmail() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals("unique_user@domain.com", user.email)
+    }
+
+    @Test
+    fun getFirstName() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals("FIRST_NAME", user.firstName)
+    }
+
+    @Test
+    fun getLastName() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals("LAST_NAME", user.lastName)
+    }
+
+    @Test
+    fun getBirthday() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals("BIRTHDAY", user.birthday)
+    }
+
+    @Test
+    fun getGender() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals("GENDER", user.gender)
+    }
+
+    @Test
+    fun getMinAge() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals(1L, user.minAge)
+    }
+
+    @Test
+    fun getMaxAge() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals(99L, user.maxAge)
+    }
+
+    @Test
+    fun getPictureUrl() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals("PICTURE_URL", user.pictureUrl)
+    }
+
+    @Test
+    fun getProviderType() {
+        val user: User = app.registerUserAndLogin(TestHelper.getRandomEmail(), "123456")
+        assertEquals("local-userpass", user.providerType)
+    }
+}
--- realm/realm-library/src/objectServer/java/io/realm/mongodb/User.java
+++ realm/realm-library/src/objectServer/java/io/realm/mongodb/User.java
@@ -20,7 +20,6 @@ import org.bson.codecs.configuration.CodecRegistry;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicReference;
 
 import javax.annotation.Nullable;
@@ -224,7 +223,6 @@ public class User {
      * Returns a new list of the user's identities.
      *
      * @return the list of identities.
-     *
      * @see UserIdentity
      */
     public List<UserIdentity> getIdentities() {
@@ -235,6 +233,15 @@ public class User {
             identities.add(new UserIdentity(data.first, data.second));
         }
         return identities;
+    }
+
+    /**
+     * Returns the provider type used to log the user
+     *
+     * @return the provider type of the user
+     */
+    public String getProviderType() {
+        return osUser.getProviderType();
     }
 
     /**
@@ -318,7 +325,6 @@ public class User {
      *
      * @param callback The callback that will receive the result or any errors from the request.
      * @return The task representing the ongoing operation.
-     *
      * @throws IllegalStateException if not called on a looper thread.
      */
     public RealmAsyncTask refreshCustomData(App.Callback<Document> callback) {
@@ -336,7 +342,6 @@ public class User {
      * Returns true if the user is currently logged in.
      * Returns whether or not this user is still logged into the MongoDB Realm App.
      *
-     * @return {@code true} if the user is logged in. {@code false} otherwise.
      * @return {@code true} if still logged in, {@code false} if not.
      */
     public boolean isLoggedIn() {
@@ -362,8 +367,8 @@ public class User {
      * must not have been used by another user.
      *
      * @param credentials the credentials to link with the current user.
-     * @throws IllegalStateException if no user is currently logged in.
      * @return the {@link User} the credentials were linked to.
+     * @throws IllegalStateException if no user is currently logged in.
      */
     public User linkCredentials(Credentials credentials) {
         Util.checkNull(credentials, "credentials");
@@ -399,8 +404,8 @@ public class User {
      * must not have been used by another user.
      *
      * @param credentials the credentials to link with the current user.
-     * @param callback callback when user identities has been linked or it failed. The callback will
-     * always happen on the same thread as this method is called on.
+     * @param callback    callback when user identities has been linked or it failed. The callback will
+     *                    always happen on the same thread as this method is called on.
      * @throws IllegalStateException if called from a non-looper thread.
      */
     public RealmAsyncTask linkCredentialsAsync(Credentials credentials, App.Callback<User> callback) {
@@ -420,7 +425,7 @@ public class User {
      *
      * @return user that was removed.
      * @throws AppException if called from the UI thread or if the user was logged in, but
-     * could not be logged out.
+     *                      could not be logged out.
      */
     public User remove() throws AppException {
         boolean loggedIn = isLoggedIn();
@@ -445,7 +450,7 @@ public class User {
      * affect the user state on the server.
      *
      * @param callback callback when removing the user has completed or failed. The callback will always
-     * happen on the same thread as this method is called on.
+     *                 happen on the same thread as this method is called on.
      * @throws IllegalStateException if called from a non-looper thread.
      */
     public RealmAsyncTask removeAsync(App.Callback<User> callback) {
@@ -473,7 +478,7 @@ public class User {
      * {@link #remove()}.
      *
      * @throws AppException if an error occurred while trying to log the user out of the Realm
-     * App.
+     *                      App.
      */
     public void logOut() throws AppException {
         boolean loggedIn = isLoggedIn();
@@ -500,7 +505,7 @@ public class User {
      * {@link #remove()}.
      *
      * @param callback callback when logging out has completed or failed. The callback will always
-     * happen on the same thread as this method is called on.
+     *                 happen on the same thread as this method is called on.
      * @throws IllegalStateException if called from a non-looper thread.
      */
     public RealmAsyncTask logOutAsync(App.Callback<User> callback) {
@@ -559,7 +564,7 @@ public class User {
     /**
      * Returns the {@link Push} instance for managing push notification registrations.
      *
-     * @param serviceName        the service name used to connect to the server.
+     * @param serviceName the service name used to connect to the server.
      */
     public synchronized Push getPush(String serviceName) {
         if (push == null) {
@@ -572,7 +577,7 @@ public class User {
     /**
      * Returns a {@link MongoClient} instance for accessing documents in the database.
      *
-     * @param serviceName        the service name used to connect to the server.
+     * @param serviceName the service name used to connect to the server.
      */
     public synchronized MongoClient getMongoClient(String serviceName) {
         Util.checkEmpty(serviceName, "serviceName");
@@ -609,6 +614,8 @@ public class User {
     }
 
     private static native void nativeRemoveUser(long nativeAppPtr, long nativeUserPtr, OsJavaNetworkTransport.NetworkTransportJNIResultCallback callback);
+
     private static native void nativeLinkUser(long nativeAppPtr, long nativeUserPtr, long nativeCredentialsPtr, OsJavaNetworkTransport.NetworkTransportJNIResultCallback callback);
+
     private static native void nativeLogOut(long appNativePtr, long userNativePtr, OsJavaNetworkTransport.NetworkTransportJNIResultCallback callback);
 }
