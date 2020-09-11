061e350ccd2d127cc7cfb3f0187b7899c7e45bc3
Clemente Tort <clementetb@gmail.com>
2020-09-11 14:49:43 +0200

    Fix broken import of cherry picking from v10

--- realm/realm-library/src/androidTest/java/io/realm/UTFStrings.java
+++ realm/realm-library/src/androidTest/java/io/realm/UTFStrings.java
@@ -15,7 +15,7 @@
  */
 package io.realm;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.After;
 import org.junit.Before;
