ef91bb62a99573e2f5a478125d981e0625493447
Clemente Tort <clementetb@gmail.com>
2020-12-14 10:41:48 +0100

    Fix rename of REALM_SYNC

--- realm-transformer/src/main/templates/Version.java
+++ realm-transformer/src/main/templates/Version.java
@@ -2,5 +2,5 @@ package io.realm.transformer;
 
 public class Version {
     public static final String VERSION = "@version@";
-    public static final String SYNC_VERSION = "@syncVersion@";
+    public static final String SYNC_VERSION = "@coreVersion@";
 }
