9dc2829d28a4e15e9a5b7e97a35543df08fa02cc
Clemente Tort <clementetb@gmail.com>
2021-02-11 14:00:53 +0100

    Fix nullability check on realm results

--- realm/realm-library/src/main/java/io/realm/internal/objectstore/OsObjectBuilder.java
+++ realm/realm-library/src/main/java/io/realm/internal/objectstore/OsObjectBuilder.java
@@ -312,7 +312,7 @@ public class OsObjectBuilder implements Closeable {
     private <T> void addListItem(long builderPtr, long columnKey, @Nullable List<T> list, ItemCallback<T> itemCallback) {
         if (list != null) {
             long listPtr = nativeStartList(list.size());
-            boolean isNullable = table.isColumnNullable(columnKey);
+            boolean isNullable = (columnKey == 0) || table.isColumnNullable(columnKey);
             for (int i = 0; i < list.size(); i++) {
                 T item = list.get(i);
                 if (item == null) {
