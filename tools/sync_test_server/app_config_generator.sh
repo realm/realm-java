4b0f6515c7b011cbdb24e1a4d38e0d00280d160f
Clemente Tort <clementetb@gmail.com>
2021-03-10 15:25:15 +0100

    Template app name in config

--- tools/sync_test_server/app_config_generator.sh
+++ tools/sync_test_server/app_config_generator.sh
@@ -4,7 +4,8 @@ TEMPLATE_APP_PATH=$1;shift
 mkdir -p $TARGET_APP_PATH
 for APP_ID in "$@"
 do
-    APP_PATH=`echo $APP_ID | cut -f1 -d-`
-    cp -r $TEMPLATE_APP_PATH $TARGET_APP_PATH/$APP_PATH
-    sed -i '.bak' 's/APP_ID_PLACEHOLDER/'$APP_ID'/g' $TARGET_APP_PATH/$APP_PATH/config.json
+    APP_NAME=`echo $APP_ID | cut -f1 -d-`
+    cp -r $TEMPLATE_APP_PATH $TARGET_APP_PATH/$APP_NAME
+    sed -i '.bak' 's/APP_ID_PLACEHOLDER/'$APP_ID'/g' $TARGET_APP_PATH/$APP_NAME/config.json
+    sed -i '.bak' 's/APP_NAME_PLACEHOLDER/'$APP_NAME'/g' $TARGET_APP_PATH/$APP_NAME/config.json
 done
