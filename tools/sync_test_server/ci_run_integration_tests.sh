53831aea4733efc801ebd5abce8fd6bf5613d53d
Christian Melchior <christian@ilios.dk>
2021-03-12 22:02:29 +0100

    Use bash everywhere

--- tools/sync_test_server/ci_run_integration_tests.sh
+++ tools/sync_test_server/ci_run_integration_tests.sh
@@ -3,6 +3,8 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd $SCRIPTPATH/baas_local
-./start_local_server.sh
+bash ./start_local_server.sh
 cd ../../../realm
 ./gradlew connectedBaseDebugAndroidTest -PbuildTargetABIs=x86 -PenableLTO=false -PbuildCore=true
+cd $SCRIPTPATH/baas_local
+bash ./stop_local_server.sh
