32ce4b218d1e714b7a133fc3f7043acfddf74ba6
Christian Melchior <christian@ilios.dk>
2021-03-12 20:16:00 +0100

    Fix path

--- tools/sync_test_server/ci_run_integration_tests.sh
+++ tools/sync_test_server/ci_run_integration_tests.sh
@@ -2,7 +2,7 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cd $SCRIPTPATH/tools/sync_test_server/baas_local
+cd $SCRIPTPATH/baas_local
 ./start_local_server.sh
 cd ../../../realm
 ./gradlew connectedBaseDebugAndroidTest -PbuildTargetABIs=x86 -PenableLTO=false -PbuildCore=true
