09e1e04f37a4d4fd85cebf5af0f9ad349d1904f5
Christian Melchior <christian@ilios.dk>
2021-03-15 16:40:43 +0100

    Archive logcat and mongodb logs

--- tools/sync_test_server/ci_run_integration_tests.sh
+++ tools/sync_test_server/ci_run_integration_tests.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-
+# Script for running integration tests on CI
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "Start building local BAAS"
 cd $SCRIPTPATH/baas_local
 bash ./start_local_server.sh
 cd ../../../realm
-# TODO: Support different flags and tests for full release buils
+adb root
+adb logcat -b all -c
+adb logcat -v time > 'logcat.txt' &
+LOG_CAT_PID=`echo \$!`
+# TODO: Build parameters should be command line parameters
 ./gradlew connectedObjectServerDebugAndroidTest -PbuildTargetABIs=x86 -PenableLTO=false -PbuildCore=true
 EXIT_CODE=`echo $?`
 echo "Android tests exit code: $EXIT_CODE"
+kill $LOG_CAT_PID
 cd $SCRIPTPATH/baas_local
 bash ./stop_local_server.sh
 echo "Local servers stopped"
