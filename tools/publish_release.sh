b582fed2ad414723700ff4090a42f8965a863d19
Christian Melchior <christian@ilios.dk>
2021-05-10 13:04:50 +0200

    Propagate build params correctly when uploading to Sonatype

--- tools/publish_release.sh
+++ tools/publish_release.sh
@@ -20,12 +20,12 @@ set -e
 
 usage() {
 cat <<EOF
-Usage: $0 <maven_central_user> <maven_central_key> <realm_s3_access_key> <realm_s3_secret_key> <docs_s3_access_key> <docs_s3_secret_key> <slack-webhook-releases-url> <slack-webhook-java-ci-url>
+Usage: $0 <maven_central_user> <maven_central_key> <realm_s3_access_key> <realm_s3_secret_key> <docs_s3_access_key> <docs_s3_secret_key> <slack-webhook-releases-url> <slack-webhook-java-ci-url> <gradle-build-params>
 Usage: $0 verify
 EOF
 }
 
-if [ "$#" -ne 8 ] && [ "$1" != "verify" ]; then
+if [ "$#" -ne 9 ] && [ "$1" != "verify" ]; then
   usage
   exit 1
 fi
@@ -45,6 +45,7 @@ DOCS_S3_ACCESS_KEY="$5"
 DOCS_S3_SECRET_KEY="$6"
 SLACK_WEBHOOK_RELEASES_URL="$7"
 SLACK_WEBHOOK_JAVA_CI_URL="$8"
+GRADLE_BUILD_PARAMS="$9"
 
 abort_release() {
   # Reporting failures to #realm-java-team-ci is done from Jenkins
@@ -115,7 +116,7 @@ upload_to_mavenCentral() {
   # with half a release. For this reason we must manually close and release the artifacts using the Maven Central UI:
   # https://oss.sonatype.org/#stagingRepositories
   # ./gradlew mavenCentralUpload closeAndReleaseStagingRepository -PossrhUsername=$MAVEN_CENTRAL_USER -PossrhPassword=$MAVEN_CENTRAL_KEY --stacktrace
-  ./gradlew mavenCentralUpload -PossrhUsername=$MAVEN_CENTRAL_USER -PossrhPassword=$MAVEN_CENTRAL_KEY --stacktrace
+  ./gradlew mavenCentralUpload $GRADLE_BUILD_PARAMS -PossrhUsername=$MAVEN_CENTRAL_USER -PossrhPassword=$MAVEN_CENTRAL_KEY --stacktrace
   cd $HERE
 }
 
