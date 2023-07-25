37083aa1219810413c378f673a93f6a5ea505646
Chris Bush <chris.bush@mongodb.com>
2023-07-25 10:21:37 -0400

    Add script to fix H1s on docs (#7835)
    * Add script to fix H1s on docs
    
    Per SEO team request, the Javadoc generated docs need H1s.
    
    * Update javadoc header-fix script
    
    ---------
    
    Co-authored-by: Claus Rørbech <claus.rorbech@mongodb.com>

---
+++ tools/fix-h1s.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -ex
+
+usage() {
+cat <<EOF
+Usage: $0 <javadoc-root-dir>
+EOF
+}
+
+if [ "$#" -ne 1 ] ; then
+  usage
+  exit 1
+fi
+
+# Assume Dokka has been run
+pushd $1
+
+find . -name "*.html" | while read ln
+do
+  # Make the output SEO friendly by converting the "h2" title to the proper "h1"
+  sed -i -e 's|<h2\(.* class="title".*\)</h2>|<h1\1</h1>|' "$ln"
+done
+find . -iname "*.html-e" | xargs rm
+
+popd
