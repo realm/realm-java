774878817ad2c7806d60279653f80367b62f3c37
Christian Melchior <christian.melchior@mongodb.com>
2023-10-11 15:34:26 +0200

    Formatting

--- realm/realm-library/src/androidTestObjectServer/kotlin/io/realm/internal/network/NetworkRequestTests.kt
+++ realm/realm-library/src/androidTestObjectServer/kotlin/io/realm/internal/network/NetworkRequestTests.kt
@@ -12,30 +12,30 @@ import kotlin.test.assertFailsWith
 
 class NetworkRequestTests {
 
-	private lateinit var app: App
+    private lateinit var app: App
 
-	@Before
-	fun setUp() {
-		Realm.init(InstrumentationRegistry.getInstrumentation().targetContext)
-	}
+    @Before
+    fun setUp() {
+        Realm.init(InstrumentationRegistry.getInstrumentation().targetContext)
+    }
 
-	// Test for https://github.com/realm/realm-java/issues/7847
-	@Test
-	fun interruptedRequestReturnsError() {
-		val request = object: NetworkRequest<Unit>() {
-			override fun mapSuccess(result: Any?): Unit {
-				Unit
-			}
+    // Test for https://github.com/realm/realm-java/issues/7847
+    @Test
+    fun interruptedRequestReturnsError() {
+        val request = object: NetworkRequest<Unit>() {
+            override fun mapSuccess(result: Any?): Unit {
+                Unit
+            }
 
-			override fun execute(callback: NetworkRequest<Unit>) {
-				Thread.currentThread().interrupt()
-			}
-		}
-		assertFailsWith<AppException> {
-			request.resultOrThrow()
-		}.also {
-			assertEquals(it.errorCode, ErrorCode.NETWORK_INTERRUPTED)
-			assertEquals(it.errorMessage, "Network request interrupted.")
-		}
-	}
+            override fun execute(callback: NetworkRequest<Unit>) {
+                Thread.currentThread().interrupt()
+            }
+        }
+        assertFailsWith<AppException> {
+            request.resultOrThrow()
+        }.also {
+            assertEquals(it.errorCode, ErrorCode.NETWORK_INTERRUPTED)
+            assertEquals(it.errorMessage, "Network request interrupted.")
+        }
+    }
 }
